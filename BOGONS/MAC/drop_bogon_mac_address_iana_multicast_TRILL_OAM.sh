f17e0e9eb8242d3d350b2be59982f1458cc0d6a2
Dylan <sunny73cr@protonmail.com>
2024-07-17 22:31:22 +1200

    add drop_bogon_mac_address_iana_multicast_TRILL_OAM.sh - a rule-building script that drops a packet when the source or destination MAC address is part of the TRILL OAM protocol multicast assignment

---
+++ BOGONS/MAC/drop_bogon_mac_address_iana_multicast_TRILL_OAM.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+usage () {
+	echo "Usage: $0 --address-type <source|destination>">&2;
+	exit 2;
+}
+
+while true; do
+	case "$1" in
+		--address-type)
+			ADDRESS_TYPE="$2";
+			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+		;;
+		"") break; ;;
+		*) usage; ;;
+	esac
+done;
+
+if [ -z "$ADDRESS_TYPE" ]; then
+	echo "$0; you must supply an address type. (try '--address-type source' or '--address-type destination')">&2;
+	exit 2;
+fi
+
+case "$ADDRESS_TYPE" in
+	"source") ETHER_ADDRESS_TYPE="saddr"; ;;
+	"destination") ETHER_ADDRESS_TYPE="daddr"; ;;
+	*)
+		echo "$0; unrecognised address type. (try '--address-type source' or '--address-type destination')"
+		exit 2;
+	;;
+esac
+
+echo "\t\tether $ETHER_ADDRESS_TYPE = 01:00:5E:90:01:00 \\";
+echo "\t\tlog level warn \\";
+echo "\t\tlog prefix \"Block Bogon MAC $ADDRESS_TYPE address IANA OUI Multicast Transparent Interconnection of Lots of Links 01:00:5E:90:01:00 - \" \\";
+echo "\t\tdrop;";
+
+exit 0;
