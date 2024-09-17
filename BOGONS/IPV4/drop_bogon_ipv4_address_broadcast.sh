d9091807306c6d391dc23116214fdc755835ef03
Dylan <sunny73cr@protonmail.com>
2024-09-17 16:20:04 +1200

    add: drop_bogon_ipv4_address_broadcast.sh - a rule building script that drops packets where the source or destination address is 255.255.255.255

--- BOGONS/IPV4/drop_bogon_ipv4_address_broadcast.sh
+++ BOGONS/IPV4/drop_bogon_ipv4_address_broadcast.sh
@@ -19,23 +19,23 @@ while true; do
 done
 
 if [ -z "$ADDRESS_TYPE" ]; then
-	echo "$0; you must provide a MAC address (try '--address-type source' or '--address-type destianation' without quotes.).">&2;
+	echo "$0; you must provide an IPV4 address 'type' (try '--address-type source' or '--address-type destianation' without quotes.).">&2;
 	exit 2;
 fi
 
-IP_ADDRESS_TYPE="";
+IPV4_ADDRESS_TYPE="";
 
 case "$ADDRESS_TYPE" in
-	"source") IP_ADDRESS_TYPE="saddr"; ;;
-	"destination") IP_ADDRESS_TYPE="daddr"; ;;
+	"source") IPV4_ADDRESS_TYPE="saddr"; ;;
+	"destination") IPV4_ADDRESS_TYPE="daddr"; ;;
 	*)
 		echo "$0; unrecognised address type (try '--address-type source' or '--address-type destianation' without quotes.)" 1>&2;
 		exit 2;
 	;;
 esac
 
-echo "\t\tip $IP_ADDRESS_TYPE 255.255.255.255 \\";
-echo "\t\tlog prefix \"Block Bogon IPV4 $ADDRESS_TYPE address - Broadcast 255.255.255.255 - \" \\";
+echo "\t\tip $IPV4_ADDRESS_TYPE = 255.255.255.255 \\";
+echo "\t\tlog prefix \"Block Bogon IPV4 $ADDRESS_TYPE address - Broadcast IPV4 255.255.255.255 - \" \\";
 echo "\t\tlog level warn \\";
 echo "\t\tlog flags skuid flags ether \\";
 echo "\t\tdrop;";
