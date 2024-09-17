749d520c0d2d74fa3ebf4d7cee66d241752e9444
Dylan <sunny73cr@protonmail.com>
2024-09-17 21:16:50 +1200

    modify: try_match_bogon_ipv4_address_port_control_protocol_anycast.sh - turn it into a match-building script

--- BOGONS/IPV4/try_match_bogon_ipv4_address_port_control_protocol_anycast.sh
+++ BOGONS/IPV4/try_match_bogon_ipv4_address_port_control_protocol_anycast.sh
@@ -36,7 +36,5 @@ case "$ADDRESS_TYPE" in
 esac
 
 echo "\t\tip $IP_ADDRESS_TYPE 192.0.0.9 \\";
-echo "\t\tlog prefix \"Block Bogon IPV4 $ADDRESS_TYPE address - Port Control Protocol Anycast 192.0.0.9 - \" \\";
-echo "\t\tlog level warn \\";
-echo "\t\tlog flags skuid flags ether \\";
-echo "\t\tdrop;";
+
+exit 0;
