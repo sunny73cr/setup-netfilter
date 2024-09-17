fdbcdbd8f5914d80b083edefa10d04fe5f79c22f
Dylan <sunny73cr@protonmail.com>
2024-09-18 10:36:47 +1200

    modify: try_match_bogon_mac_address_broadcast.sh - turn it into a match building script, fix a user facing log, and fix a portion of the match (uneccessary =)

--- BOGONS/MAC/try_match_bogon_mac_address_broadcast.sh
+++ BOGONS/MAC/try_match_bogon_mac_address_broadcast.sh
@@ -19,7 +19,7 @@ while true; do
 done
 
 if [ -z "$ADDRESS_TYPE" ]; then
-	echo "$0; you must provide a MAC address (try '--address-type source' or '--address-type destianation' without quotes.).">&2;
+	echo "$0; you must provide a MAC address 'type' (try '--address-type source' or '--address-type destianation' without quotes.).">&2;
 	exit 2;
 fi
 
@@ -34,8 +34,6 @@ case "$ADDRESS_TYPE" in
 	;;
 esac
 
-echo "\t\tether $MAC_ADDRESS_TYPE = FF:FF:FF:FF:FF:FF \\";
-echo "\t\tlog prefix \"Block Bogon MAC $ADDRESS_TYPE address - Broadcast MAC FF:FF:FF:FF:FF:FF - \" \\";
-echo "\t\tlog level warn \\";
-echo "\t\tlog flags skuid flags ether \\";
-echo "\t\tdrop;";
+echo "\t\tether $MAC_ADDRESS_TYPE FF:FF:FF:FF:FF:FF \\";
+
+exit 0;
