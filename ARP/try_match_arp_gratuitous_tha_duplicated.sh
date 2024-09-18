520bba4f60b7b6d6e87db7621ae3eb6a2c36bbf7
Dylan <sunny73cr@protonmail.com>
2024-09-19 00:08:19 +1200

    modify: try_match_arp_gratuitous_tha_duplicated.sh - cleanup: improve arg parsing, use return codes instead of stdout in validation, comment output

--- ARP/try_match_arp_gratuitous_tha_duplicated.sh
+++ ARP/try_match_arp_gratuitous_tha_duplicated.sh
@@ -27,7 +27,7 @@ if [ ! -x "$DEPENDENCY_SCRIPT_PATH_VALIDATE_IPV4_NETWORK" ]; then
 	exit 3;
 fi
 
-usage () {
+print_usage_then_exit () {
 	echo "Usage: $0 <arguments>">&2;
 	echo "ARGUMENTS:">&2;
 	echo "optional: --source-mac-address <string>">&2;
@@ -40,7 +40,7 @@ usage () {
 	exit 2;
 }
 
-if [ "$1" = "" ]; then usage; fi
+if [ "$1" = "" ]; then print_usage_then_exit; fi
 
 MAC_ADDRESS_SOURCE="";
 REQUESTED_ADDRESS="";
@@ -49,21 +49,45 @@ REQUESTED_NETWORK="";
 while true; do
 	case "$1" in
 		--source-mac-address)
-			MAC_ADDRESS_SOURCE="$2";
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+			#not enough argynents
+			if [ $# -lt 2 ]; then
+
+			#the value is empty
+			elif [ "$2" = "" ] || [ "$(echo $2 | grep -G '^-')" != "" ]; then
+
+			else
+				MAC_ADDRESS_SOURCE="$2";
+				shift 2;
+			fi
 		;;
 
 		--requested-address)
-			REQUESTED_ADDRESS="$2";
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+			#not enough argynents
+			if [ $# -lt 2 ]; then
+
+			#the value is empty
+			elif [ "$2" = "" ] || [ "$(echo $2 | grep -G '^-')" != "" ]; then
+
+			else
+				REQUESTED_ADDRESS="$2";
+				shift 2;
+			fi
 		;;
 
 		--requested-network)
-			REQUESTED_NETWORK="$2";
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+			#not enough argynents
+			if [ $# -lt 2 ]; then
+
+			#the value is empty
+			elif [ "$2" = "" ] || [ "$(echo $2 | grep -G '^-')" != "" ]; then
+
+			else
+				REQUESTED_NETWORK="$2";
+				shift 2;
+			fi
 		;;
 		"") break; ;;
-		*) usage; ;;
+		*) printf "Unrecognised argument - "; print_usage_then_exit; ;;
 	esac
 done
 
@@ -73,92 +97,75 @@ if [ -z "$MAC_ADDRESS_SOURCE" ] && [ -z "$REQUESTED_ADDRESS" ] && [ -z "$REQUEST
 fi
 
 if [ -n "$MAC_ADDRESS_SOURCE" ]; then
-	IS_SOURCE_MAC_VALID=$($DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS --address "$MAC_ADDRESS_SOURCE");
-
-	case $IS_SOURCE_MAC_VALID in
-		"true" ) ;;
-		"false" )
-			echo "$0; source mac address is invalid." >&2;
-			exit 2;
-		;;
-		* )
-			echo "$0; script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS\" produced incorrect output" >&2;
-			exit 3;
-		;;
+	$DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS --address "$MAC_ADDRESS_SOURCE";
+	case $? in
+		0) ;;
+		1) echo "$0; source mac address is invalid." >&2; exit 2; ;;
+		*) echo "$0; script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS\" produced incorrect output" >&2; exit 3; ;;
 	esac
 
-	IS_SOURCE_MAC_BANNED=$($DEPENDENCY_SCRIPT_PATH_IS_MAC_ADDRESS_BANNED_AS_SOURCE --address "$MAC_ADDRESS_SOURCE");
-
-	case $IS_SOURCE_MAC_BANNED in
-		"true" )
-			echo "$0; source mac address is not permitted." >&2;
-			exit 2;
-		;;
-		"false" ) ;;
-		* )
-			echo "$0; script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_IS_MAC_ADDRESS_BANNED_AS_SOURCE\" produced incorrect output" >&2;
-			exit 3;
-		;;
+	$DEPENDENCY_SCRIPT_PATH_IS_MAC_ADDRESS_BANNED_AS_SOURCE --address "$MAC_ADDRESS_SOURCE"
+	case $? in
+		1) ;;
+		0) echo "$0; source mac address is not permitted." >&2; exit 2; ;;
+		*) echo "$0; script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_IS_MAC_ADDRESS_BANNED_AS_SOURCE\" produced incorrect output" >&2; exit 3; ;;
 	esac
 fi
 
 TO_PROBE="":
 
 if [ -n "$REQUESTED_ADDRESS" ]; then
-	IS_REQUESTED_ADDRESS_VALID=$($DEPENDENCY_SCRIPT_PATH_VALIDATE_IPV4_ADDRESS --address "$REQUESTED_ADDRESS");
-
-	case "$IS_REQUESTED_ADDRESS_VALID" in
-		"true") ;;
-		"false")
-			echo "$0: the ipv4 address you supplied was invalid. Try: X.X.X.X where X is a number from 0-255">&2;
-			exit 2;
-		;;
-		*)
-			echo "$0; script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_VALIDATE_IPV4_ADDRESS\" produced incorrect output.">&2;
-			exit 3;
-		;;
+	$DEPENDENCY_SCRIPT_PATH_VALIDATE_IPV4_ADDRESS --address "$REQUESTED_ADDRESS"
+	case $? in
+		0) ;;
+		1) echo "$0: the ipv4 address you supplied was invalid. Try: X.X.X.X where X is a number from 0-255">&2; exit 2; ;;
+		*) echo "$0; script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_VALIDATE_IPV4_ADDRESS\" produced incorrect output.">&2; exit 3; ;;
 	esac
 
 	TO_PROBE="$REQUESTED_ADDRESS";
 fi
 
 if [ -n "$REQUESTED_NETWORK" ]; then
-	IS_REQUESTED_NETWORK_VALID=$($DEPENDENCY_SCRIPT_PATH_VALIDATE_IPV4_NETWORK --address "$REQUESTED_NETWORK");
-
-	case "$IS_REQUESTED_NETWORK_VALID" in
-		"true") ;;
-		"false")
-			echo "$0: the ipv4 network you supplied was not an address or network in CIDR form.">&2;
-			exit 2;
-		;;
-		*)
-			echo "$0; script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_VALIDATE_IPV4_NETWORK\" produced incorrect output.">&2;
-			exit 3;
-		;;
+	$DEPENDENCY_SCRIPT_PATH_VALIDATE_IPV4_NETWORK --address "$REQUESTED_NETWORK"
+	case $? in
+		0) ;;
+		1) echo "$0: the ipv4 network you supplied was not an address or network in CIDR form.">&2; exit 2; ;;
+		*) echo "$0; script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_VALIDATE_IPV4_NETWORK\" produced incorrect output.">&2; exit 3; ;;
 	esac
 
 	TO_PROBE="$REQUESTED_NETWORK";
 fi
 
+echo "\t#Hardware Type (1 = Ethernet)";
 echo "\t\tarp htype 1 \\";
+
+echo "\t#Hardware Length (6 = MAC address segment count)";
 echo "\t\tarp hlen 6 \\";
 
+echo "\t#Protocol Type (0x0800 = IPV4 ethertype)";
 echo "\t\tarp ptype 0x0800 \\";
+
+echo "\t#Protocol Length (4 = IPV4 address segment count)";
 echo "\t\tarp plen 4 \\";
 
+echo "\t#ARP OP Code (1 = request)";
 echo "\t\tarp operation 1 \\";
 
+echo "\t#Source and Destination MAC address - who is informing their peers";
 if [ -n "$MAC_ADDRESS_SOURCE" ]; then
 	echo "\t\tarp saddr ether $MAC_ADDRESS_SOURCE \\";
 	echo "\t\tarp daddr ether $MAC_ADDRESS_SOURCE \\";
 else
 	echo "\t\t#arp saddr ether unknown - please consider the security implications";
+	echo "\t\t#arp daddr ether unknown - please consider the security implications";
 fi
 
+echo "\t#Source and Destination IP address - who is informing their peers";
 if [ -n "$TO_PROBE" ]; then
 	echo "\t\tarp saddr ip $TO_PROBE \\";
 	echo "\t\tarp daddr ip $TO_PROBE \\";
 else
+	echo "\t\t#arp saddr ip unknown - please consider the security implications";
 	echo "\t\t#arp daddr ip unknown - please consider the security implications";
 fi
 
