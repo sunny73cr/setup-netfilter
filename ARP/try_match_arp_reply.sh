139de8664b435e0ccdb3a35ed72aa3df826be6a6
Dylan <sunny73cr@protonmail.com>
2024-09-19 00:07:41 +1200

    modify: try_match_arp_reply.sh - cleanup: improve arg parsing, use return codes instead of stdout in validation, comment output

--- ARP/try_match_arp_reply.sh
+++ ARP/try_match_arp_reply.sh
@@ -21,27 +21,25 @@ if [ ! -x "$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_NETWORK_IS_VALID" ]; then
 	exit 3;
 fi
 
-usage () {
+print_usage_then_exit () {
 	echo "Usage: $0 <arguments>">&2;
-	echo "Optional: --source-address-mac <XX:XX:XX:XX:XX:XX eg. 02:00:00:00:00:01>">&2;
-	echo "Optional: --destination-address-mac <XX:XX:XX:XX:XX:XX eg. 02:00:00:00:00:02>">&2;
+	echo "Optional: --source-address-mac XX:XX:XX:XX:XX:XX (where X is a-f, or A-F, or 0-9; hexadecimal.)">&2;
+	echo "Optional: --destination-address-mac XX:XX:XX:XX:XX:XX (where X is a-f, or A-F, or 0-9; hexadecimal.)">&2;
 	echo "Note: It is strongly recommended to supply a source or destination MAC Address">&2;
 	echo "">&2;
-
-	echo "Optional: --source-address-ipv4 <X.X.X.X eg. 10.0.0.1>">&2;
-	echo "Optional: --source-network-ipv4 <X.X.X.X/X eg. 10.0.0.0/8>">&2;
+	echo "Optional: --source-address-ipv4 X.X.X.X (where X is 0-255)">&2;
+	echo "Optional: --source-network-ipv4 X.X.X.X/Y (where X is 0-255, and Y is 1-32)">&2;
 	echo "Note: It is strongly reccomended to supply a source ipv4 address or network.">&2;
 	echo "">&2;
-
-	echo "Optional: --destination-address-ipv4 <X.X.X.X eg. 10.0.0.1>">&2;
-	echo "Optional: --destination-network-ipv4 <X.X.X.X/X eg. 10.0.0.0/8>">&2;;
+	echo "Optional: --destination-address-ipv4 X.X.X.X (where X is 0-255)">&2;
+	echo "Optional: --destination-network-ipv4 X.X.X.X/Y (where X is 0-255, and Y is 1-32)">&2;;
 	echo "Note: It is strongly reccomended to supply a destination ipv4 address or network.">&2;
 	exit 2;
 }
 
-if [ "$1" = "" ]; then usage; fi
+if [ "$1" = "" ]; then print_usage_then_exit; fi
 
-SOURCE_ADDRESS_MAC="";
+SOURCE_MAC_ADDRESS="";
 DESTINATION_MAC_ADDRESS="";
 
 SOURCE_IPV4_ADDRESS="";
@@ -53,150 +51,166 @@ DESTINATION_IPV4_NETWORK="";
 while true; do
 	case "$1" in
 		--source-address-mac)
-			SOURCE_ADDRESS_MAC="$2";
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+			#not enough arguments
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			#the value is empty
+			elif [ "$2" = "" ] || [ "$(echo $2 | grep -G '^-')" != "" ]; then
+				print_usage_then_exit;
+			else
+				SOURCE_MAC_ADDRESS="$2";
+				shift 2;
+			fi
 		;;
 		--destination-address-mac)
-			DESTINATION_ADDRESS_MAC="$2";
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+			#not enough arguments
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			#the value is empty
+			elif [ "$2" = "" ] || [ "$(echo $2 | grep -G '^-')" != "" ]; then
+				print_usage_then_exit;
+			else
+				DESTINATION_MAC_ADDRESS="$2";
+				shift 2;
+			fi
 		;;
 		--source-address-ipv4)
-			SOURCE_ADDRESS_IPV4="$2";
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+			#not enough arguments
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			#the value is empty
+			elif [ "$2" = "" ] || [ "$(echo $2 | grep -G '^-')" != "" ]; then
+				print_usage_then_exit;
+			else
+				SOURCE_ADDRESS_IPV4="$2";
+				shift 2;
+			fi
 		;;
 		--source-network-ipv4)
-			SOURCE_NETWORK_IPV4="$2";
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+			#not enough arguments
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			#the value is empty
+			elif [ "$2" = "" ] || [ "$(echo $2 | grep -G '^-')" != "" ]; then
+				print_usage_then_exit;
+			else
+				SOURCE_NETWORK_IPV4="$2";
+				shift 2;
+			fi
 		;;
 		--destination-address-ipv4)
-			DESTINATION_ADDRESS_IPV4="$2";
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+			#not enough arguments
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			#the value is empty
+			elif [ "$2" = "" ] || [ "$(echo $2 | grep -G '^-')" != "" ]; then
+				print_usage_then_exit;
+			else
+				DESTINATION_ADDRESS_IPV4="$2";
+				shift 2;
+			fi
 		;;
 		--destination-network-ipv4)
-			DESTINATION_NETWORK_IPV4="$2";
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+			#not enough arguments
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			#the value is empty
+			elif [ "$2" = "" ] || [ "$(echo $2 | grep -G '^-')" != "" ]; then
+				print_usage_then_exit;
+			else
+				DESTINATION_NETWORK_IPV4="$2";
+				shift 2;
+			fi
 		;;
 		"") break; ;;
-		*) usage; ;;
+		*) echo "Unrecognised argument - ">&2; print_usage_then_exit; ;;
 	esac
 done
 
-if [ -n "$SOURCE_ADDRESS_MAC" ]; then
-	IS_SOURCE_MAC_VALID=$($DEPENDENCY_SCRIPT_PATH_CHECK_MAC_ADDRESS_IS_VALID --address "$SOURCE_ADDRESS_MAC");
-
-	case "$IS_SOURCE_MAC_VALID" in
-		"true") ;;
-		"false")
-			echo "$0; the source mac address is invalid.">&2;
-			exit 2;
-		;;
-		*)
-			echo "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CHECK_MAC_ADDRESS_IS_VALID\" produced incorrect output.">&2;
-			exit 3;
-		;;
+if [ -n "$MAC_ADDRESS_SOURCE" ]; then
+	$DEPENDENCY_SCRIPT_PATH_CHECK_MAC_ADDRESS_IS_VALID --address "$MAC_ADDRESS_SOURCE";
+	case $? in
+		0) ;;
+		1) echo "$0; the source mac address is invalid.">&2; exit 2; ;;
+		*) echo "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CHECK_MAC_ADDRESS_IS_VALID\" produced incorrect output.">&2; exit 3; ;;
 	esac
 fi
 
 if [ -n "$DESTINATION_ADDRESS_MAC" ]; then
-	IS_DESTINATION_MAC_VALID=$($DEPENDENCY_SCRIPT_PATH_CHECK_MAC_ADDRESS_IS_VALID --address "$DESTINATION_ADDRESS_MAC");
-
-	case "$IS_DESTINATION_MAC_VALID" in
-		"true") ;;
-		"false")
-			echo "$0; the destination mac address is invalid.">&2;
-			exit 2;
-		;;
-		*)
-			echo "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CHECK_MAC_ADDRESS_IS_VALID\" produced incorrect output.">&2;
-			exit 3;
-		;;
+	$DEPENDENCY_SCRIPT_PATH_CHECK_MAC_ADDRESS_IS_VALID --address "$DESTINATION_ADDRESS_MAC";
+	case $? in
+		0) ;;
+		1) echo "$0; the destination mac address is invalid.">&2; exit 2; ;;
+		*) echo "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CHECK_MAC_ADDRESS_IS_VALID\" produced incorrect output.">&2; exit 3; ;;
 	esac
 fi
 
 if [ -n "$SOURCE_ADDRESS_IPV4" ]; then
-	IS_SOURCE_ADDRESS_IPV4_VALID=$($DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_ADDRESS_IS_VALID --address "$SOURCE_ADDRESS_IPV4");
-
-	case "$IS_SOURCE_ADDRESS_IPV4_VALID" in
-		"true") ;;
-		"false")
-			echo "$0; source ipv4 address is invalid.">&2;
-			exit 2;
-		;;
-		*)
-			echo "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_ADDRESS_IS_VALID\" produced incorrect output.">&2;
-			exit 3;
-		;;
+	$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_ADDRESS_IS_VALID --address "$SOURCE_ADDRESS_IPV4";
+	case $? in
+		0) ;;
+		1) echo "$0; source ipv4 address is invalid.">&2; exit 2; ;;
+		*) echo "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_ADDRESS_IS_VALID\" produced incorrect output.">&2; exit 3; ;;
 	esac
 fi
 
 if [ -n "$SOURCE_NETWORK_IPV4" ]; then
-	IS_SOURCE_NETWORK_IPV4_VALID=$($DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_NETWORK_IS_VALID --network "$SOURCE_NETWORK_IPV4");
-
-	case "$IS_SOURCE_NETWORK_IPV4_VALID" in
-		"true") ;;
-		"false")
-			echo "$0; source ipv4 NETWORK is invalid.">&2;
-			exit 2;
-		;;
-		*)
-			echo "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_NETWORK_IS_VALID\" produced incorrect output.">&2;
-			exit 3;
-		;;
+	$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_NETWORK_IS_VALID --network "$SOURCE_NETWORK_IPV4";
+	case $? in
+		0) ;;
+		1) echo "$0; source ipv4 NETWORK is invalid.">&2; exit 2; ;;
+		*) echo "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_NETWORK_IS_VALID\" produced incorrect output.">&2; exit 3; ;;
 	esac
 fi
 
 if [ -n "$DESTINATION_ADDRESS_IPV4" ]; then
-	IS_DESTINATION_ADDRESS_IPV4_VALID=$($DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_ADDRESS_IS_VALID --address "$DESTINATION_ADDRESS_IPV4");
-
-	case "$IS_DESTINATION_ADDRESS_IPV4_VALID" in
-		"true") ;;
-		"false")
-			echo "$0; destination ipv4 address is invalid.">&2;
-			exit 2;
-		;;
-		*)
-			echo "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_ADDRESS_IS_VALID\" produced incorrect output.">&2;
-			exit 3;
-		;;
+	$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_ADDRESS_IS_VALID --address "$DESTINATION_ADDRESS_IPV4";
+	case $? in
+		0) ;;
+		1) echo "$0; destination ipv4 address is invalid.">&2; exit 2; ;;
+		*) echo "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_ADDRESS_IS_VALID\" produced incorrect output.">&2; exit 3; ;;
 	esac
 fi
 
 if [ -n "$DESTINATION_NETWORK_IPV4" ]; then
-	IS_DESTINATION_NETWORK_IPV4_VALID=$($DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_NETWORK_IS_VALID --network "$DESTINATION_NETWORK_IPV4");
-
-	case "$IS_DESTINATION_NETWORK_IPV4_VALID" in
-		"true") ;;
-		"false")
-			echo "$0; destination ipv4 network is invalid.">&2;
-			exit 2;
-		;;
-		*)
-			echo "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_NETWORK_IS_VALID\" produced incorrect output.">&2;
-			exit 3;
-		;;
+	$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_NETWORK_IS_VALID --network "$DESTINATION_NETWORK_IPV4";
+	case $? in
+		0) ;;
+		1) echo "$0; destination ipv4 network is invalid.">&2; exit 2; ;;
+		*) echo "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_NETWORK_IS_VALID\" produced incorrect output.">&2; exit 3; ;;
 	esac
 fi
 
+echo "\t#Hardware Ethernet (1 = Ethernet)"
 echo "\t\tarp htype 1 \\";
+
+echo "\t#Hardware Length (6 = MAC address segment count)"
 echo "\t\tarp hlen 6 \\";
 
+echo "\t#Protocol Type (0x0800 = IPV4 ethertype)";
 echo "\t\tarp ptype 0x0800 \\";
+
+echo "\t#Protocol Length (4 = IPV4 address segment length)";
 echo "\t\tarp plen 4 \\";
 
+echo "\t#ARP OP Code (2 = reply)";
 echo "\t\tarp operation 2 \\";
 
-if [ -n "$SOURCE_ADDRESS_MAC" ]; then
-	echo "arp saddr ether $SOURCE_ADDRESS_MAC \\";
+echo "\t#Source MAC address - who is replying to a probe.";
+if [ -n "$SOURCE_MAC_ADDRESS" ]; then
+	echo "arp saddr ether $MAC_ADDRESS_SOURCE \\";
 else
 	echo "\t\t#arp saddr ether unknown - please consider the security implications";
 fi
 
+echo "\t#Destination MAC address - who is replying to a probe.";
 if [ -n "$DESTINATION_ADDRESS_MAC" ]; then
 	echo "arp daddr ether $DESTINATION_ADDRESS_MAC \\";
 else
 	echo "\t\t#arp daddr ether unknown - please consider the security implications";
 fi
 
+echo "\t#ARP source ip address - who is replying to a probe.";
 if [ -n "$SOURCE_ADDRESS_IPV4" ]; then
 	echo "\t\tarp saddr ip $SOURCE_ADDRESS_IPV4 \\":
 
@@ -207,6 +221,7 @@ else
 	echo "\t\t#arp saddr ip unknown - please consider the security implications";
 fi
 
+echo "\t#ARP destination ip address - who is being informed of the IP's owner (hardware address)";
 if [ -n "$DESTINATION_ADDRESS_IPV4" ]; then
 	echo "\t\tarp daddr ip $DESTINATION_ADDRESS_IPV4 \\":
 
