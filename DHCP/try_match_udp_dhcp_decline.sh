f58a65f5d2b69789f5f3544a8a4fbbd5f23d29c9
Dylan <sunny73cr@protonmail.com>
2024-09-19 01:40:10 +1200

    modify: try_match_udp_dhcp_decline.sh - cleanup: improve arg parsing, use return codes instead of stdout in validation, comment output

--- DHCP/try_match_udp_dhcp_decline.sh
+++ DHCP/try_match_udp_dhcp_decline.sh
@@ -46,13 +46,6 @@ print_usage_and_exit() {
 
 if [ "$1" = "" ]; then print_usage_and_exit; fi
 
-confirm_exit_code_is_zero() {
-	if [ "$?" -ne 0 ]; then
-		echo "$0: cannot try to match udp dhcp client declining an offer">&2;
-		exit 2;
-	fi
-}
-
 SERVER_ADDRESS="";
 REQUESTED_ADDRESS="";
 CLIENT_MAC_ADDRESS="";
@@ -65,7 +58,7 @@ while true; do
 			if [ $# -lt 2 ]; then
 				print_usage_and_exit;
 			#if no value, or another argument follows
-			elif [ "$2" = "" ] || [ "$(echo "$2" | grep -P '^-')" != "" ]; then
+			elif [ "$2" = "" ] || [ "$(echo "$2" | grep -G '^-')" != "" ]; then
 				print_usage_and_exit;
 			else
 				SERVER_ADDRESS=$2;
@@ -77,7 +70,7 @@ while true; do
 				#not enough argyments
 				print_usage_and_exit;
 			#if no value, or another argument follows
-			elif [ "$2" = "" ] || [ "$(echo "$2" | grep -P '^-')" != "" ]; then
+			elif [ "$2" = "" ] || [ "$(echo "$2" | grep -G '^-')" != "" ]; then
 				#if no value
 				print_usage_and_exit;
 			else
@@ -90,7 +83,7 @@ while true; do
 				#not enough argyments
 				print_usage_and_exit;
 			#if no value, or another argument follows
-			elif [ "$2" = "" ] || [ "$(echo "$2" | grep -P '^-')" != "" ]; then
+			elif [ "$2" = "" ] || [ "$(echo "$2" | grep -G '^-')" != "" ]; then
 				#if no value
 				print_usage_and_exit;
 			else
@@ -102,7 +95,7 @@ while true; do
 			if [ $# -lt 2 ]; then
 				#not enough argyments
 				print_usage_and_exit;
-			elif [ "$2" = "" ] || [ "$(echo "$2" | grep -P '^-')" != "" ]; then
+			elif [ "$2" = "" ] || [ "$(echo "$2" | grep -G '^-')" != "" ]; then
 				#if no value
 				print_usage_and_exit;
 			else
@@ -112,31 +105,45 @@ while true; do
 
 		;;
 		"") break; ;;
-		*)
-			echo "$0: unrecognised argument: $1 $2">&2:
-			exit 2;
+		*) printf "Unrecognised argument - ">&2; print_usage_then_exit;
 		;;
 	esac
 done
 
 if [ -n "$SERVER_ADDRESS" ]; then
-	IS_SERVER_ADDRESS_VALID=$($SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID --address $SERVER_ADDRESS);
-	confirm_exit_code_is_zero;
+	$SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID --address $SERVER_ADDRESS
+	case $? in
+		0) ;;
+		1) printf "$0: the server ip address is invalid.\n"; exit 2; ;;
+		*) printf "$0: script dependency failure: \"$SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID\" produced an error.\n"; exit 3 ;;
+	esac
 fi
 
 if [ -n "$REQUESTED_ADDRESS" ]; then
-	IS_REQUESTED_ADDRESS_VALID=$($SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID --address $REQUESTED_ADDRESS);
-	confirm_exit_code_is_zero;
+	$SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID --address $REQUESTED_ADDRESS
+	case $? in
+		0) ;;
+		1) printf "$0: the requested ip address is invalid.\n"; exit 2; ;;
+		*) printf "$0: script dependency failure: \"$SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID\" produced an error.\n"; exit 3 ;;
+	esac
 fi
 
 if [ -n "$CLIENT_MAC_ADDRESS" ]; then
-	IS_CLIENT_MAC_ADDRESS_VALID=$($SCRIPT_DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID --address $CLIENT_MAC_ADDRESS);
-	confirm_exit_code_is_zero;
+	$SCRIPT_DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID --address $CLIENT_MAC_ADDRESS
+	case $? in
+		0) ;;
+		1) printf "$0: the client mac address is invalid.\n"; exit 2; ;;
+		*) printf "$0: script dependency failure: \"$SCRIPT_DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID\" produced an error.\n"; exit 3 ;;
+	esac
 fi
 
 if [ -n "$SERVICE_USER_ID" ]; then
-	IS_SERVICE_USER_ID_VALID=$($SCRIPT_DEPENDENCY_PATH_CHECK_SERVICE_USER_ID_IS_VALID --id $SERVICE_USER_ID);
-	confirm_exit_code_is_zero;
+	$SCRIPT_DEPENDENCY_PATH_CHECK_SERVICE_USER_ID_IS_VALID --id $SERVICE_USER_ID
+	case $? in
+		0) ;;
+		1) printf "$0: the service user id is invalid. (confirm the /etc/passwd entry)\n"; exit 2; ;;
+		*) printf "$0: script dependency failure: \"$SCRIPT_DEPENDENCY_PATH_CHECK_SERVICE_USER_ID_IS_VALID\" produced an error.\n"; exit 3 ;;
+	esac
 fi
 
 echo "\t#DHCP DECLINE message length is most likely to be 2304 bytes, or 288 octets"
@@ -150,57 +157,57 @@ else
 fi
 
 echo "\t#DHCP OP Code of 1 (BOOTREQUEST)";
-echo "\t\t@ih,0,8 0x01 \\\\";
+echo "\t\t@ih,0,8 0x01 \\";
 
 echo "\t#HTYPE (Hardware Address Type) (1 Ethernet)";
-echo "\t\t@ih,8,8 1 \\\\";
+echo "\t\t@ih,8,8 1 \\";
 
 echo "\t#HLEN (Hardware Address Length) (6 Segment MAC)";
-echo "\t\t@ih,16,8 6 \\\\";
+echo "\t\t@ih,16,8 6 \\";
 
 echo "\t#HOPS (Client sets to 0, optionally set by relay-agents)";
-echo "\t\t@ih,24,8 0 \\\\";
+echo "\t\t@ih,24,8 0 \\";
 
 echo "\t#XID (Transaction ID) client generated random number to associate communications";
-echo "\t\t@ih,32,32 != 0 \\\\";
+echo "\t\t@ih,32,32 != 0 \\";
 
 echo "\t#SECS (Seconds since the request was made)";
 echo "\t#See RFC1541 - DHCP DECLINE should be 0 sec";
-echo "\t\t@ih,64,16 0 \\\\";
+echo "\t\t@ih,64,16 0 \\";
 
 echo "\t#Flags: no flags for DHCP DECLINE, 16 zeroes, DHCPDECLINE is not broadcasted (See RFC1541)";
-echo "\t\t@ih,80,16 0 \\\\";
+echo "\t\t@ih,80,16 0 \\";
 
 echo "\t#CIADDR (Client IP Address)";
 echo "\t#In this case, the address being declined.";
 if [ -n "$REQUESTED_ADDRESS" ]; then
-	echo "\t\t@ih,96,8 $(echo $REQUESTED_ADDRESS | cut -d '.' -f 1) \\\\";
-	echo "\t\t@ih,104,8 $(echo $REQUESTED_ADDRESS | cut -d '.' -f 2) \\\\";
-	echo "\t\t@ih,112,8 $(echo $REQUESTED_ADDRESS | cut -d '.' -f 3) \\\\";
-	echo "\t\t@ih,120,8 $(echo $REQUESTED_ADDRESS | cut -d '.' -f 4) \\\\";
+	echo "\t\t@ih,96,8 $(echo $REQUESTED_ADDRESS | cut -d '.' -f 1) \\";
+	echo "\t\t@ih,104,8 $(echo $REQUESTED_ADDRESS | cut -d '.' -f 2) \\";
+	echo "\t\t@ih,112,8 $(echo $REQUESTED_ADDRESS | cut -d '.' -f 3) \\";
+	echo "\t\t@ih,120,8 $(echo $REQUESTED_ADDRESS | cut -d '.' -f 4) \\";
 else
 	echo "\t\t#@ih,96,32 unrestricted - please consider the security implications";
 fi
 
 echo "\t#YIADDR (Your IP address) Your (client) IP address";
-echo "\t\t@ih,128,32 0 \\\\";
+echo "\t\t@ih,128,32 0 \\";
 
 echo "\t#SIADDR (Server IP address) Returned in DHCPOFFER, DHCPACK, DHCPNAK";
-echo "\t\t@ih,160,32 0 \\\\";
+echo "\t\t@ih,160,32 0 \\";
 
 echo "\t#GIADDR (Relay Agent IP address)";
-echo "\t\t@ih,192,32 0 \\\\";
+echo "\t\t@ih,192,32 0 \\";
 
 echo "\t#CHADDR (Client Hardware Address)";
 #echo "\t\t@ih,224,64 0 \\";
 echo "\t#Confirm each segment of the MAC address matches";
 if [ -n "$CLIENT_MAC_ADDRESS" ]; then
-	echo "\t\t@ih,224,8 0x$(echo $CLIENT_MAC_ADDRESS | cut -d ':' -f 1) \\\\";
-	echo "\t\t@ih,232,8 0x$(echo $CLIENT_MAC_ADDRESS | cut -d ':' -f 2) \\\\";
-	echo "\t\t@ih,240,8 0x$(echo $CLIENT_MAC_ADDRESS | cut -d ':' -f 3) \\\\";
-	echo "\t\t@ih,248,8 0x$(echo $CLIENT_MAC_ADDRESS | cut -d ':' -f 4) \\\\";
-	echo "\t\t@ih,256,8 0x$(echo $CLIENT_MAC_ADDRESS | cut -d ':' -f 5) \\\\";
-	echo "\t\t@ih,264,8 0x$(echo $CLIENT_MAC_ADDRESS | cut -d ':' -f 6) \\\\";
+	echo "\t\t@ih,224,8 0x$(echo $CLIENT_MAC_ADDRESS | cut -d ':' -f 1) \\";
+	echo "\t\t@ih,232,8 0x$(echo $CLIENT_MAC_ADDRESS | cut -d ':' -f 2) \\";
+	echo "\t\t@ih,240,8 0x$(echo $CLIENT_MAC_ADDRESS | cut -d ':' -f 3) \\";
+	echo "\t\t@ih,248,8 0x$(echo $CLIENT_MAC_ADDRESS | cut -d ':' -f 4) \\";
+	echo "\t\t@ih,256,8 0x$(echo $CLIENT_MAC_ADDRESS | cut -d ':' -f 5) \\";
+	echo "\t\t@ih,264,8 0x$(echo $CLIENT_MAC_ADDRESS | cut -d ':' -f 6) \\";
 else
 	echo "\t\t#@ih,224,64 unrestricted - please consider the security implications"
 fi
