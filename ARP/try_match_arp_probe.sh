b40b9eaa7a764fa3ed708e0b4402dbfc6a8cba13
Dylan <sunny73cr@protonmail.com>
2024-09-19 00:07:23 +1200

    modify: try_match_arp_probe.sh - cleanup: improve arg parsing, use return codes instead of stdout in validation, comment output

--- ARP/try_match_arp_probe.sh
+++ ARP/try_match_arp_probe.sh
@@ -27,20 +27,26 @@ if [ ! -x "$DEPENDENCY_SCRIPT_PATH_VALIDATE_IPV4_NETWORK" ]; then
 	exit 3;
 fi
 
-usage () {
+print_usage_then_exit () {
 	echo "Usage: $0 <arguments>">&2;
-	echo "ARGUMENTS:">&2;
-	echo "optional: --source-mac-address <string>">&2;
+	echo "Use case: a network client needs to check if an IP address is reserved by a peer.">&2;
+	echo "">&2;
+	echo "--source-mac-address XX:XX:XX:XX:XX:XX (where X is a-f, A-F, 0-9, or hexadecimal)">&2;
+	echo "Options:">&2;
+	echo "1. the mac address of the probing device">&2;
+	echo "2. no restrictions on source address."
 	echo "Note: it is strongly recommended to supply a source MAC address.">&2;
 	echo "">&2;
-	echo "optional: --probed-address-ipv4 <X.X.X.X eg. 10.0.0.1>">&2
-	echo "optional: --probed-network-ipv4 <X.X.X.X/X eg. 10.0.0.0/8 or 10.0.0.1/32>" 1>&2;
+	echo "--probed-address">&2
+	echo "Options:">&2
+	echo "--probed-address-ipv4 X.X.X.X (where X is 0-255)">&2
+	echo "--probed-network-ipv4 X.X.X.X/Y (where X is 0-255, and Y is 1-32)" 1>&2;
 	echo "Note: it is strongly recommended to supply either an address or a network.">&2;
 	echo "Note: you cannot supply both an address and a network.">&2;
 	exit 2;
 }
 
-if [ "$1" = "" ]; then usage; fi
+if [ "$1" = "" ]; then print_usage_then_exit; fi
 
 MAC_ADDRESS_SOURCE="";
 PROBED_ADDRESS="";
@@ -49,110 +55,117 @@ PROBED_NETWORK="";
 while true; do
 	case "$1" in
 		--source-mac-address)
-			MAC_ADDRESS_SOURCE="$2";
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+			#not enough arguments
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			#the value is empty
+			elif [ "$2" = "" ] || [ "$(echo $2 | grep -G '^-')" != "" ]; then
+				print_usage_then_exit;
+			else
+				MAC_ADDRESS_SOURCE=$2;
+				shift 2;
+			fi
 		;;
 
 		--probed-address)
-			PROBED_ADDRESS="$2";
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+			#not enough arguments
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			#the value is empty
+			elif [ "$2" = "" ] || [ "$(echo $2 | grep -G '^-')" != "" ]; then
+				print_usage_then_exit;
+			else
+				PROBED_ADDRESS=$2;
+				shift 2;
+			fi
 		;;
 
 		--probed-network)
-			PROBED_NETWORK="$2";
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+			#not enough arguments
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			#the value is empty
+			elif [ "$2" = "" ] || [ "$(echo $2 | grep -G '^-')" != "" ]; then
+				print_usage_then_exit;
+			else
+				PROBED_NETWORK=$2;
+				shift 2;
+			fi
 		;;
 		"") break; ;;
-		*) usage; ;;
+		*) printf "Unrecognised argument - "; print_usage_then_exit; ;;
 	esac
 done
 
 if [ -n "$MAC_ADDRESS_SOURCE" ]; then
-	IS_SOURCE_MAC_VALID=$($DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS --address "$MAC_ADDRESS_SOURCE");
-
-	case $IS_SOURCE_MAC_VALID in
-		"true" ) ;;
-		"false" )
-			echo "$0; source mac address is invalid." >&2;
-			exit 2;
-		;;
-		* )
-			echo "$0; script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS\" produced incorrect output" >&2;
-			exit 3;
-		;;
+	$DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS --address "$MAC_ADDRESS_SOURCE";
+	case $? in
+		0) ;;
+		1) echo "$0; source mac address is invalid." >&2;						exit 2; ;;
+		*) echo "$0; script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS\"" >&2;  exit 3; ;;
 	esac
 
-	IS_SOURCE_MAC_BANNED=$($DEPENDENCY_SCRIPT_PATH_IS_MAC_ADDRESS_BANNED_AS_SOURCE --address "$MAC_ADDRESS_SOURCE");
-
-	case $IS_SOURCE_MAC_BANNED in
-		"true" )
-			echo "$0; source mac address is not permitted." >&2;
-			exit 2;
-		;;
-		"false" ) ;;
-		* )
-			echo "$0; script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_IS_MAC_ADDRESS_BANNED_AS_SOURCE\" produced incorrect output" >&2;
-			exit 3;
-		;;
+	$DEPENDENCY_SCRIPT_PATH_IS_MAC_ADDRESS_BANNED_AS_SOURCE --address "$MAC_ADDRESS_SOURCE"
+	case $? in
+		1) ;;
+		0) echo "$0; source mac address is not permitted." >&2; 									    exit 2; ;;
+		*) echo "$0; script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_IS_MAC_ADDRESS_BANNED_AS_SOURCE\" produced incorrect output" >&2; exit 3; ;;
 	esac
 fi
 
 TO_PROBE="":
 
 if [ -n "$PROBED_ADDRESS" ]; then
-	IS_PROBED_ADDRESS_VALID=$($DEPENDENCY_SCRIPT_PATH_VALIDATE_IPV4_ADDRESS --address "$PROBED_ADDRESS");
-
-	case "$IS_PROBED_ADDRESS_VALID" in
-		"true") ;;
-		"false")
-			echo "$0: the ipv4 address you supplied was invalid. Try: X.X.X.X where X is a number from 0-255">&2;
-			exit 2;
-		;;
-		*)
-			echo "$0; script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_VALIDATE_IPV4_ADDRESS\" produced incorrect output.">&2;
-			exit 3;
-		;;
+	$DEPENDENCY_SCRIPT_PATH_VALIDATE_IPV4_ADDRESS --address "$PROBED_ADDRESS"
+	case $? in
+		0) ;;
+		1) echo "$0: the ipv4 address you supplied was invalid. Try: X.X.X.X where X is a number from 0-255">&2; 		  exit 2; ;;
+		*) echo "$0; script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_VALIDATE_IPV4_ADDRESS\" produced incorrect output.">&2; exit 3; ;;
 	esac
 
 	TO_PROBE="$PROBED_ADDRESS";
 fi
 
 if [ -n "$PROBED_NETWORK" ]; then
-	IS_PROBED_NETWORK_VALID=$($DEPENDENCY_SCRIPT_PATH_VALIDATE_IPV4_NETWORK --address "$PROBED_NETWORK");
-
-	case "$IS_PROBED_NETWORK_VALID" in
-		"true") ;;
-		"false")
-			echo "$0: the ipv4 network you supplied was not an address or network in CIDR form.">&2;
-			exit 2;
-		;;
-		*)
-			echo "$0; script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_VALIDATE_IPV4_NETWORK\" produced incorrect output.">&2;
-			exit 3;
-		;;
+	$DEPENDENCY_SCRIPT_PATH_VALIDATE_IPV4_NETWORK --address "$PROBED_NETWORK"
+	case $? in
+		0) ;;
+		1) echo "$0: the ipv4 network you supplied was not an address or network in CIDR form.">&2; 				  exit 2; ;;
+		*) echo "$0; script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_VALIDATE_IPV4_NETWORK\" produced incorrect output.">&2; exit 3; ;;
 	esac
 
 	TO_PROBE="$PROBED_NETWORK";
 fi
 
+echo "\t#Hardware Type (1 = Ethernet)"
 echo "\t\tarp htype 1 \\";
+
+echo "\t#Hardware Length (6 = MAC segment count)"
 echo "\t\tarp hlen 6 \\";
 
+echo "\t#Protocol Type (0x0800 = 'IPV4' ethertype";
 echo "\t\tarp ptype 0x0800 \\";
+
+echo "\t#Protocol Length (4 = IPV4 segment count)"
 echo "\t\tarp plen 4 \\";
 
+echo "\t#Operation code (1 = request)"
 echo "\t\tarp operation 1 \\";
 
+echo "\t#ARP source MAC - who is asking";
 if [ -n "$MAC_ADDRESS_SOURCE" ]; then
 	echo "\t\tarp saddr ether $MAC_ADDRESS_SOURCE \\";
 else
 	echo "\t\t#arp saddr ether unknown - please consider the security implications";
 fi
 
+echo "\t#ARP destination mac - who to ask (unknown in a 'probe')";
 echo "\t\tarp daddr ether 00:00:00:00:00:00 \\";
 
+echo "\t#ARP source ip address - who is asking (not yet assigned in a 'probe')";
 echo "\t\tarp saddr ip 0.0.0.0 \\";
 
+echo "\t#ARP destination ip address - who to ask (for their mac address)";
 if [ -n "$TO_PROBE" ]; then
 	echo "\t\tarp daddr ip $TO_PROBE \\";
 else
