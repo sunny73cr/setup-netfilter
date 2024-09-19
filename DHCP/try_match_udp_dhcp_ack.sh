89763a5681915aca101406d44260a9ecd9b17dfb
Dylan <sunny73cr@protonmail.com>
2024-09-20 10:15:36 +1200

    modify: try_match_udp_dhcp_ack.sh - improve arg parsing, debug output and packet matching

--- DHCP/try_match_udp_dhcp_ack.sh
+++ DHCP/try_match_udp_dhcp_ack.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-if [ -z "$ENV_SETUP_NFT" ]; then echo "Set ENV_SETUP_NFT to the absolute path of the setup-netfilter directory first.">&2; exit 4; fi
+if [ -z "$ENV_SETUP_NFT" ]; then printf "setup-netfilter: Set ENV_SETUP_NFT to the absolute path of the setup-netfilter directory first.\n">&2; exit 4; fi
 
-SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID="$ENV_SETUP_NFT/SCRIPT_HELPERS/check_ipv4_address_is_valid.sh";
 SCRIPT_DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID="$ENV_SETUP_NFT/SCRIPT_HELPERS/check_mac_address_is_valid.sh";
+SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID="$ENV_SETUP_NFT/SCRIPT_HELPERS/check_ipv4_address_is_valid.sh";
 SCRIPT_DEPENDENCY_PATH_CHECK_SERVICE_USER_ID_IS_VALID="$ENV_SETUP_NFT/SCRIPT_HELPERS/check_service_id_is_valid.sh";
-
-if [ ! -x $SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID ]; then
-	echo "$0: script dependency failure: $SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID is missing or is not executable.">&2;
-	exit 3;
-fi
 
 if [ ! -x $SCRIPT_DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID ]; then
 	echo "$0: script dependency failure: $SCRIPT_DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID is missing or is not executable.">&2;
+	exit 3;
+fi
+
+if [ ! -x $SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID ]; then
+	echo "$0: script dependency failure: $SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID is missing or is not executable.">&2;
 	exit 3;
 fi
 
@@ -23,68 +23,39 @@ fi
 
 print_usage_and_exit() {
 	echo "Usage: $0 <arguments>">&2;
-	echo "Either:">&2;
-	echo "1. --server-address-ipv4 X.X.X.X (where X is 0-255)">&2;
-	echo "2. no restrictions on the server address.">&2;
-	echo "">&2;
-	echo "Either:">&2;
-	echo "1. --requested-address-ipv4 X.X.X.X (where X is 0-255)">&2;
-	echo "2. no restrictions on the client (requested) address.">&2;
-	echo "">&2;
-	echo "Note: it is strongly recommended to supply both a server and client address.">&2;
-	echo "">&2;
-	echo "--client-mac-address XX:XX:XX:XX:XX:XX (where X is a-f, or A-F, or 0-9 - hexadecimal)">&2;
+	echo "Optional: --client-mac-address XX:XX:XX:XX:XX:XX (where X is a-f, or A-F, or 0-9 - hexadecimal)">&2;
 	echo "Note: it is strongly recommended to supply the client mac address, if you know it.">&2;
 	echo "">&2;
-	echo "--dhcp-service-uid X (where X is 1-65535)">&2;
+	echo "Optional: --server-address-ipv4 X.X.X.X (where X is 0-255)">&2;
+	echo "">&2;
+	echo "Optional: --requested-address-ipv4 X.X.X.X (where X is 0-255)">&2;
+	echo "">&2
+	echo "Note: it is strongly recommended to supply both a server and client address.">&2;
+	echo "">&2;
+	echo "Optional: --dhcp-service-uid X (where X is 1-65535)">&2;
 	echo "Note: it is strongly recommended to supply a service 'socket' user id.">&2;
 	echo "Note: this relates to an entry in the /etc/passwd file. It is the user id of a designated DHCP client or server program.">&2;
-	echo "Note: without this restriction, DHCP ACK packets are permitted to any program."
+	echo "Note: without this restriction, DHCPACK packets are permitted to any program."
+	echo "">&2;
+	echo "Developer/Special Use Flags:">&2;
+	echo "--only-validate">&2;
+	echo "Don't print the generated output / exit after validating inputs.">&2;
 	echo "">&2;
 	exit 2;
 }
 
 if [ "$1" = "" ]; then print_usage_and_exit; fi
 
-confirm_exit_code_is_zero() {
-	if [ "$?" -ne 0 ]; then
-		echo "$0: cannot try to match udp dhcp server ackowledgement">&2;
-		exit 2;
-	fi
-}
-
-SERVER_ADDRESS="";
-REQUESTED_ADDRESS="";
 CLIENT_MAC_ADDRESS="";
+SERVER_IPV4_ADDRESS="";
+ALLOCATED_IPV4_ADDRESS="";
+CLIENT_IS_REUSING_IPV4_ADDRESS=0;
 SERVICE_USER_ID="";
+
+ONLY_VALIDATE=0;
 
 while true; do
 	case $1 in
-		--server-address-ipv4)
-			#not enough argyments
-			if [ $# -lt 2 ]; then
-				print_usage_and_exit;
-			#if no value, or another argument follows
-			elif [ "$2" = "" ] || [ "$(echo "$2" | grep -P '^-')" != "" ]; then
-				print_usage_and_exit;
-			else
-				SERVER_ADDRESS=$2;
-				shift 2;
-			fi
-		;;
-		--requested-address-ipv4)
-			if [ $# -lt 2 ]; then
-				#not enough argyments
-				print_usage_and_exit;
-			#if no value, or another argument follows
-			elif [ "$2" = "" ] || [ "$(echo "$2" | grep -P '^-')" != "" ]; then
-				#if no value
-				print_usage_and_exit;
-			else
-				REQUESTED_ADDRESS=$2;
-				shift 2;
-			fi
-		;;
 		--client-mac-address)
 			if [ $# -lt 2 ]; then
 				#not enough argyments
@@ -98,46 +69,108 @@ while true; do
 				shift 2;
 			fi
 		;;
+		--server-address-ipv4)
+			#not enough argyments
+			if [ $# -lt 2 ]; then
+				print_usage_and_exit;
+			#if no value, or another argument follows
+			elif [ "$2" = "" ] || [ "$(echo "$2" | grep -P '^-')" != "" ]; then
+				print_usage_and_exit;
+			else
+				SERVER_IPV4_ADDRESS=$2;
+				shift 2;
+			fi
+		;;
+		--client-is-reusing-ipv4-address)
+			CLIENT_IS_REUSING_IPV4_ADDRESS=1;
+			shift 1;
+		;;
+		--allocated-ipv4-address)
+			if [ $# -lt 2 ]; then
+				#not enough argyments
+				print_usage_and_exit;
+			#if no value, or another argument follows
+			elif [ "$2" = "" ] || [ "$(echo "$2" | grep -P '^-')" != "" ]; then
+				#if no value
+				print_usage_and_exit;
+			else
+				ALLOCATED_IPV4_ADDRESS=$2;
+				shift 2;
+			fi
+		;;
 		--dhcp-service-uid)
 			if [ $# -lt 2 ]; then
 				#not enough argyments
 				print_usage_and_exit;
 			elif [ "$2" = "" ] || [ "$(echo "$2" | grep -P '^-')" != "" ]; then
-				#if no value
+			#if no value
 				print_usage_and_exit;
 			else
-				SERVICE_ID=$2;
+				SERVICE_USER_ID=$2;
 				shift 2;
 			fi
 
 		;;
-		"") break; ;;
-		*)
-			echo "$0: unrecognised argument: $1 $2">&2:
-			exit 2;
+		--only-validate)
+			ONLY_VALIDATE=1;
+			shift 1;
 		;;
+		"") break; ;;
+		*) printf "Unrecognised argument - "; print_usage_then_exit; ;;
 	esac
 done
 
-if [ -n "$SERVER_ADDRESS" ]; then
-	IS_SERVER_ADDRESS_VALID=$($SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID --address $SERVER_ADDRESS);
-	confirm_exit_code_is_zero;
-fi
-
-if [ -n "$REQUESTED_ADDRESS" ]; then
-	IS_REQUESTED_ADDRESS_VALID=$($SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID --address $REQUESTED_ADDRESS);
-	confirm_exit_code_is_zero;
-fi
-
 if [ -n "$CLIENT_MAC_ADDRESS" ]; then
-	IS_CLIENT_MAC_ADDRESS_VALID=$($SCRIPT_DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID --address $CLIENT_MAC_ADDRESS);
-	confirm_exit_code_is_zero;
+	$SCRIPT_DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID --address $CLIENT_MAC_ADDRESS
+	case $? in
+		0) ;;
+		1) echo "$0: the client mac address is invalid. The address is in the form of XX:XX:XX:XX:XX:XX where X is a-f, or A-F, or 0-9: hexadecimal.">&2; exit 2; ;;
+		*) echo "$0: script dependency failure: \"$SCRIPT_DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID\" produced a failure exit code.">&2; exit 3; ;;
+	esac
+fi
+
+if [ -n "$SERVER_IPV4_ADDRESS" ]; then
+	$SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID --address $SERVER_IPV4_ADDRESS
+	case $? in
+		0) ;;
+		1) echo "$0: the server ip address is invalid. The address is in the form of X.X.X.X where X is 0-255.">&2; exit 2; ;;
+		*) echo "$0: script dependency failure: \"$SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID\" produced a failure exit code.">&2; exit 3; ;;
+	esac
+fi
+
+if [ $CLIENT_IS_REUSING_IPV4_ADDRESS -eq 0 ] && [ -n "$ALLOCATED_IPV4_ADDRESS" ]; then
+	echo "I cannot assume that you intend to allow a bound/renew/rebind DHCPACK; please retry with the flag --client-is-reusing-ipv4-address enabled.">&2;
+	echo "Alternatively, please omit the $ALLOCATED_IPV4_ADDRESS.">&2;
+	echo "Please refer to RFC2131 and RFC2132 for more information.">&2;
+	exit 2;
+fi
+
+if [ $CLIENT_IS_REUSING_IPV4_ADDRESS -eq 1 ] && [ -z "$ALLOCATED_IPV4_ADDRESS" ]; then
+	echo "If you intend to allow a bound/renew/rebind DHCPACK; please retry and supply an 'allocated' IPV4 address/network you allow the server to issue.">&2;
+	echo "Alternatively, please omit the --client-is-reusing-ipv4-address-enabled flag.">&2;
+	echo "Please refer to RFC2131 and RFC2132 for more information.">&2;
+	exit 2;
+fi
+
+if [ -n "$ALLOCATED_IPV4_ADDRESS" ]; then
+	$SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID --address $ALLOCATED_IPV4_ADDRESS;
+	case $? in
+		0) ;;
+		1) echo "$0: the allocated ip address is invalid. The address is in the form of X.X.X.X where X is 0-255.">&2; exit 2; ;;
+		*) echo "$0: script dependency failure: \"$SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID\" produced a failure exit code.">&2; exit 3; ;;
+	esac
 fi
 
 if [ -n "$SERVICE_USER_ID" ]; then
-	IS_SERVICE_USER_ID_VALID=$($SCRIPT_DEPENDENCY_PATH_CHECK_SERVICE_USER_ID_IS_VALID --id $SERVICE_USER_ID);
-	confirm_exit_code_is_zero;
+	$SCRIPT_DEPENDENCY_PATH_CHECK_SERVICE_USER_ID_IS_VALID --id $SERVICE_USER_ID
+	case $? in
+		0) ;;
+		1) echo "$0: the service user id is invalid. (confirm the /etc/passwd entry)">&2; exit 2; ;;
+		*) echo "$0: script dependency failure: \"$SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID\" produced a failure exit code.">&2; exit 3; ;;
+	esac
 fi
+
+if [ $ONLY_VALIDATE -eq 1 ]; then exit 0; fi
 
 echo "\t#DHCP message length is a minimum of 2000 bits, packet length should be greater than 250 bytes."
 echo "\t#Packet length should not be longer than 512 bytes to avoid fragmentation; DHCP messages should be delivered in a single transmission."
@@ -173,31 +206,36 @@ echo "\t#Flags: no flags for DHCP ACK, 16 zeroes, DHCPACK is not broadcasted (Se
 echo "\t\t@ih,80,16 0 \\\\";
 
 echo "\t#CIADDR (Client IP Address)";
-if [ -n "$REQUESTED_ADDRESS" ]; then
-	echo "\t\t@ih,96,8 $(echo $REQUESTED_ADDRESS | cut -d '.' -f 1) \\";
-	echo "\t\t@ih,104,8 $(echo $REQUESTED_ADDRESS | cut -d '.' -f 2) \\";
-	echo "\t\t@ih,112,8 $(echo $REQUESTED_ADDRESS | cut -d '.' -f 3) \\";
-	echo "\t\t@ih,120,8 $(echo $REQUESTED_ADDRESS | cut -d '.' -f 4) \\";
+if [ $CLIENT_IS_REUSING_IPV4_ADDRESS -eq 1 ]; then
+	if [ -n "$ALLOCATED_IPV4_ADDRESS" ]; then
+		echo "\t\t@ih,96,8 $(echo $ALLOCATED_IPV4_ADDRESS | cut -d '.' -f 1) \\";
+		echo "\t\t@ih,104,8 $(echo $ALLOCATED_IPV4_ADDRESS | cut -d '.' -f 2) \\";
+		echo "\t\t@ih,112,8 $(echo $ALLOCATED_IPV4_ADDRESS | cut -d '.' -f 3) \\";
+		echo "\t\t@ih,120,8 $(echo $ALLOCATED_IPV4_ADDRESS | cut -d '.' -f 4) \\";
+	else
+		echo "\t\t#@ih,96,32 - Cannot verify the allocated address, please consider the secrity implications.";
+	fi
 else
-	echo "\t\t#@ih,96,32 unrestricted - please consider the security implications";
+	echo "\t#Initial DHCPACK, this is the first allocation, and client IP address should be 0 (is not yet known)"
+	echo "\t\t@ih,96,32 0";
 fi
 
-if [ -n "$REQUESTED_ADDRESS" ]; then
-	echo "\t#YIADDR (Your IP address) Your (client) IP address";
-	echo "\t\t@ih,128,8 $(echo $REQUESTED_ADDRESS | cut -d '.' -f 1) \\";
-	echo "\t\t@ih,136,8 $(echo $REQUESTED_ADDRESS | cut -d '.' -f 2) \\";
-	echo "\t\t@ih,144,8 $(echo $REQUESTED_ADDRESS | cut -d '.' -f 3) \\";
-	echo "\t\t@ih,152,8 $(echo $REQUESTED_ADDRESS | cut -d '.' -f 4) \\";
+echo "\t#YIADDR (Your IP address) Your (client) IP address";
+if [ -n "$ALLOCATED_IPV4_ADDRESS" ]; then
+	echo "\t\t@ih,128,8 $(echo $ALLOCATED_IPV4_ADDRESS | cut -d '.' -f 1) \\";
+	echo "\t\t@ih,136,8 $(echo $ALLOCATED_IPV4_ADDRESS | cut -d '.' -f 2) \\";
+	echo "\t\t@ih,144,8 $(echo $ALLOCATED_IPV4_ADDRESS | cut -d '.' -f 3) \\";
+	echo "\t\t@ih,152,8 $(echo $ALLOCATED_IPV4_ADDRESS | cut -d '.' -f 4) \\";
 else
 	echo "\t\t#@ih,128,32 unrestricted - please consider the security implications";
 fi
 
 echo "\t#SIADDR (Server IP address) Returned in DHCPOFFER, DHCPACK, DHCPNAK";
-if [ -n "$SERVER_ADDRESS" ]; then
-	echo "\t\t@ih,160,8 $(echo $SERVER_ADDRESS | cut -d '.' -f 1) \\";
-	echo "\t\t@ih,168,8 $(echo $SERVER_ADDRESS | cut -d '.' -f 2) \\";
-	echo "\t\t@ih,176,8 $(echo $SERVER_ADDRESS | cut -d '.' -f 3) \\";
-	echo "\t\t@ih,184,8 $(echo $SERVER_ADDRESS | cut -d '.' -f 4) \\";
+if [ -n "$SERVER_IPV4_ADDRESS" ]; then
+	echo "\t\t@ih,160,8 $(echo $SERVER_IPV4_ADDRESS | cut -d '.' -f 1) \\";
+	echo "\t\t@ih,168,8 $(echo $SERVER_IPV4_ADDRESS | cut -d '.' -f 2) \\";
+	echo "\t\t@ih,176,8 $(echo $SERVER_IPV4_ADDRESS | cut -d '.' -f 3) \\";
+	echo "\t\t@ih,184,8 $(echo $SERVER_IPV4_ADDRESS | cut -d '.' -f 4) \\";
 else
 	echo "\t\t#@ih,160,32 unrestricted - please consider the security implications";
 fi
