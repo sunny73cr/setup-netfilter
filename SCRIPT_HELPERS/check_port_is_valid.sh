5247192764109f6372e05c08cbbf4172bf1c56ab
Dylan <sunny73cr@protonmail.com>
2024-09-20 15:39:46 +1200

    modify: check_port_is_valid.sh - improve arg parsing, improve regex matching (limit length), improve debug output, improve exit codes.

--- SCRIPT_HELPERS/check_port_is_valid.sh
+++ SCRIPT_HELPERS/check_port_is_valid.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-usage () {
+print_usage_then_exit () {
 	echo "">&2;
 	echo "Usage: $0 <arguments>">&2;
 	echo "--port <string>">&2;
@@ -8,126 +8,82 @@ usage () {
 	exit 2;
 }
 
+if [ "$1" = "" ]; then print_usage_then_exit; fi
+
 PORT="";
 
-if [ "$1" = "" ]; then usage; fi
-
 while true; do
-	case "$1" in
-		--port )
-			PORT="$2";
-			#if not enough arguments
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+	case $1 in
+		--port)
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			elif [ "$2" = "" ] || [ "$(echo $2 | grep -E '^-')" != "" ]; then
+				print_usage_then_exit;
+			else
+				PORT=$2;
+				shift 2;
+			fi
 		;;
-		--port-label)
-			PORT_LABEL="$2";
-			#if not enough arguments
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
-		;;
-		"" ) break; ;;
-		*)
-			echo "">&2;
-			echo "Unrecognised option: $1 $2">&2;
-			usage;
-		;;
+		"") break; ;;
+		*) printf "Unrecognised argument - ">&2; print_usage_then_exit; ;;
 	esac
 done
 
 if [ -z "$PORT" ]; then
-	if [ -n "$PORT_LABEL" ]; then
-		echo "$0; you must provide a $PORT_LABEL port (--port <number>).">&2;
-	else
-		echo "$0; you must provide a port (--port <number>).">&2;
-	fi
+	printf "$0: you must provide a port. (number between 1 and 65535)\n">&2;
 	exit 2;
 fi
 
 if [ "$(echo "$PORT" | cut -d '-' -f 1)" = "$PORT" ]; then
 	#not a range
-	if [ -z "$(echo "$PORT" | grep -P '[0-9]+')" ]; then
-		if [ -n "$PORT_LABEL" ]; then
-			echo "$0; $PORT_LABEL port is not a number.">&2;
-		else
-			echo "$0; port is not a number.">&2;
-		fi
-		exit 2;
+	if [ -z "$(echo "$PORT" | grep -P '[0-9]{1,5}')" ]; then
+		printf "$0: port is not a number.\n">&2;
+		exit 1;
 	fi
 
 	if [ "$PORT" -lt 1 ]; then
-		if [ -n "$PORT_LABEL" ]; then
-			echo "$0; $PORT_LABEL port is not in range.">&2;
-		else
-			echo "$0; port is not in range.">&2;
-		fi
-		exit 2;
+		printf "$0: port is too low.\n">&2;
+		exit 1;
 	fi
 
 	if [ "$PORT" -gt 65535 ]; then
-		if [ -n "$PORT_LABEL" ]; then
-			echo "$0; $PORT_LABEL port is not in range.">&2;
-		else
-			echo "$0; port is not in range.">&2;
-		fi
-		exit 2;
+		printf "$0: port is too high.\n">&2;
+		exit 1;
 	fi
 else
 	#a port range
 	PORT_RANGE_START=$(echo "$PORT" | cut -d '-' -f 1);
-	
-	if [ -z "$(echo "$PORT_RANGE_START" | grep -P '[0-9]+')" ]; then
-		if [ -n "$PORT_LABEL" ]; then
-			echo "$0; $PORT_LABEL port range start is not a number.">&2;
-		else
-			echo "$0; port range start is not a number.">&2;
-		fi
-		exit 2;
+
+	if [ -z "$(echo "$PORT_RANGE_START" | grep -P '[0-9]{1,5}')" ]; then
+		printf "$0: port range lower bound is not a number.\n">&2;
+		exit 1;
 	fi
 
 	if [ "$PORT_RANGE_START" -lt 1 ]; then
-		if [ -n "$PORT_LABEL" ]; then
-			echo "$0; $PORT_LABEL port range start is not a port number.">&2;
-		else
-			echo "$0; port range start is not a port number.">&2;
-		fi
-		exit 2;
+		printf "$0: port range start is too low.\n">&2;
+		exit 1;
 	fi
 
 	if [ "$PORT_RANGE_START" -gt 65535 ]; then
-		if [ -n "$PORT_LABEL" ]; then
-			echo "$0; $PORT_LABEL port range start is not a port number.">&2;
-		else
-			echo "$0; port range start is not a port number.">&2;
-		fi
-		exit 2;
+		printf "$0: port range start is too high.\n">&2;
+		exit 1;
 	fi
-	
+
 	PORT_RANGE_END=$(echo "$PORT" | cut -d '-' -f 2);
 
-	if [ -z "$(echo "$PORT_RANGE_END" | grep -P '[0-9]+')" ]; then
-		if [ -n "$PORT_LABEL" ]; then
-			echo "$0; $PORT_LABEL port range end is not a number.">&2;
-		else
-			echo "$0; port range end is not a number.">&2;
-		fi
-		exit 2;
+	if [ -z "$(echo "$PORT_RANGE_END" | grep -P '[0-9]{1,5}')" ]; then
+		printf "$0: port range end is not a number.\n">&2;
+		exit 1;
 	fi
 
 	if [ "$PORT_RANGE_END" -lt 1 ]; then
-		if [ -n "$PORT_LABEL" ]; then
-			echo "$0; $PORT_LABEL port range end is not a port number.">&2;
-		else
-			echo "$0; port range end is not a port number.">&2;
-		fi
-		exit 2;
+		printf "$0: port range end is too low.\n">&2;
+		exit 1;
 	fi
 
 	if [ "$PORT_RANGE_END" -gt 65535 ]; then
-		if [ -n "$PORT_LABEL" ]; then
-			echo "$0; $PORT_LABEL port range end is not a port number.">&2;
-		else
-			echo "$0; port range end is not a port number.">&2;
-		fi
-		exit 2;
+		printf "$0: port range end is too high.\n">&2;
+		exit 1;
 	fi
 fi
 
