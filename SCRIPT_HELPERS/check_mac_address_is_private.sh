daa2da104ebda31c8ca9c11358b17e3411af7c8a
Dylan <sunny73cr@protonmail.com>
2024-09-21 07:38:14 +1200

    modify: check_mac_address_is_private.sh - improve debug logging

--- SCRIPT_HELPERS/check_mac_address_is_private.sh
+++ SCRIPT_HELPERS/check_mac_address_is_private.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-if [ -z "$ENV_SETUP_NFT" ]; then printf "setup-netfilter: set ENV_SETUP_NFT to the absolute path of the setup-netfilter directory first.">&2; exit 4; fi
+if [ -z "$ENV_SETUP_NFT" ]; then printf "setup-netfilter: set ENV_SETUP_NFT to the absolute path of the setup-netfilter directory first.\n">&2; exit 4; fi
 
 DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS="$ENV_SETUP_NFT/SCRIPT_HELPERS/check_mac_address_is_valid.sh";
 
 if [ ! -x $DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS ]; then
-	echo "$0; script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS\" is missing or is not executable.">&2;
+	echo "$0: dependency: \"$DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS\" is missing or is not executable.">&2;
 	exit 3;
 fi
 
 print_usage_then_exit () {
-	echo "Usage: $0 --address XX:XX:XX:XX:XX:XX, where X is a-f, A-F, 0-9: hexadecimal">&2;
+	printf "Usage: $0\n">&2
+	printf " --address XX:XX:XX:XX:XX:XX (where X is a-f, A-F, 0-9)\n">&2;
+	printf "\n">&2;
 	exit 2;
 }
 
@@ -31,14 +33,19 @@ while true; do
 			fi
 		;;
 		"") break; ;;
-		*) printf "Unrecognised argument - ">&2; print_usage_then_exit; ;;
+		*) printf "Unrecognised argument $1. ">&2; print_usage_then_exit; ;;
 	esac
 done
+
+if [ -z "$ADDRESS" ]; then
+	printf "\nMissing --address. ">&2;
+	print_usage_then_exit;
+fi
 
 $DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS --address "$ADDRESS"
 case $? in
 	0) ;;
-	1) printf "$0: you provided an invalid mac address. It must be in the form XX:XX:XX:XX:XX:XX, where X is a-f, or A-F, or 0-9: hexadecimal.\n">&2; exit 2; ;;
+	1) printf "\nInvalid --address. ">&2; print_usage_then_exit; ;;
 	*) printf "$0: script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS\" produced a failure status code.\n">&2; exit 3; ;;
 esac
 
