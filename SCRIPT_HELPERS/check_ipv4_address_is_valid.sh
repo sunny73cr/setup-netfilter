7112b9fd0e486b7947ff693a43a661a68eebe3ad
Dylan <sunny73cr@protonmail.com>
2024-09-20 13:15:14 +1200

    modify: check_ipv4_address_is_valid.sh - improve arg parsing, debug output, and fix exit codes

--- SCRIPT_HELPERS/check_ipv4_address_is_valid.sh
+++ SCRIPT_HELPERS/check_ipv4_address_is_valid.sh
@@ -1,66 +1,55 @@
 #!/bin/sh
 
-usage () {
+print_usage_then_exit () {
 	echo "Usage: $0 --address <string>">&2;
 	exit 2;
 }
 
 ADDRESS="";
 
-if [ "$1" = "" ]; then usage; fi
+if [ "$1" = "" ]; then print_usage_then_exit; fi
 
 while true; do
 	case "$1" in
-		--address )
-			ADDRESS="$2";
-			#if not enough arguments
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+		--address)
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			elif [ "$2" = "" ] || [ "$(echo $2 | grep -E '^-')" != "" ]; then
+				print_usage_then_exit;
+			else
+				ADDRESS=$2;
+				shift 2;
+			fi
 		;;
-		"" ) break; ;;
-		*)
-			echo "">&2;
-			echo "Unrecognised option: $1 $2">&2;
-			usage;
-		;;
+		"") break; ;;
+		*) printf "Unrecognised argument - ">&2; print_usage_then_exit; ;;
 	esac
 done
 
 if [ -z "$ADDRESS" ]; then
-	echo "$0; you must provide an IPV4 address (--address <string>).">&2;
-	exit 2;
+	echo "$0; you must provide an IPV4 address in the form of X.X.X.X (where X is 0-255)">&2;
+	exit 1;
 fi
 
 if [ -z $(echo "$ADDRESS" | grep -P '^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$') ]; then
-	echo "$0; the address does not match the IPV4 address pattern (X.X.X.X).">&2;
-	exit 2;
+	echo "$0; the address does not match the form (X.X.X.X, where X is 0-999).">&2;
+	exit 1;
 fi
 
 FIRST_OCTET=$(echo "$ADDRESS" | cut -d '.' -f 1);
 
-if [ "$FIRST_OCTET" -gt 255 ]; then
-	echo "$0; the first octet is too high.">&2;
-	exit 2;
-fi
+if [ "$FIRST_OCTET" -gt 255 ]; then exit 1; fi
 
 SECOND_OCTET=$(echo "$ADDRESS" | cut -d '.' -f 2);
 
-if [ "$SECOND_OCTET" -gt 255 ]; then
-	echo "$0; the second octet is too high.">&2;
-	exit 2;
-fi
+if [ "$SECOND_OCTET" -gt 255 ]; then exit 1; fi
 
 THIRD_OCTET=$(echo "$ADDRESS" | cut -d '.' -f 3);
 
-if [ "$THIRD_OCTET" -gt 255 ]; then
-	echo "$0; the third octet is too high.">&2;
-	exit 2;
-fi
+if [ "$THIRD_OCTET" -gt 255 ]; then exit 1; fi
 
 FOURTH_OCTET=$(echo "$ADDRESS" | cut -d '.' -f 4);
 
-if [ "$FOURTH_OCTET" -gt 255 ]; then
-	echo "$0; the fourth octet is too high.">&2;
-	exit 2;
-fi
+if [ "$FOURTH_OCTET" -gt 255 ]; then exit 1; fi
 
 exit 0;
