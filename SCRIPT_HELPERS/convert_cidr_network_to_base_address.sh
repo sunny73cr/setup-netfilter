10e4bd6af24572329bfb66805b905b644ae1f058
Dylan <sunny73cr@protonmail.com>
2024-09-21 08:47:59 +1200

    modify: convert_cidr_network_to_base_address.sh - improve debug output

--- SCRIPT_HELPERS/convert_cidr_network_to_base_address.sh
+++ SCRIPT_HELPERS/convert_cidr_network_to_base_address.sh
@@ -1,82 +1,83 @@
 #!/bin/sh
 
-DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_NETWORK_IS_VALID="./SCRIPT_HELPERS/check_ipv4_network_is_valid.sh";
+if [ -z "$ENV_SETUP_NFT" ]; then printf "setup-netfilter: set ENV_SETUP_NFT to the absolute path of the setup-netfilter directory first.\n">&2; exit 4; fi
 
-if [ ! -x "$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_NETWORK_IS_VALID" ]; then
-	echo "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_NETWORK_IS_VALID\" is missing or is not executable.">&2;
+DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_NETWORK_IS_VALID="$ENV_SETUP_NFT/SCRIPT_HELPERS/check_ipv4_network_is_valid.sh";
+
+if [ ! -x $DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_NETWORK_IS_VALID ]; then
+	echo "$0; dependency: \"$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_NETWORK_IS_VALID\" is missing or is not executable.">&2;
 	exit 3;
 fi
 
-DEPENDENCY_SCRIPT_PATH_CONVERT_IPV4_ADDRESS_TO_BINARY="./SCRIPT_HELPERS/convert_ipv4_address_to_binary.sh";
+DEPENDENCY_SCRIPT_PATH_CONVERT_IPV4_ADDRESS_TO_BINARY="$ENV_SETUP_NFT/SCRIPT_HELPERS/convert_ipv4_address_to_binary.sh";
 
-if [ ! -x "$DEPENDENCY_SCRIPT_PATH_CONVERT_IPV4_ADDRESS_TO_BINARY" ]; then
-	echo "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CONVERT_IPV4_ADDRESS_TO_BINARY\" is missing or is not executable.">&2;
+if [ ! -x $DEPENDENCY_SCRIPT_PATH_CONVERT_IPV4_ADDRESS_TO_BINARY ]; then
+	echo "$0; dependency: \"$DEPENDENCY_SCRIPT_PATH_CONVERT_IPV4_ADDRESS_TO_BINARY\" is missing or is not executable.">&2;
 	exit 3;
 fi
 
-DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_DECIMAL="./SCRIPT_HELPERS/convert_binary_to_base10.sh";
+DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_DECIMAL="$ENV_SETUP_NFT/SCRIPT_HELPERS/convert_binary_to_base10.sh";
 
-if [ ! -x "$DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_DECIMAL" ]; then
-	echo "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_DECIMAL\" is missing or is not executable.">&2;
+if [ ! -x $DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_DECIMAL ]; then
+	echo "$0; dependency: \"$DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_DECIMAL\" is missing or is not executable.">&2;
 	exit 3;
 fi
 
-check_success () {
-	if [ "$?" -ne 0 ]; then
-		echo "$0; cannot find the base address.">&2;
-		exit 3;
-	fi
-}
-
-usage () {
-	echo "Usage: $0 --network <X.X.X.X/X>">&2;
+print_usage_then_exit () {
+	printf "Usage: $0 <arguments>\n">&2
+	printf " --network X.X.X.X/Y (where X is 0-255, and Y is 1-32)\n">&2;
+	printf "\n">&2
 	exit 2;
 }
 
-if [ "$1" = "" ]; then usage; fi
+if [ "$1" = "" ]; then print_usage_then_exit; fi
 
 while true; do
 	case "$1" in
 		--network )
-			ADDRESS="$2";
-			#if not enough arguments
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			elif [ "$2" = "" ]; then
+				print_usage_then_exit;
+			else
+				ADDRESS=$2;
+				shift 2;
+			fi
 		;;
-		"" ) break; ;;
-		*)
-			echo "">&2;
-			echo "Unrecognised option: $1 $2">&2;
-			usage;
-		;;
+		"") break; ;;
+		*) printf "Unrecognised argument $1. ">&2; print_usage_then_exit; ;;
 	esac
 done
 
 if [ -z "$ADDRESS" ]; then
-	echo "$0; you must provide an ipv4 network in cidr notation (0.0.0.0/0)">&2;
-	exit 2;
+	echo "\nMissing --address. ">&2;
+	print_usage_then_exit;
 fi
 
-IS_IPV4_NETWORK_VALID=$($DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_NETWORK_IS_VALID --network "$ADDRESS");
-check_success;
+$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_NETWORK_IS_VALID --network "$ADDRESS"
+case $? in
+	0) ;;
+	1) printf "\nInvalid --address. ">&2; print_usage_then_exit; ;;
+	*) printf "$0: dependency: \"$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_NETWORK_IS_VALID\" produced a failure exit code.">&2; exit 3; ;;
+esac
 
 MASK=$(echo "$ADDRESS" | cut -d '/' -f 2);
 
 if [ "$MASK" -eq 0 ]; then
-	echo "$0; mask should not be zero.">&2;
-	exit 2;
+	echo "\nInvalid CIDR network mask. ">&2;
+	print_usage_then_exit;
 fi
 
 ADDRESS=$(echo "$ADDRESS" | cut -d '/' -f 1);
 
 if [ "$MASK" -eq 32 ]; then
-	echo "$ADDRESS";
-	exit 0;
+	echo "\nInvalid CIDR network mask. ">&2;
+	print_usage_then_exit;
 fi
 
 ##s
 # Convert the address to binary.
-# Cut the address where its mask ends
-# Set the remaining bits to zero.
+# Zero the bits that are not masked.
 # Convert the address into decimal.
 #
 #			                 |'Zero' these bits.
@@ -92,44 +93,51 @@ fi
 ADDRESS_BINARY=$($DEPENDENCY_SCRIPT_PATH_CONVERT_IPV4_ADDRESS_TO_BINARY \
 --address "$ADDRESS" \
 --output-bit-order "little-endian");
-check_success;
+case $? in
+	0) ;;
+	*) printf "$0: dependency: \"$DEPENDENCY_SCRIPT_PATH_CONVERT_IPV4_ADDRESS_TO_BINARY\" produced a failure exit code.">&2; exit 3; ;;
+esac
 
 ADDRESS_BINARY_MASKED=$(echo $ADDRESS_BINARY | cut -c "1-$MASK");
 
 THIRTY_TWO_ZEROES="00000000000000000000000000000000";
 
-SPACE_LEFT=$((32-$MASK));
-
-ZERO_FILL=$(echo "$THIRTY_TWO_ZEROES" | cut -c "1-$SPACE_LEFT");
+ZERO_FILL=$(echo "$THIRTY_TWO_ZEROES" | cut -c "1-$((32-$MASK))");
 
 ADDRESS_BASE_BINARY="$ADDRESS_BINARY_MASKED$ZERO_FILL";
 
-ADDRESS_BASE_BINARY_OCTET1=$(echo "$ADDRESS_BASE_BINARY" | cut -c "1-8");
-ADDRESS_BASE_BINARY_OCTET2=$(echo "$ADDRESS_BASE_BINARY" | cut -c "9-16");
-ADDRESS_BASE_BINARY_OCTET3=$(echo "$ADDRESS_BASE_BINARY" | cut -c "17-24");
-ADDRESS_BASE_BINARY_OCTET4=$(echo "$ADDRESS_BASE_BINARY" | cut -c "25-32");
-
 ADDRESS_BASE_DECIMAL_OCTET1=$($DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_DECIMAL \
---binary "$ADDRESS_BASE_BINARY_OCTET1" \
+--binary "$(echo "$ADDRESS_BASE_BINARY" | cut -c "1-8")" \
 --input-bit-order "little-endian");
-check_success;
+case $? in
+	0) ;;
+	*) printf "$0: dependency: \"$DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_DECIMAL\" produced a failure exit code.">&2; exit 3; ;;
+esac
 
 ADDRESS_BASE_DECIMAL_OCTET2=$($DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_DECIMAL \
---binary "$ADDRESS_BASE_BINARY_OCTET2" \
+--binary "$(echo "$ADDRESS_BASE_BINARY" | cut -c "9-16")" \
 --input-bit-order "little-endian");
-check_success;
+case $? in
+	0) ;;
+	*) printf "$0: dependency: \"$DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_DECIMAL\" produced a failure exit code.">&2; exit 3; ;;
+esac
 
 ADDRESS_BASE_DECIMAL_OCTET3=$($DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_DECIMAL \
---binary "$ADDRESS_BASE_BINARY_OCTET3" \
+--binary "$(echo "$ADDRESS_BASE_BINARY" | cut -c "17-24")" \
 --input-bit-order "little-endian");
-check_success;
+case $? in
+	0) ;;
+	*) printf "$0: dependency: \"$DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_DECIMAL\" produced a failure exit code.">&2; exit 3; ;;
+esac
 
 ADDRESS_BASE_DECIMAL_OCTET4=$($DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_DECIMAL \
---binary "$ADDRESS_BASE_BINARY_OCTET4" \
+--binary "$(echo "$ADDRESS_BASE_BINARY" | cut -c "25-32")" \
 --input-bit-order "little-endian");
-check_success;
+case $? in
+	0) ;;
+	*) printf "$0: dependency: \"$DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_DECIMAL\" produced a failure exit code.">&2; exit 3; ;;
+esac
 
-ADDRESS_DECIMAL_BASE="$ADDRESS_BASE_DECIMAL_OCTET1.$ADDRESS_BASE_DECIMAL_OCTET2.$ADDRESS_BASE_DECIMAL_OCTET3.$ADDRESS_BASE_DECIMAL_OCTET4";
+printf "$ADDRESS_BASE_DECIMAL_OCTET1.$ADDRESS_BASE_DECIMAL_OCTET2.$ADDRESS_BASE_DECIMAL_OCTET3.$ADDRESS_BASE_DECIMAL_OCTET4";
 
-echo $ADDRESS_DECIMAL_BASE;
 exit 0;
