13164d63a10400a72de6f1589eabd95a96581556
Dylan <sunny73cr@protonmail.com>
2024-09-21 06:56:14 +1200

    modify: check_interface_exists_by_mac_address.sh - improve debug output

--- SCRIPT_HELPERS/check_interface_exists_by_mac_address.sh
+++ SCRIPT_HELPERS/check_interface_exists_by_mac_address.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-if [ -z "$ENV_SETUP_NFT" ]; then printf "Set ENV_SETUP_NFT to the absolute path of the setup-netfilter directory first.">&2; exit 4; fi
+if [ -z "$ENV_SETUP_NFT" ]; then printf "setup-netfilter: set ENV_SETUP_NFT to the absolute path of the setup-netfilter directory first.\n">&2; exit 4; fi
 
 DEPENDENCY_SCRIPT_PATH_CHECK_MAC_ADDRESS_IS_VALID="$ENV_SETUP_NFT/SCRIPT_HELPERS/check_mac_address_is_valid.sh";
 
 if [ ! -x $DEPENDENCY_SCRIPT_PATH_CHECK_MAC_ADDRESS_IS_VALID ]; then
-	printf "$0; script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_CHECK_MAC_ADDRESS_IS_VALID\" is missing or is not executable.\n">&2;
+	printf "$0; dependency: \"$DEPENDENCY_SCRIPT_PATH_CHECK_MAC_ADDRESS_IS_VALID\" is missing or is not executable.\n">&2;
 	exit 3;
 fi
 
 print_usage_then_exit () {
-	printf "Usage: $0 --address XX:XX:XX:XX:XX:XX (where X is a-f, or A-F, or 0-9: hexadecimal)">&2;
+	printf "Usage: $0 <arguments>\n">&2;
+	printf "--address XX:XX:XX:XX:XX:XX (where X is a-f, or A-F, or 0-9: hexadecimal)\n">&2;
+	printf "\n">&2;
 	exit 2;
 }
 
@@ -33,13 +35,13 @@ while true; do
 			fi
 		;;
 		"") break; ;;
-		*) printf "Unrecognised argument - ">&2; print_usage_then_exit; ;;
+		*) printf "Unrecognised argument $1. ">&2; print_usage_then_exit; ;;
 	esac
 done
 
 if [ -z "$ADDRESS" ]; then
-	printf "$0: you must provide a mac address.\n">&2;
-	exit 2;
+	printf "\nMissing --address. ">&2;
+	print_usage_then_exit;
 fi
 
 $DEPENDENCY_SCRIPT_PATH_CHECK_MAC_ADDRESS_IS_VALID --address "$ADDRESS"
