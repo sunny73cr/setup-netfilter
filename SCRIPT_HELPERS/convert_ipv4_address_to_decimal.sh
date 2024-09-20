35f7339ef8e59619cce351c0c62067de5ce4e3e7
Dylan <sunny73cr@protonmail.com>
2024-09-21 09:25:13 +1200

    modify: convert_ipv4_address_to_decimal.sh - improve debug output

--- SCRIPT_HELPERS/convert_ipv4_address_to_decimal.sh
+++ SCRIPT_HELPERS/convert_ipv4_address_to_decimal.sh
@@ -1,68 +1,82 @@
 #!/bin/sh
 
-DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_ADDRESS_IS_VALID="./SCRIPT_HELPERS/check_ipv4_address_is_valid.sh";
+if [ -z "$ENV_SETUP_NFT" ]; then printf "setup-netfilter: set ENV_SETUP_NFT to the absolute path of the setup-netfilter directory first.\n">&2; exit 4; fi
 
-if [ ! -x "$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_ADDRESS_IS_VALID" ]; then
-	echo "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_ADDRESS_IS_VALID\" is missing or is not executable.">&2;
+DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_ADDRESS_IS_VALID="$ENV_SETUP_NFT/SCRIPT_HELPERS/check_ipv4_address_is_valid.sh";
+
+if [ ! -x $DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_ADDRESS_IS_VALID ]; then
+	printf "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_ADDRESS_IS_VALID\" is missing or is not executable.\n">&2;
 	exit 3;
 fi
 
-DEPENDENCY_SCRIPT_PATH_CONVERT_IPV4_ADDRESS_TO_BINARY="./SCRIPT_HELPERS/convert_ipv4_address_to_binary.sh";
+DEPENDENCY_SCRIPT_PATH_CONVERT_IPV4_ADDRESS_TO_BINARY="$ENV_SETUP_NFT/SCRIPT_HELPERS/convert_ipv4_address_to_binary.sh";
 
-if [ ! -x "$DEPENDENCY_SCRIPT_PATH_CONVERT_IPV4_ADDRESS_TO_BINARY" ]; then
-	echo "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CONVERT_IPV4_ADDRESS_TO_BINARY\" is missing or is not executable.">&2;
+if [ ! -x $DEPENDENCY_SCRIPT_PATH_CONVERT_IPV4_ADDRESS_TO_BINARY ]; then
+	printf "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CONVERT_IPV4_ADDRESS_TO_BINARY\" is missing or is not executable.\n">&2;
 	exit 3;
 fi
 
-DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_BASE10="./SCRIPT_HELPERS/convert_binary_to_base10.sh";
+DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_BASE10="$ENV_SETUP_NFT/SCRIPT_HELPERS/convert_binary_to_base10.sh";
 
-if [ ! -x "$DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_BASE10" ]; then
-	echo "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_BASE10\" is missing or is not executable.">&2;
+if [ ! -x $DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_BASE10 ]; then
+	printf "$0; dependency script failure: \"$DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_BASE10\" is missing or is not executable.\n">&2;
 	exit 3;
 fi
 
-check_success () {
-	if [ "$?" -ne 0 ]; then
-		echo "$0; cannot convert the address to decimal.">&2;
-		exit 3;
-	fi
-}
-
-usage () {
-	echo "Usage: $0 --address <X.X.X.X>">&2;
+print_usage_then_exit() {
+	printf "Usage: $0 <argument>\.">&2
+	printf " --address X.X.X.X (where X is 0-255)\n">&2;
+	printf "\n">&2;
 	exit 2;
 }
 
-if [ "$1" = "" ]; then usage; fi
+if [ "$1" = "" ]; then print_usage_then_exit; fi
 
 while true; do
 	case "$1" in
-		--address )
-			ADDRESS="$2";
-			#if not enough arguments
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+		--address)
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			elif [ "$2" = "" ]; then
+				print_usage_then_exit;
+			else
+				ADDRESS=$2;
+				shift 2;
+			fi
 		;;
-		"" ) break; ;;
-		*)
-			echo "">&2;
-			echo "Unrecognised option: $1 $2">&2;
-			usage;
-		;;
+		"") break; ;;
+		*) printf "Unrecognised argument $1. ">&2; print_usage_then_exit; ;;
 	esac
 done
 
-IS_IPV4_ADDRESS_VALID=$($DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_ADDRESS_IS_VALID --address "$ADDRESS");
-check_success;
+if [ -z "$ADDRESS" ]; then
+	printf "\nMissing --address. ">&2;
+	print_status_then_exit;
+fi
+
+$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_ADDRESS_IS_VALID --address "$ADDRESS"
+case $? in
+	0) ;;
+	1) printf "\nInvalid --address. ">&2; print_usage_then_exit; ;;
+	*) printf "$0: dependency: \"$DEPENDENCY_SCRIPT_PATH_CHECK_IPV4_ADDRESS_IS_VALID\" produced a failure exit code.\n">&2;
+esac
 
 ADDRESS_BINARY=$($DEPENDENCY_SCRIPT_PATH_CONVERT_IPV4_ADDRESS_TO_BINARY \
 --address "$ADDRESS" \
 --output-bit-order "little-endian");
-check_success;
+case $? in
+	0) ;;
+	*) printf "$0: dependency: \"$DEPENDENCY_SCRIPT_PATH_CONVERT_IPV4_ADDRESS_TO_BINARY\" produced a failure exit code.\n">&2;
+esac
 
 ADDRESS_BASE10=$($DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_BASE10 \
 --binary "$ADDRESS_BINARY" \
 --input-bit-order "little-endian");
-check_success;
+case $? in
+	0) ;;
+	*) printf "$0: dependency: \"$DEPENDENCY_SCRIPT_PATH_CONVERT_BINARY_TO_BASE10\" produced a failure exit code.\n">&2;
+esac
 
-echo $ADDRESS_BASE10;
+printf "$ADDRESS_BASE10";
+
 exit 0;
