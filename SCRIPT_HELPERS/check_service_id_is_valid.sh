d8ab7db3240e97711c506d32c369616e8f2565e0
Dylan <sunny73cr@protonmail.com>
2024-09-20 16:18:30 +1200

    modify: check_service_id_is_valid.sh - improve arg parsing, improve debug output, improve regex, improve exit codes

--- SCRIPT_HELPERS/check_service_id_is_valid.sh
+++ SCRIPT_HELPERS/check_service_id_is_valid.sh
@@ -1,56 +1,49 @@
 #!/bin/sh
 
-usage () {
+print_usage_and_exit () {
 	echo "Usage: $0 --service-user-id <number>">&2;
 	exit 2;
 }
 
-SERVICE_ID="";
+if [ "$1" = "" ]; then print_usage_and_exit; fi
 
-if [ "$1" = "" ]; then usage; fi
+SERVICE_USER_ID="";
 
 while true; do
-	case "$1" in
+	case $1 in
 		--service-user-id)
-			SERVICE_ID="$2";
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			elif [ "$2" = "" ] || [ "$(echo $2 | grep -E '^-')" != "" ]; then
+				print_usage_then_exit;
+			else
+				SERVICE_USER_ID=$2;
+				shift 2;
+			fi
 		;;
 		"") break; ;;
-		*)
-			echo "">&2;
-			echo "Unrecognised option: $1 $2">&2;
-			usage;
-		;;
+		*) printf "Unrecognised argument - ">&2; print_usage_and_exit; ;;
 	esac
 done;
 
-if [ -z "$SERVICE_ID" ]; then
-	echo "$0; you must provide a service user id.">&2;
+if [ -z "$SERVICE_USER_ID" ]; then
+	printf "$0; you must provide a service user id.\n">&2;
 	exit 2;
 fi
 
-NUMBER_REGEX='[0-9]+';
-
-IS_NUMBER=$(echo "$SERVICE_ID" | grep -E '[0-9]+');
-
-if [ "$IS_NUMBER" = "" ]; then
-	echo "$0; the service user id must be a number.">&2;
-	exit 2;
+if [ "$(echo $SERVICE_USER_ID | grep -E '^[0-9]{1,5}$')" = "" ]; then
+	printf "$0; the service user id must be a number between 1 and 65535\n">&2;
+	exit 1;
 fi
 
-if [ "$SERVICE_ID" -eq "0" ]; then
-	echo "$0; the service user id must not be zero.">&2;
-	exit 2;
+if [ "$SERVICE_USER_ID" -eq 0 ]; then
+	printf "$0; the service user id must not be zero.\n">&2;
+	exit 1;
 fi
 
-SERVICE_REGEX="^[a-zA-Z]+:x:$SERVICE_ID";
-
-SERVICES_MATCHING_USER_ID=$(cat /etc/passwd | grep -E "$SERVICE_REGEX");
-
-if [ -z "$SERVICES_MATCHING_USER_ID" ]; then
-	echo "$0; the Service User ID is not valid.">&2;
-	exit 2;
+if [ -z "$(cat /etc/passwd | grep -E [\-_a-zA-Z]{1,32}:x:$SERVICE_USER_ID)" ]; then
+	printf "$0: the service user id was not found in /etc/passwd\n">&2;
+	exit 1;
 else
 	exit 0;
 fi
-
