12fb8e4faf36181f8c7f850a2ded18ca36bc2bd5
Dylan <sunny73cr@protonmail.com>
2024-09-20 15:29:54 +1200

    modify: check_mac_address_is_valid.sh - improve arg parsing, improve debug output, improve exit codes.

--- SCRIPT_HELPERS/check_mac_address_is_valid.sh
+++ SCRIPT_HELPERS/check_mac_address_is_valid.sh
@@ -1,40 +1,40 @@
 #!/bin/sh
 
-usage () {
+print_usage_then_exit () {
 	echo "Usage: $0 --address <string>">&2;
 	exit 2;
 }
 
-ADDRESS="";
+if [ "$1" = "" ]; then print_usage_then_exit; fi
 
-if [ "$1" = "" ]; then usage; fi
+ADDRESS="";
 
 while true; do
 	case "$1" in
 		--address )
-			ADDRESS="$2";
-			#if not enough arguments
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			elif [ "$2" = "" ] || [ "$(echo $2 | grep -E '^-')" != "" ]; then
+				print_usage_then_exit;
+			else
+				ADDRESS=$2;
+				shift 2;
+			fi
 		;;
-		"" ) break; ;;
-		*)
-			echo "">&2;
-			echo "Unrecognised option: $1 $2">&2;
-			usage;
-		;;
+		"") break; ;;
+		*) printf "Unrecognised argument - ">&2; print_usage_then_exit; ;;
 	esac
 done
 
 if [ -z "$ADDRESS" ]; then
-	echo "$0; you must provide a MAC address (--address <string>)">&2;
+	echo "$0; you must provide a MAC address (--address XX:XX:XX:XX:XX:XX, where X is a-f, or A-F, or 0-9: hexadecimal)">&2;
 	exit 2;
 fi
 
 REGEX='^[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}$';
 
-if [ -n "$(echo "$ADDRESS" | grep -P $REGEX)" ]; then
+if [ -n "$(echo "$ADDRESS" | grep -E $REGEX)" ]; then
 	exit 0;
 else
-	echo "$0; the MAC address is not valid.">&2;
-	exit 2;
+	exit 1;
 fi;
