9b98c306bce0a4ee10cd85eea89312e266993405
Dylan <sunny73cr@protonmail.com>
2024-09-21 06:51:25 +1200

    modify: convert_binary_to_base10.sh - improve debug output

--- SCRIPT_HELPERS/convert_binary_to_base10.sh
+++ SCRIPT_HELPERS/convert_binary_to_base10.sh
@@ -1,64 +1,102 @@
 #!/bin/sh
 
-usage () {
-	echo "Usage: $0 --binary <string> --input-bit-order <big-endian|little-endian>">&2;
+print_usage_then_exit () {
+	printf "Usage: $0 <arguments>\n";
+	printf " --binary <string> (0's and 1's).\n";
+	printf "\n">&2;
+	printf " Optional: --input-bit-order (big-endian or little-endian).\n">&2;
+	printf " Note: if input-bit-order is omitted, the default is little-endian.\n">&2;
+	printf "\n">&2;
+	printf " Optional: --output-signed-numbers\n">&2;
+	printf " Note: if the output-signed-numbers flag is present, numbers with the highest bit set are output as negative numbers.\n">&2;
+	printf " WARNING: this is very likely not what you want, unless the binary is already a length matching a data type, with zero padding if neccessary.\n">&2;
+	printf "\n">&2;
+	printf " Optional: --newline-suffix-output\n">&2;
+	printf " Note: this causes the program to append a newline to the output.\n">&2;
+	printf "\n">&2;
 	exit 2;
 }
 
 BINARY="";
 BIT_ORDER="";
-
-if [ "$1" = "" ]; then usage; fi
-
+OUTPUT_SIGNED_NUMBERS=0;
+SKIP_VALIDATE=0;
+NEWLINE_SUFFIX_OUTPUT=0;
 while true; do
-	case "$1" in
+	case $1 in
 		--binary)
-			BINARY="$2";
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			elif [ "$2" = "" ]; then
+				print_usage_then_exit;
+			else
+				BINARY=$2;
+				shift 2;
+			fi
 		;;
-		
 		--input-bit-order)
-			BIT_ORDER="$2";
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			elif [ "$2" = "" ]; then
+				print_usage_then_exit;
+			else
+				BIT_ORDER=$2;
+				shift 2;
+			fi
 		;;
-		
+		--output-signed-numbers)
+			OUTPUT_SIGNED_NUMBERS=1;
+			shift 1;
+		;;
+		--skip-validate)
+			SKIP_VALIDATE=1;
+			shift 1;
+		;;
+		--newline-suffix-output)
+			NEWLINE_SUFFIX_OUTPUT=1;
+			shift 1;
+		;;
 		"") break; ;;
-		*)
-			echo "">&2;
-			echo "Unrecognised option: $1 $2">&2;
-			usage;
-		;;
+		*) printf "Unrecognised argument $1. ">&2; print_usage_then_exit; ;;
 	esac
 done
 
 if [ -z "$BINARY" ]; then
-	echo "$0; you must provide a binary string">&2;
-	exit 2;
+	BINARY=$(dd if=/dev/stdin of=/dev/stdout bs=1 count=32 status=none);
+
+	if [ -z "BINARY" ]; then print_usage_then_exit; fi
 fi
 
-BINARY_REGEX='[0|1]{1,32}';
+if [ $SKIP_VALIDATE -eq 0 ]; then
 
-if [ "$(echo "$BINARY" | grep -P $BINARY_REGEX)" = "" ]; then
-	echo "$0; you must provide a binary string (1-32 characters of 1's or 0's)">&2;
-	exit 2;
+	if [ "$(echo "$BINARY" | grep -E '^[0|1]{1,32}$')" = "" ]; then
+		printf "\nInvalid --binary. ">&2;
+		print_usage_then_exit;
+	fi
+
+	if [ "$(echo "$BINARY" | grep -E '^[0]{1,32}$')" != "" ]; then
+		printf "0";
+		if [ $NEWLINE_SUFFIX_OUTPUT -eq 1 ]; then
+			printf "\n";
+		fi
+		exit 0;
+	fi
 fi
 
-if [ "$BINARY" = "00000000" ]; then
-	echo "0";
-	exit 0;
-fi
-
-if [ -z "$BIT_ORDER" ]; then
-	echo "$0; you must provide an input bit-order. (try '--input-bit-order big-endian' or '--input-bit-order little-endian' without quotes.)">&2;
-	exit 2;
-fi
+BIT_LENGTH="${#BINARY}";
 
 case "$BIT_ORDER" in
-	big-endian) ;;
-	little-endian) ;;
+	big-endian)
+		OFFSET=1;
+		BIT_ORDER=0;
+	;;
+	""|little-endian)
+		OFFSET=$BIT_LENGTH;
+		BIT_ORDER=1;
+	;;
 	*)
-		echo "$0; unrecognised bit-order. (try '--input-bit-order big-endian' or '--input-bit-order little-endian' without quotes.)">&2;
-		exit 2;
+		printf "\nInvalid --bit-order. ">&2;
+		print_usage_then_exit;
 	;;
 esac
 
@@ -68,35 +106,39 @@ esac
 # For a binary number with n digits:
 # D(n-1) 	= 	d3 d2 d1 d0
 # 0110 		=	0  1  1  0
-# The decimal number is equal to the sum of binary digits times their power of 2
+# The decimal number is equal to the sum of binary digits multiplied by their power of 2
 # Decimal	=	d0*(2^0) + d1*(2^1) + d2*(2^2) + d3*(d^3)
 # Binary	=	0*1	 + 1*2	    + 1*4      + 0*8
 #
-# If the supplied number is 'big-endian', then the 
-#
-#
 
-BIT_LENGTH="${#BINARY}";
-
-if [ "$BIT_LENGTH" -gt 32 ]; then
-	echo "$0; the maximum length is 32-bits.">&2;
-	exit 2;
+#if signing output numbers, output a - if the number is negative
+if [ $OUTPUT_SIGNED_NUMBERS -eq 1 ]; then
+	if [ $BIT_ORDER -eq 1 ]; then
+		OFFSET=$(($OFFSET+1));
+		if [ "$(echo $BINARY | cut -c 1)" = "1" ]; then
+			#negative little endian number
+			echo -n '-';
+		fi
+	else
+		OFFSET=$(($OFFSET-1));
+		if [ "$(echo $BINARY | cut -c $BIT_LENGTH)" = "1" ]; then
+			#negative big endian number
+			echo -n '-';
+		fi
+	fi
 fi
 
 CHAR_IDX=1;
 
-case "$BIT_ORDER" in
-	big-endian) OFFSET=1; ;;
-	little-endian) OFFSET=$BIT_LENGTH; ;;
-esac
-
 TOTAL=0;
 
-while true; do	
+BIT="";
+
+while true; do
 	BIT=$(echo $BINARY | cut -c "$CHAR_IDX");
-	
-	if [ "$BIT" -eq 1 ]; then
-		case "$OFFSET" in
+
+	if [ $BIT -eq 1 ]; then
+		case $OFFSET in
 			1) TOTAL=$((TOTAL+1)); ;;
 			2) TOTAL=$((TOTAL+2)); ;;
 			3) TOTAL=$((TOTAL+4)); ;;
@@ -131,19 +173,24 @@ while true; do
 			32) TOTAL=$((TOTAL+2147483648)); ;;
 		esac
 	fi
-	
-	if [ "$BIT_ORDER" = "big-endian" ]; then
+
+	if [ $CHAR_IDX -eq $BIT_LENGTH ]; then
+		break;
+	fi
+
+	CHAR_IDX=$(($CHAR_IDX+1));
+
+	if [ $BIT_ORDER -eq 0 ]; then
 		OFFSET=$((OFFSET+1));
 	else
 		OFFSET=$((OFFSET-1));
 	fi
-	
-	CHAR_IDX=$(($CHAR_IDX+1));
-	
-	if [ "$CHAR_IDX" -gt "$BIT_LENGTH" ]; then
-		break;
-	fi
 done;
 
-echo $TOTAL;
+printf $TOTAL;
+
+if [ $NEWLINE_SUFFIX_OUTPUT -eq 1 ]; then
+	printf "\n";
+fi
+
 exit 0;
