5dc0737415e8f8d3cd46f68e48c72867fe47f0b7
Dylan <sunny73cr@protonmail.com>
2024-09-21 09:46:59 +1200

    modify: convert_layer_2_protocol_id_to_name.sh - improve debug output

--- SCRIPT_HELPERS/convert_layer_2_protocol_id_to_name.sh
+++ SCRIPT_HELPERS/convert_layer_2_protocol_id_to_name.sh
@@ -1,72 +1,75 @@
 #!/bin/sh
 
-usage () {
-	echo "Usage: $0 --id <string>">&2;
+print_usage_then_exit () {
+	printf "Usage: $0 <argument>\n">&2;
+	printf " --id 0xYYYY (where Y is A-F, or 0-9).\n">&2;
+	printf "\n">&2;
 	exit 2;
 }
 
 ID="";
 
 while true; do
-	case "$1" in
-		--id )
-			ID="$2";
-			#if not enough arguments
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+	case $1 in
+		--id)
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			elif [ "$2" = "" ]; then
+				print_usage_then_exit;
+			else
+				ID=$2;
+				shift 2;
+			fi
 		;;
-		"" ) break; ;;
-		*)
-			echo "">&2;
-			echo "Unrecognised option: $1 $2">&2;
-			usage;
-		;;
+		"") break; ;;
+		*) printf "Unrecognised argument $1. ">&2; print_usage_then_exit; ;;
 	esac
 done
 
 if [ -z $ID ]; then
-	echo "$0; you must provide an id (--id <number>).">&2;
-	exit 2;
+	echo "\nMissing --id. ">&2;
+	print_usage_then_exit;
 fi
 
 case $ID in
-	"0x0800") echo "IPV4"; exit 0; ;;				# IP (IPv4)
-	"0x0805") echo "$0; unsupported protocol">&2; exit 2; ;;	# X25
-	"0x0806") echo "ARP"; exit 0; ;;				# Address Resolution Protocol
-	"0x0808") echo "$0; unsupported protocol">&2; exit 2; ;;	# Frame Relay ARP [RFC1701]
-	"0x08FF") echo "$0; unsupported protocol">&2; exit 2; ;;	# G8BPQ AX.25 over Ethernet
-	"0x22F3") echo "$0; unsupported protocol">&2; exit 2; ;;	# TRILL [RFC6325]
-	"0x22F4") echo "$0; unsupported protocol">&2; exit 2; ;;	# TRILL L2-IS-IS [RFC6325]
-	"0x6558") echo "$0; unsupported protocol">&2; exit 2; ;;	# Transparent Ethernet Bridging [RFC1701]
-	"0x6559") echo "$0; unsupported protocol">&2; exit 2; ;;	# Raw Frame Relay [RFC1701]
-	"0x8035") echo "$0; unsupported protocol">&2; exit 2; ;;	# Reverse ARP [RFC903]
-	"0x809B") echo "$0; unsupported protocol">&2; exit 2; ;;	# Appletalk
-	"0x80F3") echo "$0; unsupported protocol">&2; exit 2; ;;	# Appletalk Address Resolution Protocol
-	"0x8137") echo "$0; unsupported protocol">&2; exit 2; ;;	# Novell IPX
-	"0x8191") echo "$0; unsupported protocol">&2; exit 2; ;;	# NetBEUI
-	"0x86DD") echo "IPV6"; exit 0; ;;				# IP version 6
-	"0x880B") echo "$0; unsupported protocol">&2; exit 2; ;;	# Point-to-Point Protocol
-	"0x8847") echo "$0; unsupported protocol">&2; exit 2; ;;	# MPLS [RFC5332]
-	"0x8848") echo "$0; unsupported protocol">&2; exit 2; ;;	# MPLS with upstream-assigned label [RFC5332]
-	"0x884C") echo "$0; unsupported protocol">&2; exit 2; ;;	# MultiProtocol over ATM
-	"0x8863") echo "$0; unsupported protocol">&2; exit 2; ;;	# PPP over Ethernet discovery stage
-	"0x8864") echo "$0; unsupported protocol">&2; exit 2; ;;	# PPP over Ethernet session stage
-	"0x8884") echo "$0; unsupported protocol">&2; exit 2; ;;	# Frame-based ATM Transport over Ethernet
-	"0x888E") echo "$0; unsupported protocol">&2; exit 2; ;;	# EAP over LAN [802.1x]
-	"0x88C7") echo "$0; unsupported protocol">&2; exit 2; ;;	# EAPOL Pre-Authentication [802.11i]
-	"0x88CC") echo "$0; unsupported protocol">&2; exit 2; ;;	# Link Layer Discovery Protocol [802.1ab]
-	"0x88E5") echo "$0; unsupported protocol">&2; exit 2; ;;	# Media Access Control Security [802.1ae]
-	"0x88E7") echo "$0; unsupported protocol">&2; exit 2; ;;	# Provider Backbone Bridging [802.1ah]
-	"0x88F5") echo "$0; unsupported protocol">&2; exit 2; ;;	# Multiple VLAN Registration Protocol [802.1q]
-	"0x88F7") echo "$0; unsupported protocol">&2; exit 2; ;;	# Precision Time Protocol
-	"0x8906") echo "$0; unsupported protocol">&2; exit 2; ;;	# Fibre Channel over Ethernet
-	"0x8914") echo "$0; unsupported protocol">&2; exit 2; ;;	# FCoE Initialization Protocol
-	"0x8915") echo "$0; unsupported protocol">&2; exit 2; ;;	# RDMA over Converged Ethernet
-	"0xA0ED") echo "$0; unsupported protocol">&2; exit 2; ;;	# LoWPAN encapsulation
-	"0x8100") echo "VLAN-C"; exit 0; ;;				# VLAN tagged frame [802.1q]
-	"0x88A8") echo "VLAN-S"; exit 0; ;;				# QinQ Service VLAN tag identifier [802.1q]
-	*) #Unknown
-		echo "$0; invalid EtherType/Layer 2 Protocol ID.">&2;
-		exit 2;
+	"0x0800") echo "IPV4"; exit 0; ;; 	# IP (IPv4)
+	"0x0805") echo exit 1; ;;		# X25
+	"0x0806") echo "ARP"; exit 0; ;;	# Address Resolution Protocol
+	"0x0808") echo exit 1; ;;		# Frame Relay ARP [RFC1701]
+	"0x08FF") echo exit 1; ;;		# G8BPQ AX.25 over Ethernet
+	"0x22F3") echo exit 1; ;;		# TRILL [RFC6325]
+	"0x22F4") echo exit 1; ;;		# TRILL L2-IS-IS [RFC6325]
+	"0x6558") echo exit 1; ;;		# Transparent Ethernet Bridging [RFC1701]
+	"0x6559") echo exit 1; ;;		# Raw Frame Relay [RFC1701]
+	"0x8035") echo exit 1; ;;		# Reverse ARP [RFC903]
+	"0x809B") echo exit 1; ;;		# Appletalk
+	"0x80F3") echo exit 1; ;;		# Appletalk Address Resolution Protocol
+	"0x8137") echo exit 1; ;;		# Novell IPX
+	"0x8191") echo exit 1; ;;		# NetBEUI
+	"0x86DD") echo "IPV6"; exit 0; ;;	# IP version 6
+	"0x880B") echo exit 1; ;;		# Point-to-Point Protocol
+	"0x8847") echo exit 1; ;;		# MPLS [RFC5332]
+	"0x8848") echo exit 1; ;;		# MPLS with upstream-assigned label [RFC5332]
+	"0x884C") echo exit 1; ;;		# MultiProtocol over ATM
+	"0x8863") echo exit 1; ;;		# PPP over Ethernet discovery stage
+	"0x8864") echo exit 1; ;;		# PPP over Ethernet session stage
+	"0x8884") echo exit 1; ;;		# Frame-based ATM Transport over Ethernet
+	"0x888E") echo exit 1; ;;		# EAP over LAN [802.1x]
+	"0x88C7") echo exit 1; ;;		# EAPOL Pre-Authentication [802.11i]
+	"0x88CC") echo exit 1; ;;		# Link Layer Discovery Protocol [802.1ab]
+	"0x88E5") echo exit 1; ;;		# Media Access Control Security [802.1ae]
+	"0x88E7") echo exit 1; ;;		# Provider Backbone Bridging [802.1ah]
+	"0x88F5") echo exit 1; ;;		# Multiple VLAN Registration Protocol [802.1q]
+	"0x88F7") echo exit 1; ;;		# Precision Time Protocol
+	"0x8906") echo exit 1; ;;		# Fibre Channel over Ethernet
+	"0x8914") echo exit 1; ;;		# FCoE Initialization Protocol
+	"0x8915") echo exit 1; ;;		# RDMA over Converged Ethernet
+	"0xA0ED") echo exit 1; ;;		# LoWPAN encapsulation
+	"0x8100") echo "VLAN-C"; exit 0; ;;	# VLAN tagged frame [802.1q]
+	"0x88A8") echo "VLAN-S"; exit 0; ;;	# QinQ Service VLAN tag identifier [802.1q]
+	*) 					#Unknown
+		echo "\nInvalid --id">&2;
+		print_usage_then_exit;
 	;;
 esac
 
