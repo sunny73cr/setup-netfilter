7584345b834adf71b226b4e71db8befa57677016
Dylan <sunny73cr@protonmail.com>
2024-09-21 07:41:41 +1200

    modify: check_mac_address_is_public.sh - improve debug output

--- SCRIPT_HELPERS/check_mac_address_is_public.sh
+++ SCRIPT_HELPERS/check_mac_address_is_public.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-if [ -z "$ENV_SETUP_NFT" ]; then printf "setup-netfilter: set ENV_SETUP_NFT to the absolute path of the setup-netfilter directory first.">&2; exit 4; fi
+if [ -z "$ENV_SETUP_NFT" ]; then printf "setup-netfilter: set ENV_SETUP_NFT to the absolute path of the setup-netfilter directory first.\n">&2; exit 4; fi
 
 DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS="$ENV_SETUP_NFT/SCRIPT_HELPERS/check_mac_address_is_valid.sh";
 
 if [ ! -x $DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS ]; then
-	printf "$0; script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS\" is missing or is not executable.\n">&2;
+	printf "$0: dependency: \"$DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS\" is missing or is not executable.\n">&2;
 	exit 3;
 fi
 
 print_usage_and_exit () {
-	echo "Usage: $0 --address XX:XX:XX:XX:XX:XX, where X is a-f, A-F, or 0-9: hexadecimal.">&2;
+	printf "Usage: $0 <argument>\n">&2
+	printf " --address XX:XX:XX:XX:XX:XX (where X is a-f, A-F, or 0-9)\n">&2;
+	printf "\n">&2;
 	exit 2;
 }
 
@@ -19,7 +21,7 @@ if [ "$1" = "" ]; then print_usage_and_exit; fi
 ADDRESS="";
 
 while true; do
-	case "$1" in
+	case $1 in
 		--address)
 			if [ $# -lt 2 ]; then
 				print_usage_then_exit;
@@ -31,15 +33,20 @@ while true; do
 			fi
 		;;
 		"") break; ;;
-		*) printf "Unrecognised argument - ">&2; print_usage_and_exit; ;;
+		*) printf "Unrecognised argument $1. ">&2; print_usage_and_exit; ;;
 	esac
 done
+
+if [ "$ADDRESS" ]; then
+	printf "\nMissing --address. ">&2;
+	print_usage_then_exit;
+fi
 
 $DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS --address "$ADDRESS"
 case $? in
 	0) ;;
-	1) printf "$0: the mac address you provided in invalid. It must be in the form XX:XX:XX:XX:XX:XX, where X is a-f, or A-F or 0-9.\n">&2; exit 2; ;;
-	*) printf "$0: script dependency failure: \"\" produced a failure exit code.\n">&2 exit 3 ;;
+	1) printf "\nInvalid --address. ">&2; print_usage_then_exit; ;;
+	*) printf "$0: dependency: \"$DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS\" produced a failure exit code.\n">&2 exit 3 ;;
 esac
 
 FIRST_OCTET=$(echo "$ADDRESS" | cut -d ':' -f 1);
