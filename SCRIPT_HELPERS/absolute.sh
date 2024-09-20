077e68a8e64a6cc70d4b5cc06d6e9b2d8b70553d
Dylan <sunny73cr@protonmail.com>
2024-09-21 01:36:41 +1200

    modify: absolute.sh - improve debug output, add an option to suffix the output with a newline.

--- SCRIPT_HELPERS/absolute.sh
+++ SCRIPT_HELPERS/absolute.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 print_usage_then_exit () {
-	echo "Usage: $0 <arguments>">&2;
-	echo "--number <number>">&2;
-	echo "">&2;
-	echo "Developer / Special use flags:">&2;
-	echo "--only-validate">&2;
-	echo "Exit after performing validation">&2;
-	echo "">&2;
+	printf "Usage: $0 <arguments>\n">&2;
+	printf " --number <number>\n">&2;
+	printf "\n">&2;
+	printf " Optional: --only-validate\n">&2;
+	printf " Note: causes the program to exit after performing validation\n">&2;
+	printf "\n">&2;
+	printf " Optional: --newline-suffix-output\n">&2;
+	printf " Note: this appends a newline to the output.\n">&2;
+	printf "\n">&2;
 	exit 2;
 }
 
@@ -15,6 +17,7 @@ if [ "$1" = "" ]; then print_usage_then_exit; fi
 
 NUMBER="";
 ONLY_VALIDATE=0;
+NEWLINE_SUFFIX_OUTPUT=0;
 
 while true; do
 	case "$1" in
@@ -22,8 +25,8 @@ while true; do
 			#not enough arguments
 			if [ $# -lt 2 ]; then
 				print_usage_then_exit;
-			#valie is empty
-			elif [ "$2" = "" ] || [ "$(echo $2 | grep -G '^-')" != "" ]; then
+			#value is empty
+			elif [ "$2" = "" ]; then
 				print_usage_then_exit;
 			else
 				NUMBER=$2;
@@ -34,18 +37,22 @@ while true; do
 			ONLY_VALIDATE=1;
 			shift 1;
 		;;
+		--newline-suffix-output)
+			NEWLINE_SUFFIX_OUTPUT=1;
+			shift 1;
+		;;
 		"") break; ;;
 		*) printf "Unrecognised argument - ">&2; print_usage_then_exit; ;;
 	esac
 done
 
 if [ -z "$NUMBER" ]; then
-	echo "$0; you must provide a number.">&2;
+	printf "$0; you must provide a number.\n">&2;
 	exit 2;
 fi
 
-if [ "$(echo "$NUMBER" | grep -E '[-]{0,1}[0-9]+')" = "" ]; then
-	echo "$0; the string you provided is not a number.">&2;
+if [ "$(echo "$NUMBER" | grep -E '[-]{0,1}[0-9]{1,64}')" = "" ]; then
+	printf "$0: you must provide a positive or negative number up to 64 digits in length (including 0).\n">&2;
 	exit 2;
 fi
 
@@ -54,14 +61,18 @@ if [ $ONLY_VALIDATE -eq 1 ]; then exit 0; fi;
 STRING_LENGTH=${#NUMBER};
 
 #if the number is negative
-if [ "$(echo $NUMBER | cut -c 1)" = "-" ]; then
+if [ "$(echo "$NUMBER" | cut -c 1)" = "-" ]; then
 	#return a substring and truncate the sign
-	RESULT=$(echo $NUMBER | cut -c "2-$STRING_LENGTH");
+	RESULT=$(echo "$NUMBER" | cut -c "2-$STRING_LENGTH");
 else
 	#the number is positive, return it.
 	RESULT=$NUMBER;
 fi
 
-echo $RESULT;
+printf "$RESULT";
+
+if [ $NEWLINE_SUFFIX_OUTPUT -eq 1 ]; then
+	printf "\n";
+fi
 
 exit 0;
