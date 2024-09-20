7ff44ddf939de252463286b787e8bf159edd4e08
Dylan <sunny73cr@protonmail.com>
2024-09-21 02:28:56 +1200

    modify: check_mac_address_is_private.sh - improve debug output, improve usage output

--- SCRIPT_HELPERS/check_mac_address_is_private.sh
+++ SCRIPT_HELPERS/check_mac_address_is_private.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -z "$ENV_SETUP_NFT" ]; then printf "Set ENV_SETUP_NFT to the absolute path of the setup-netfilter directory first.">&2; exit 4; fi
+if [ -z "$ENV_SETUP_NFT" ]; then printf "setup-netfilter: set ENV_SETUP_NFT to the absolute path of the setup-netfilter directory first.">&2; exit 4; fi
 
 DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS="$ENV_SETUP_NFT/SCRIPT_HELPERS/check_mac_address_is_valid.sh";
 
@@ -10,7 +10,7 @@ if [ ! -x $DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS ]; then
 fi
 
 print_usage_then_exit () {
-	echo "Usage: $0 --address <string>">&2;
+	echo "Usage: $0 --address XX:XX:XX:XX:XX:XX, where X is a-f, A-F, 0-9: hexadecimal">&2;
 	exit 2;
 }
 
@@ -19,11 +19,11 @@ if [ "$1" = "" ]; then print_usage_then_exit; fi
 ADDRESS="";
 
 while true; do
-	case "$1" in
+	case $1 in
 		--address )
 			if [ $# -lt 2 ]; then
 				print_usage_then_exit;
-			elif [ "$2" = "" ] || [ "$(echo $2 | grep -E '^-')" != "" ]; then
+			elif [ "$2" = "" ]; then
 				print_usage_then_exit;
 			else
 				ADDRESS=$2;
@@ -38,13 +38,13 @@ done
 $DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS --address "$ADDRESS"
 case $? in
 	0) ;;
-	1) printf "$0: you provided an invalid mac address. It must be in the form XX:XX:XX:XX:XX:XX, where X is a-f, or A-F, or 0-9: hexadecimal.">&2; exit 2; ;;
-	*) printf "$0: script dependency failure: \"\" produced a failure status code.">&2; exit 3; ;;
+	1) printf "$0: you provided an invalid mac address. It must be in the form XX:XX:XX:XX:XX:XX, where X is a-f, or A-F, or 0-9: hexadecimal.\n">&2; exit 2; ;;
+	*) printf "$0: script dependency failure: \"$DEPENDENCY_SCRIPT_PATH_VALIDATE_MAC_ADDRESS\" produced a failure status code.\n">&2; exit 3; ;;
 esac
 
 FIRST_OCTET=$(echo "$ADDRESS" | cut -d ':' -f 1);
 
-MASK_FIRST_OCTET=$(( $FIRST_OCTET&2 ));
+MASK_FIRST_OCTET=$(( $FIRST_OCTET & 2 ));
 
 if [ "$MASK_FIRST_OCTET" -ne 0 ]; then
 #the 2nd least significant bit in the first octet is 1.
