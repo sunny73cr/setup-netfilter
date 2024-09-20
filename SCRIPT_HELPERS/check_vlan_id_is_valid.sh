3c9159825dd979199df50d223322518d2cc9f607
Dylan <sunny73cr@protonmail.com>
2024-09-20 16:21:35 +1200

    modify: check_vlan_id_is_valid.sh - improve arg parsing, improve debug output, improve exit codes

--- SCRIPT_HELPERS/check_vlan_id_is_valid.sh
+++ SCRIPT_HELPERS/check_vlan_id_is_valid.sh
@@ -1,43 +1,44 @@
 #!/bin/sh
 
-usage () {
+print_usage_then_exit () {
 	echo "Usage: $0 --id <string>">&2;
 	exit 2;
 }
 
+if [ "$1" = "" ]; then print_usage_then_exit; fi
+
 ID="";
 
-if [ "$1" = "" ]; then usage; fi
-
 while true; do
-	case "$1" in
-		--id )
-			ID="$2";
-			#if not enough arguments
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+	case $1 in
+		--id)
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			elif [ "$2" = "" ] || [ "$(echo $2 | grep -E '^-')" != "" ]; then
+				print_usage_then_exit;
+			else
+				ID=$2;
+				shift 2;
+			fi
 		;;
-		"" ) break; ;;
-		*)
-			echo "">&2;
-			echo "Unrecognised option: $1 $2">&2;
-			usage;
-		;;
+		"") break; ;;
+		*) printf "Unrecognised argument - ">&2; print_usage_then_exit; ;;
 	esac
 done
 
 if [ -z "$ID" ]; then
-	echo "$0; you must provide an id (--id <number>).">&2;
+	echo "$0; you must provide an id (--id number, where number is between 1 and 4096).">&2;
 	exit 2;
 fi
 
 if [ "$ID" -lt 1 ]; then
-	echo "$0; the port is too low.">&2;
-	exit 2;
+	echo "$0; the vlan id is too low.">&2;
+	exit 1;
 fi
 
 if [ "$ID" -gt 4096 ]; then
-	echo "$0; the port is too high.">&2;
-	exit 2;
+	echo "$0; the vlan id is too high.">&2;
+	exit 1;
 fi
 
 exit 0;
