74c62c79e3da944806375a72ac1519d12c409b58
Dylan <sunny73cr@protonmail.com>
2024-09-22 20:28:14 +1200

    modify: get_user_id_by_name.sh - improve debug output, arg parsing, improve features

--- SCRIPT_HELPERS/get_user_id_by_name.sh
+++ SCRIPT_HELPERS/get_user_id_by_name.sh
@@ -1,36 +1,52 @@
 #!/bin/sh
 
-usage () {
-	echo "Usage: $0 --name <string>">&2;
+print_usage_then_exit () {
+	printf "Usage: $0 <arguments>\n">&2;
+	printf " --name string\n">&2;
+	printf "\n";
+	printf " Optional: --newline-suffix-output\n";
+	printf " Note: this causes the program to output a newline after the result.\n";
+	printf "\n";
 	exit 2;
 }
 
-NAME="";
-
 if [ "$1" = "" ]; then usage; fi
 
+NAME="";
+NEWLINE_SUFFIX_OUTPUT=0;
+
 while true; do
-	case "$1" in
-		--name )
-			NAME="$2";
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+	case $1 in
+		--name)
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			elif [ "$2" = "" ]; then
+				print_usage_then_exit;
+			else
+				NAME=$2;
+				shift 2;
+			fi
 		;;
-		"" ) break; ;;
-		*)
-			echo "">&2;
-			echo "Unrecognised option: $1 $2">&2;
-			usage;
+		--newline-suffix-output)
+			NEWLINE_SUFFIX_OUTPUT=1;
+			shift 1;
 		;;
+		"") break; ;;
+		*) printf "\nUnrecognised argument $1. ">&2; print_usage_then_exit; ;;
 	esac
 done
 
-if [ -z $NAME ]; then
-	echo "$0; you must provide a user name (--name <string>)">&2;
-	exit 2;
+if [ -z "$NAME" ]; then
+	echo "\nInvalid --name. ">&2;
+	print_usage_then_exit;
 fi
 
 USER_ID="$(sudo cat /etc/passwd | grep -P "^$NAME:" | cut -d ":" -f 3)";
 
-echo $USER_ID;
+printf "$USER_ID";
+
+if [ $NEWLINE_SUFFIX_OUTPUT -eq 1 ]; then
+	printf "\n";
+fi
 
 exit 0;
