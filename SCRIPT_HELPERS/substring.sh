d2e2ca0e156d3d901c41869090c38fc5c25bf340
Dylan <sunny73cr@protonmail.com>
2024-09-22 20:22:56 +1200

    modify: substring.sh - revamp, improve logic, debug output, arg parsing, usage text, add features, etc

--- SCRIPT_HELPERS/substring.sh
+++ SCRIPT_HELPERS/substring.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
 print_usage_then_exit () {
-	echo "Usage: $0 --input <string> --start-idx <number> --length <number>">&2;
+	printf "Usage: $0 <arguments>\n">&2;
+	printf " --input string (cannot be empty, indirect upper bound of (2^32)-1, or max of unsigned int32.)\n">&2;
+	printf " --start-idx number (cannot be negative, limit of (2^32)-1, or max of unsigned int32.)\n">&2;
+	printf " --length number (cannot be below 1, limit of (2^32)-1, or max of unsigned int32.)\n">&2;
+	printf "\n">&2;
+	printf "Optional: --newline-suffix-output\n">&2;
+	printf " Note: this causes the program to output a newline after the result.\n">&2;
+	printf "\n">&2;
+	printf " Optional: --skip-validate\n">&2;
+	printf " Note: the program skips validating the arguments (if you know they are valid.)\n">&2;
+	printf "\n">&2;
+	printf " Optional: --only-validate\n">&2;
+	printf " Note: the program exits after validating the arguments.\n">&2;
+	printf "\n">&2;
 	exit 2;
 }
 
@@ -10,13 +23,16 @@ if [ "$1" = "" ]; then print_usage_then_exit; fi
 INPUT="";
 START_IDX="";
 LENGTH="";
+NEWLINE_SUFFIX_OUTUPUT=0;
+ONLY_VALIDATE=0;
+SKIP_VALIDATE=0;
 
 while true; do
 	case "$1" in
 		--input)
 			if [ $# -lt 2 ]; then
 				print_usage_then_exit;
-			elif [ "$2" = "" ] || [ "$(echo $2 | grep -E '^-')" != "" ]; then
+			elif [ "$2" = "" ]; then
 				print_usage_then_exit;
 			else
 				INPUT=$2;
@@ -26,7 +42,7 @@ while true; do
 		--start-idx)
 			if [ $# -lt 2 ]; then
 				print_usage_then_exit;
-			elif [ "$2" = "" ] || [ "$(echo $2 | grep -E '^-')" != "" ]; then
+			elif [ "$2" = "" ]; then
 				print_usage_then_exit;
 			else
 				START_IDX=$2;
@@ -36,50 +52,82 @@ while true; do
 		--length)
 			if [ $# -lt 2 ]; then
 				print_usage_then_exit;
-			elif [ "$2" = "" ] || [ "$(echo $2 | grep -E '^-')" != "" ]; then
+			elif [ "$2" = "" ]; then
 				print_usage_then_exit;
 			else
 				LENGTH=$2;
 				shift 2;
 			fi
 		;;
+		--newline-suffix-output)
+			NEWLINE_SUFFIX_OUTPUT=1;
+			shift 1;
+		;;
+		--skip-validate)
+			SKIP_VALIDATE=1;
+			shift 1;
+		;;
+		--only-validate)
+			ONLY_VALIDATE=1;
+			shift 1;
+		;;
 		"") break; ;;
-		*) printf "Unrecognised argument - ">&2; print_usage_then_exit; ;;
+		*) printf "\nUnrecognised argument - ">&2; print_usage_then_exit; ;;
 	esac
 done
 
-if [ "$(echo $START_IDX | grep -E '[-]{0,1}0|[1-9]{1,10}[0-9]{0,10}')" = "" ]; then
-	echo "$0; the start index must be a number.">&2;
-	exit 2;
+#Why, user?
+if [ $SKIP_VALIDATE -eq 1 ] && [ $ONLY_VALIDATE -eq 1 ]; then exit 0; fi
+
+if [ $SKIP_VALIDATE -eq 0 ]; then
+	if [ "$(echo $START_IDX | grep -E '0|[1-9]{1,10}[0-9]{0,10}')" = "" ]; then
+		printf "\nInvalid --start-idx. ">&2;
+		print_usage_then_exit;
+	fi
+
+	if [ $START_IDX -gt 4294967295 ]; then
+		printf "\nInvalid --start-idx. ">&2;
+		print_usage_then_exit;
+	fi
+
+	INPUT_LENGTH=${#INPUT};
+
+	if [ $START_IDX -lt 0 ] || [ $START_IDX -gt $(($INPUT_LENGTH-1)) ]; then
+		echo "\nInvalid --start-idx. ">&2;
+		print_usage_then_exit;
+	fi
+
+	if [ "$(echo $LENGTH | grep -P '[1-9]{1,10}[0-9]{0,10}')" = "" ]; then
+		echo "\nInvalid --length. ">&2;
+		print_usage_then_exit;
+	fi
+
+	if [ $START_IDX -gt 4294967295 ]; then
+		printf "\nInvalid --start-idx. ">&2;
+		print_usage_then_exit;
+	fi
+
+	START_IDX_PLUS_ONE=$(($START_IDX+1));
+
+	SUBSTRING_LENGTH=$(($START_IDX_PLUS_ONE+$LENGTH));
+
+	if [ $SUBSTRING_LENGTH -gt $INPUT_LENGTH ]; then
+		printf "\nsubstring is too long. ">&2;
+		exit 2;
+	fi
 fi
 
-INPUT_LENGTH=${#INPUT};
-
-if [ $START_IDX -lt 0 ] || [ $START_IDX -gt $(($INPUT_LENGTH-1)) ]; then
-	echo "$0; start index out of bounds.">&2;
-	exit 2;
-fi
-
-if [ "$(echo $LENGTH | grep -P '[-]{0,1}0|[1-9]{1,10}[0-9]{0,10}')" = "" ]; then
-	echo "$0; the length must be a number.">&2;
-	exit 2;
-fi
-
-START_IDX_PLUS_ONE=$(($START_IDX+1));
-
-SUBSTRING_LENGTH=$(($START_IDX_PLUS_ONE+$LENGTH));
-
-if [ $SUBSTRING_LENGTH -gt $INPUT_LENGTH ]; then
-	echo "$0; the substring is too long.">&2;
-	exit 2;
-fi
+if [ $ONLY_VALIDATE -eq 1 ]; then exit 0; fi
 
 SUBSTRING=$(echo $INPUT | awk \
 -v start_idx=$START_IDX_PLUS_ONE \
 -v len=$LENGTH \
 -- '{ string=substr($0, start_idx, len); print string; }' \
 );
+printf "$SUBSTRING";
 
-echo "$SUBSTRING";
+if [ $NEWLINE_SUFFIX_OUTPUT -eq 1 ]; then
+	printf "\n";
+fi
 
 exit 0;
