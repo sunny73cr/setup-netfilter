dde08ae16d96af6a16aa63cee2435908da9bc3e1
Dylan <sunny73cr@protonmail.com>
2024-09-25 16:08:15 +1200

    modify: try_match_bogon_mac_address_iana_multicast_reserved_range_5.sh - migrate to the new script format.

--- BOGONS/MAC/try_match_bogon_mac_address_iana_multicast_reserved_range_5.sh
+++ BOGONS/MAC/try_match_bogon_mac_address_iana_multicast_reserved_range_5.sh
@@ -1,36 +1,114 @@
 #!/bin/sh
 
-usage () {
-	echo "Usage: $0 --address-type <source|destination>">&2;
+print_description() {
+	printf "A program that prints a portion of an NFT rule match section. The match identifies packets with a source or destination MAC (ether) address between 01:00:5E:90:11:00 and 01:00:5E:FF:FF:FF (reserved) (IANA Reserved Range '5').\n">&2;
+}
+
+print_description_then_exit() {
+	print_description;
 	exit 2;
 }
 
+if [ "$1" = "-e" ]; then print_description_then_exit; fi
+
+print_dependencies() {
+	printf "printf\n">&2;
+	printf "\n">&2;
+}
+
+print_dependencies_then_exit() {
+	print_dependencies;
+	exit 2;
+}
+
+if [ "$1" = "-d" ]; then print_dependencies_then_exit; fi
+
+print_usage() {
+	printf "Usage: $0 <parameters>\n">&2;
+	printf " -e\n">&2;
+	printf " calling the program with the '-e' flag prints an explanation of the scripts' function or purpose.\n">&2;
+	printf " The program then exits with a code of 2 (user input error).\n">&2;
+	printf "\n">&2;
+	printf " -h\n">&2;
+	printf " calling the program with the '-h' flag prints an explanation of the scripts' parameters and their effect.\n">&2;
+	printf " The program then exits with a code of 2 (user input error).\n">&2;
+	printf "\n">&2;
+	printf " -d\n">&2;
+	printf " callling the program with the '-d' flags prints a (new-line separated, and terminated) list of the programs' dependencies (what it needs to run).\n">&2;
+	printf " The program then exits with a code of 2 (user input error).\n">&2;
+	printf "\n">&2;
+	printf " -ehd\n">&2;
+	printf " calling the program with the '-ehd' flag (or, ehd-ucate me) prints the description, the dependencies list, and the usage text.\n">&2;
+	printf " The program then exits with a code of 2 (user input error).\n">&2;
+	printf "\n">&2;
+	printf " Note that calling all scripts in a project with the flag '-ehd', then concatenating their output using file redirection (string > file),\n">&2;
+	printf " Is a nice and easy way to maintain documentation for your project.\n">&2;
+	printf "\n">&2;
+	printf "Parameters:\n">&2;
+	printf "\n">&2;
+	printf "  Required: --source-or-destination ('source' or 'destination', without quotes.)\n">&2;
+	printf "\n">&2;
+}
+
+print_usage_then_exit() {
+	print_usage;
+	exit 2;
+}
+
+if [ "$1" = "-h" ]; then print_usage_then_exit; fi
+
+if [ "$1" = "-ehd" ]; then print_description; printf "\n">&2; print_dependencies; printf "\n">&2; print_usage; exit 2; fi
+
+#ARGUMENTS:
+SOURCE_OR_DESTINATION="";
+
+#FLAGS:
+
 while true; do
-	case "$1" in
-		--address-type)
-			ADDRESS_TYPE="$2";
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
+	case $1 in
+		#Approach to parsing arguments:
+		#If the length of 'all arguments' is less than 2 (shift reduces this number),
+		#since this is an argument parameter and requires a value; the program cannot continue.
+		#Else, if the argument was provided, and its 'value' is empty; the program cannot continue.
+		#Else, assign the argument, and shift 2 (both the argument indicator and its value / move next)
+
+		--source-or-destination)
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			elif [ -z "$2" ]; then
+				print_usage_then_exit;
+			else
+				SOURCE_OR_DESTINATION=$2;
+				shift 2;
+			fi
 		;;
+
+		#Handle the case of 'end' of arg parsing; where all flags are shifted from the list,
+		#or the program was called without any parameters. exit the arg parsing loop.
 		"") break; ;;
-		*) usage; ;;
+
+		#Handle the case where an argument or flag was called that the program does not recognise.
+		#This should prefix the 'usage' text with the reason the program failed.
+		#The 'Standard Error' file descriptor is used to separate failure output or log messages from actual program output.
+		*) printf "\nUnrecognised argument $1. ">&2; print_usage_then_exit; ;;
+
 	esac
 done;
 
-if [ -z "$ADDRESS_TYPE" ]; then
-	echo "$0; you must supply a MAC address 'type' (try '--address-type source' or '--address-type destination')">&2;
-	exit 2;
+if [ -z "$SOURCE_OR_DESTINATION" ]; then
+	printf "\nMissing --source-or-destination. ">&2;
+	print_usage_then_exit;
 fi
 
-case "$ADDRESS_TYPE" in
-	"source") ETHER_ADDRESS_TYPE="saddr"; ;;
-	"destination") ETHER_ADDRESS_TYPE="daddr"; ;;
-	*)
-		echo "$0; unrecognised address type. (try '--address-type source' or '--address-type destination')"
-		exit 2;
-	;;
+MAC_ADDRESS_TYPE="";
+
+case "$SOURCE_OR_DESTINATION" in
+	"source") MAC_ADDRESS_TYPE="saddr"; ;;
+	"destination") MAC_ADDRESS_TYPE="daddr"; ;;
+	*) printf "\nInvalid --source-or-destination. ">&2; print_usage_then_exit; ;;
 esac
 
-echo "\t\tether $ETHER_ADDRESS_TYPE >= 01:00:5E:90:11:00 \\";
-echo "\t\tether $ETHER_ADDRESS_TYPE <= 01:00:5E:FF:FF:FF \\";
+printf "\\t\\tether $MAC_ADDRESS_TYPE >= 01:00:5E:90:11:00 \\\\\n";
+printf "\\t\\tether $MAC_ADDRESS_TYPE <= 01:00:5E:FF:FF:FF \\\\\n";
 
 exit 0;
