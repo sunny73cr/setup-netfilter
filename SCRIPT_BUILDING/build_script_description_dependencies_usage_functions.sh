5dfe5d2966632f0d360249276879ca372a534af5
Dylan <sunny73cr@protonmail.com>
2024-09-25 21:59:24 +1200

    create: build_script_description_dependencies_usage_functions.sh - a script that is useful for developers when writing scripts: this portion of "build_script_from_template" will generate code to both document the new script itself, and to assist the user during use. The script includes functionality for auto-documentation of a project, if done right.

---
+++ SCRIPT_BUILDING/build_script_description_dependencies_usage_functions.sh
@@ -0,0 +1,126 @@
+#!/bin/sh
+
+print_description() {
+	printf "A program that assists a developer in writing scripts by building a template for description, dependency list and usage functions.\n">&2;
+	printf "\n";
+}
+
+print_description_then_exit() {
+	print_description;
+	exit 2;
+}
+
+if [ "$1" = "-e" ]; then print_description_then_exit; fi
+
+print_dependencies() {
+	printf "printf\n">&2;
+	printf "\n">&2;
+}
+
+print_dependencies_then_exit() {
+	print_dependencies;
+	exit 2;
+}
+
+if [ "$1" = "-d" ]; then print_dependencies_then_exit; fi
+
+print_usage() {
+	printf "Usage: $0 <parameters>\n">&2;
+	printf " -e\n">&2;
+	printf " calling the program with the '-e' flag prints an explanation of the scripts' function or purpose.\n">&2;
+	printf " The program then exits with a code of 2 (user input error).\n">&2;
+	printf "\n">&2;
+	printf " -h\n">&2;
+	printf " calling the program with the '-h' flag prints an explanation of the scripts' parameters and their effect.\n">&2;
+	printf " The program then exits with a code of 2 (user input error).\n">&2;
+	printf "\n">&2;
+	printf " -d\n">&2;
+	printf " callling the program with the '-d' flags prints a (new-line separated, and terminated) list of the programs' dependencies (what it needs to run).\n">&2;
+	printf " The program then exits with a code of 2 (user input error).\n">&2;
+	printf "\n">&2;
+	printf " -ehd\n">&2;
+	printf " calling the program with the '-ehd' flag (or, ehd-ucate me) prints the description, the dependencies list, and the usage text.\n">&2;
+	printf " The program then exits with a code of 2 (user input error).\n">&2;
+	printf "\n">&2;
+	printf " Note that calling all scripts in a project with the flag '-ehd', then concatenating their output using file redirection (string > file),\n">&2;
+	printf " Is a nice and easy way to maintain documentation for your project.\n">&2;
+	printf "\n">&2;
+	printf "Parameters:\n">&2;
+	printf "\n None.">&2;
+	printf "\n">&2;
+	printf " This script should be called without parameters.\n">&2;
+	printf " It is most likely that you wish to concatenate the output to a shell script file.\n">&2;
+	printf "\n">&2;
+}
+
+print_usage_then_exit() {
+	print_usage;
+	exit 2;
+}
+
+if [ "$1" = "-h" ]; then print_usage_then_exit; fi
+
+if [ "$1" = "-ehd" ]; then print_description; printf "\n">&2; printf "Dependencies:\n">&2;  print_dependencies; printf "\n">&2; print_usage; exit 2; fi
+
+printf "print_description() {\n";
+printf "\tprintf \"A program that performs a function.\\\n\">&2;\n";
+printf "}\n";
+printf "\n";
+printf "print_description_then_exit() {\n";
+printf "\tprint_description;\n";
+printf "\texit 2;\n";
+printf "}\n";
+printf "\n";
+printf "if [ \"\$1\" = \"-e\" ]; then print_description_then_exit; fi\n";
+printf "\n";
+printf "print_dependencies() {\n";
+printf "\tprintf \"printf\\\n\">&2;\n";
+printf "\tprintf \"echo\\\n\">&2;\n";
+printf "\tprintf \"\\\n\">&2;\n";
+printf "}\n";
+printf "\n";
+printf "print_dependencies_then_exit() {\n";
+printf "\tprint_dependencies;\n";
+printf "\texit 2;\n";
+printf "}\n";
+printf "\n";
+printf "if [ \"\$1\" = \"-d\" ]; then print_dependencies_then_exit; fi\n";
+printf "\n";
+printf "print_usage() {\n";
+printf "\tprintf \"Usage: \$0 <parameters>\\\n\">&2;\n";
+printf "\tprintf \" -e\\\n\">&2;\n";
+printf "\tprintf \" calling the program with the '-e' flag prints an explanation of the scripts' function or purpose.\\\n\">&2;\n";
+printf "\tprintf \" The program then exits with a code of 2 (user input error).\\\n\">&2;\n";
+printf "\tprintf \"\\\n\">&2;\n";
+printf "\tprintf \" -h\\\n\">&2;\n";
+printf "\tprintf \" calling the program with the '-h' flag prints an explanation of the scripts' parameters and their effect.\\\n\">&2;\n";
+printf "\tprintf \" The program then exits with a code of 2 (user input error).\\\n\">&2;\n";
+printf "\tprintf \"\\\n\">&2;\n";
+printf "\tprintf \" -d\\\n\">&2;\n";
+printf "\tprintf \" callling the program with the '-d' flags prints a (new-line separated, and terminated) list of the programs' dependencies (what it needs to run).\\\n\">&2;\n";
+printf "\tprintf \" The program then exits with a code of 2 (user input error).\\\n\">&2;\n";
+printf "\tprintf \"\\\n\">&2;\n";
+printf "\tprintf \" -ehd\\\n\">&2;\n";
+printf "\tprintf \" calling the program with the '-ehd' flag (or, ehd-ucate me) prints the description, the dependencies list, and the usage text.\\\n\">&2;\n";
+printf "\tprintf \" The program then exits with a code of 2 (user input error).\\\n\">&2;\n";
+printf "\tprintf \"\\\n\">&2;\n";
+printf "\tprintf \" Note that calling all scripts in a project with the flag '-ehd', then concatenating their output using file redirection (string > file),\\\n\">&2;\n";
+printf "\tprintf \" Is a nice and easy way to maintain documentation for your project.\\\n\">&2;\n";
+printf "\tprintf \"\\\n\">&2;\n";
+printf "\tprintf \"Parameters:\\\n\">&2;\n";
+printf "\tprintf \"\\\n\">&2;\n";
+printf "\tprintf \"  Optional: --argument string (describe a required format).\\\n\">&2;\n";
+printf "\tprintf \"  Required: --argument string (describe a required format).\\\n\">&2;\n";
+printf "\tprintf \"\\\n\">&2;\n";
+printf "}\n";
+printf "\n";
+printf "print_usage_then_exit() {\n";
+printf "\tprint_usage;\n";
+printf "\texit 2;\n";
+printf "}\n";
+printf "\n";
+printf "if [ \"\$1\" = \"-h\" ]; then print_usage_then_exit; fi\n";
+printf "\n";
+printf "if [ \"\$1\" = \"-ehd\" ]; then print_description; printf \"\\\n\">&2; print_dependencies; printf \"\\\n\">&2; print_usage; exit 2; fi\n";
+
+exit 0;
