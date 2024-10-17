cf9f7d2d276373f9a4d0fc53201651460fdf1c6c
Dylan <sunny73cr@protonmail.com>
2024-10-17 14:05:23 +1300

    modify: try_match_interface.sh - migrate the script to the new format

--- LAYER_1/try_match_interface.sh
+++ LAYER_1/try_match_interface.sh
@@ -1,88 +1,183 @@
 #!/bin/sh
 
-DEP_SCRIPT_PATH_VALIDATE_IFACE_BY_NAME="./SCRIPT_HELPERS/check_interface_exists_by_name.sh";
+if [ -z "$ENV_SETUP_NFT" ]; then printf "setup-netfilter: set ENV_SETUP_NFT to the root path of the setup-netfilter directory before continuing.\n">&2; exit 4; fi
 
-if [ ! -x "$DEP_SCRIPT_PATH_VALIDATE_IFACE_BY_NAME" ]; then
-	echo "$0; script dependency failure: \"$DEP_SCRIPT_PATH_VALIDATE_IFACE_BY_NAME\" is missing or is not executable.">&2;
+DEPENDENCY_PATH_SCRIPT_NAME="$ENV_SETUP_NFT/path_to_script.sh";
+
+if [ ! -x $DEPENDENCY_PATH_SCRIPT_NAME ]; then
+	printf "$0: dependency: \"$DEPENDENCY_PATH_NAME\" is missing or is not executable.\n">&2;
 	exit 3;
 fi
 
-DEP_SCRIPT_PATH_VALIDATE_VLAN_ID="./SCRIPT_HELPERS/check_vlan_id_is_valid.sh";
+DEPENDENCY_PATH_VALIDATE_IFACE_BY_NAME="$ENV_SETUP_NFT/SCRIPT_HELPERS/check_interface_exists_by_name.sh";
 
-if [ ! -x "$DEP_SCRIPT_PATH_VALIDATE_VLAN_ID" ]; then
-	echo "$0; script dependency failure: \"$DEP_SCRIPT_PATH_VALIDATE_VLAN_ID\" is missing or is not executable.">&2;
+if [ ! -x $DEPENDENCY_PATH_VALIDATE_IFACE_BY_NAME ]; then
+	printf "$0: dependency: \"$DEPENDENCY_PATH_VALIDATE_IFACE_BY_NAME\" is missing or is not executable.\n">&2;
 	exit 3;
 fi
 
-check_success () {
-	if [ "$?" -ne 0 ]; then
-		echo "$0; cannot match on the interface.">&2;
-		exit 3;
-	fi
+print_description() {
+	printf "A program that prints part of an NFT rule 'match' section. The match intends to match an interface name.\n">&2;
 }
 
-usage () {
-	echo "">&2;
-	echo "Usage: $0 <arguments>">&2;
-	echo "--direction <in|out>">&2;
-	echo "--interface-name <string>">&2;
-	echo "optional: --vlan-id-dot1q <number>">&2;
+print_description_then_exit() {
+	print_description;
 	exit 2;
 }
 
+if [ "$1" = "-e" ]; then print_description_then_exit; fi
+
+print_dependencies() {
+	printf "printf\n">&2;
+	printf "\n">&2;
+}
+
+print_dependencies_then_exit() {
+	print_dependencies;
+	exit 2;
+}
+
+if [ "$1" = "-d" ]; then print_dependencies_then_exit; fi
+
+print_usage() {
+	printf "Usage: $0 <parameters>\n">&2;
+	printf " -e\n">&2;
+	printf " calling the program with the '-e' flag prints an explanation of the scripts' function or purpose.\n">&2;
+	printf " The program then exits with a code of 2 (user input error).\n">&2;
+	printf "\n">&2;
+	printf " -h\n">&2;
+	printf " calling the program with the '-h' flag prints an explanation of the scripts' parameters and their effect.\n">&2;
+	printf " The program then exits with a code of 2 (user input error).\n">&2;
+	printf "\n">&2;
+	printf " -d\n">&2;
+	printf " callling the program with the '-d' flags prints a (new-line separated, and terminated) list of the programs' dependencies (what it needs to run).\n">&2;
+	printf " The program then exits with a code of 2 (user input error).\n">&2;
+	printf "\n">&2;
+	printf " -ehd\n">&2;
+	printf " calling the program with the '-ehd' flag (or, ehd-ucate me) prints the description, the dependencies list, and the usage text.\n">&2;
+	printf " The program then exits with a code of 2 (user input error).\n">&2;
+	printf "\n">&2;
+	printf " Note that calling all scripts in a project with the flag '-ehd', then concatenating their output using file redirection (string > file),\n">&2;
+	printf " Is a nice and easy way to maintain documentation for your project.\n">&2;
+	printf "\n">&2;
+	printf "Parameters:\n">&2;
+	printf "\n">&2;
+	printf " Required: --direction in|out\n">&2;
+	printf "  Note: the direction the packet is travelling through the interface.\n">&2;
+	printf "\n">&2;
+	printf " Required: --interface-name\n">&2;
+	printf "  Note: the name of the interface to match.\n">&2;
+	printf "\n">&2;
+	printf "\n">&2;
+}
+
+print_usage_then_exit() {
+	print_usage;
+	exit 2;
+}
+
+if [ "$1" = "-h" ]; then print_usage_then_exit; fi
+
+if [ "$1" = "-ehd" ]; then print_description; printf "\n">&2; print_dependencies; printf "\n">&2; print_usage; exit 2; fi
+
+#ARGUMENTS:
 DIRECTION="";
 INTERFACE_NAME="";
-VLAN_ID_DOT1Q="";
 
-if [ "$1" = "" ]; then usage; fi
+#FLAGS:
+SKIP_VALIDATION=0;
+ONLY_VALIDATE=0;
 
 while true; do
-	case "$1" in
-		--direction )
-			DIRECTION="$2";
-			#not enough arguments
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
-		;;
-		--interface-name )
-			INTERFACE_NAME="$2";
-			#not enough arguments
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
-		;;
-		--vlan-id-dot1q )
-			VLAN_ID_DOT1Q="$2";
-			#not enough arguments
-			if [ "$#" -lt 2 ]; then usage; else shift 2; fi
-		;;
-		"" ) break; ;;
-		*)
-			echo "">&2;
-			echo "Unrecognised option: $1 $2">&2;
-			usage;
-		;;
-	esac
-done
+	case $1 in
+		#Approach to parsing arguments:
+		#If the length of 'all arguments' is less than 2 (shift reduces this number),
+		#since this is an argument parameter and requires a value; the program cannot continue.
+		#Else, if the argument was provided, and its 'value' is empty; the program cannot continue.
+		#Else, assign the argument, and shift 2 (both the argument indicator and its value / move next)
 
-case "$DIRECTION" in
-	"in") ;;
-	"out") ;;
-	*)
-		echo "$0; unrecognised direction. you must supply a direction. (try '--direction in' or '--direction out' without quotes.)">&2;
-		exit 2;
-	;;
+		--direction)
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			elif [ -z "$2" ]; then
+				print_usage_then_exit;
+			else
+				DIRECTION=$2;
+				shift 2;
+			fi
+		;;
+
+		--interface-name)
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			elif [ -z "$2" ]; then
+				print_usage_then_exit;
+			else
+				INTERFACE_NAME=$2;
+				shift 2;
+			fi
+		;;
+
+		#Approach to parsing flags:
+		#If the flag was provided, toggle on its value; then move next
+		#Or shift 1 / remove the flag from the list
+
+		--skip-validation)
+			SKIP_VALIDATION=1;
+			shift 1;
+		;;
+
+		--only-validate)
+			ONLY_VALIDATE=1;
+			shift 1;
+		;;
+
+		#Handle the case of 'end' of arg parsing; where all flags are shifted from the list,
+		#or the program was called without any parameters. exit the arg parsing loop.
+		"") break; ;;
+
+		#Handle the case where an argument or flag was called that the program does not recognise.
+		#This should prefix the 'usage' text with the reason the program failed.
+		#The 'Standard Error' file descriptor is used to separate failure output or log messages from actual program output.
+		*) printf "\nUnrecognised argument $1. ">&2; print_usage_then_exit; ;;
+
+	esac
+done;
+
+#Why, user?
+if [ $SKIP_VALIDATION -eq 1 ] && [ $ONLY_VALIDATE -eq 1 ]; then exit 0; fi
+
+if [ $SKIP_VALIDATION -eq 0 ]; then
+	if [ -n "$DIRECTION" ]; then
+		printf "\nMissing --direction. ">&2;
+		print_usage_then_exit;
+	fi
+
+	case $DIRECTION in
+		"in") ;;
+		"out") ;;
+		*) printf "\nInvalid --direction (try IN or OUT). ">&2; print_usage_then_exit; ;;
+	esac
+
+	if [ -n "$INTERFACE_NAME" ]; then
+		printf "\nMissing --interface-name. ">&2;
+		print_usage_then_exit;
+	fi
+
+	$DEPENDENCY_PATH_VALIDATE_IFACE_BY_NAME --name "$INTERFACE_NAME";
+	case $? in
+		0) ;;
+		1) printf "\nInvalid --interface-name. ">&2; print_usage_then_exit; ;;
+		*) printf "$0: dependency \"$DEPENDENCY_PATH_VALIDATE_IFACE_BY_NAME\" produced a failure exit code. ">&2; exit 3; ;;
+	esac
+fi
+
+if [ $ONLY_VALIDATE -eq 1 ]; then exit 0; fi
+
+printf "\\t\\t#Match Interface and Direction\n";
+case $DIRECTION in
+	IN) printf "\\t\\tmeta iifname $INTERFACE_NAME \\\\\n" ;;
+	OUT) printf "\\t\\tmeta oifname $INTERFACE_NAME \\\\\n" ;;
 esac
 
-if [ -n "$IS_VLAN_VALID" ]; then
-	IS_VLAN_VALID=$($DEP_SCRIPT_PATH_VALIDATE_VLAN_ID --id "$VLAN_ID_DOT1Q");
-	check_success;
-fi
-
-IS_IFACE_VALID=$($DEP_SCRIPT_PATH_VALIDATE_IFACE_BY_NAME --name "$INTERFACE_NAME");
-check_success;
-
-if [ $DIRECTION = "in" ]; then
-	echo "\t\tmeta iifname $INTERFACE_NAME \\";
-	exit 0;
-elif [ $DIRECTION = "out" ]; then
-	echo "\t\tmeta oifname $INTERFACE_NAME \\";
-	exit 0;
-fi
+exit 0;
