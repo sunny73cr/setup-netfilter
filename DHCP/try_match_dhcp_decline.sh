ecfa43b0b7353916a2a4073bde9fb83c35481ee5
Dylan <sunny73cr@protonmail.com>
2024-10-20 02:18:58 +1300

    modify: try_match_dhcp_decline.sh - migrate to a bit-level/raw payload expression format, generally improve output

--- DHCP/try_match_dhcp_decline.sh
+++ DHCP/try_match_dhcp_decline.sh
@@ -2,27 +2,21 @@
 
 if [ -z "$ENV_SETUP_NFT" ]; then printf "setup-netfilter: set ENV_SETUP_NFT to the root path of the setup-netfilter directory before continuing.\n">&2; exit 4; fi
 
-SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID="$ENV_SETUP_NFT/SCRIPT_HELPERS/check_ipv4_address_is_valid.sh";
-SCRIPT_DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID="$ENV_SETUP_NFT/SCRIPT_HELPERS/check_mac_address_is_valid.sh";
-SCRIPT_DEPENDENCY_PATH_CHECK_SERVICE_USER_ID_IS_VALID="$ENV_SETUP_NFT/SCRIPT_HELPERS/check_service_id_is_valid.sh";
+DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID="$ENV_SETUP_NFT/SCRIPT_HELPERS/check_mac_address_is_valid.sh";
 
-if [ ! -x $SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID ]; then
-	printf "$0: dependency $SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID is missing or is not executable.">&2;
+if [ ! -x $DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID ]; then
+	printf "$0: dependency: \"$DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID\" is missing or is not executable.\n">&2;
 	exit 3;
 fi
+DEPENDENCY_PATH_CHECK_SERVICE_USER_ID_IS_VALID="$ENV_SETUP_NFT/SCRIPT_HELPERS/check_service_user_id_is_valid.sh";
 
-if [ ! -x $SCRIPT_DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID ]; then
-	printf "$0: dependency $SCRIPT_DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID is missing or is not executable.">&2;
-	exit 3;
-fi
-
-if [ ! -x $SCRIPT_DEPENDENCY_PATH_CHECK_SERVICE_USER_ID_IS_VALID ]; then
-	printf "$0: dependency $SCRIPT_DEPENDENCY_PATH_CHECK_SERVICE_USER_ID_IS_VALID is missing or is not executable.">&2;
+if [ ! -x $DEPENDENCY_PATH_CHECK_SERVICE_USER_ID_IS_VALID ]; then
+	printf "$0: dependency: \"$DEPENDENCY_PATH_CHECK_SERVICE_USER_ID_IS_VALID\" is missing or is not executable.\n">&2;
 	exit 3;
 fi
 
 print_description() {
-	printf "A program that prints part of an NFT rule 'match' section. The match intends to identify DHCP Decline packets.\n">&2;
+	printf "A program that prints part of an NFT rule 'match' section. The match intends to identify DHCP DECLINE packets.\n">&2;
 }
 
 print_description_then_exit() {
@@ -33,8 +27,10 @@ print_description_then_exit() {
 if [ "$1" = "-e" ]; then print_description_then_exit; fi
 
 print_dependencies() {
+	printf "Dependencies: \n">&2;
 	printf "printf\n">&2;
-	printf "echo\n">&2;
+	printf "$DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID\n">&2;
+	printf "$DEPENDENCY_PATH_CHECK_SERVICE_USER_ID_IS_VALID\n">&2;
 	printf "\n">&2;
 }
 
@@ -47,45 +43,31 @@ if [ "$1" = "-d" ]; then print_dependencies_then_exit; fi
 
 print_usage() {
 	printf "Usage: $0 <parameters>\n">&2;
-	printf " -e\n">&2;
-	printf " calling the program with the '-e' flag prints an explanation of the scripts' function or purpose.\n">&2;
-	printf " The program then exits with a code of 2 (user input error).\n">&2;
-	printf "\n">&2;
-	printf " -h\n">&2;
-	printf " calling the program with the '-h' flag prints an explanation of the scripts' parameters and their effect.\n">&2;
-	printf " The program then exits with a code of 2 (user input error).\n">&2;
-	printf "\n">&2;
-	printf " -d\n">&2;
-	printf " callling the program with the '-d' flags prints a (new-line separated, and terminated) list of the programs' dependencies (what it needs to run).\n">&2;
-	printf " The program then exits with a code of 2 (user input error).\n">&2;
-	printf "\n">&2;
-	printf " -ehd\n">&2;
-	printf " calling the program with the '-ehd' flag (or, ehd-ucate me) prints the description, the dependencies list, and the usage text.\n">&2;
-	printf " The program then exits with a code of 2 (user input error).\n">&2;
-	printf "\n">&2;
-	printf " Note that calling all scripts in a project with the flag '-ehd', then concatenating their output using file redirection (string > file),\n">&2;
-	printf " Is a nice and easy way to maintain documentation for your project.\n">&2;
+	printf "Flags used by themselves: \n">&2;
+	printf " -e (prints an explanation of the functions' purpose) (exit code 2)\n">&2;
+	printf " -h (prints an explanation of the functions' available parameters, and their effect) (exit code 2)\n">&2;
+	printf " -d (prints the functions' dependencies: newline delimited list) (exit code 2)\n">&2
+	printf " -ehd (prints the above three) (exit code 2)\n">&2;
 	printf "\n">&2;
 	printf "Parameters:\n">&2;
 	printf "\n">&2;
-	printf " Optional: --server-address-ipv4 X.X.X.X (where X is 0-255)\n">&2;
-	printf "\n">&2;
-	printf " Optional: --requested-address-ipv4 X.X.X.X (where X is 0-255)\n">&2;
-	printf "\n">&2;
-	printf " Note: it is strongly recommended to supply both a server and client address.\n">&2;
-	printf "\n">&2;
 	printf " Optional: --client-mac-address XX:XX:XX:XX:XX:XX (where X is a-f, or A-F, or 0-9 - hexadecimal)\n">&2;
-	printf " Note: it is strongly recommended to supply the client mac address, if you know it.\n">&2;
+	printf "  Note: it is strongly recommended to supply the client mac address, if you know it.\n">&2;
+	printf "\n">&2;
+	printf " Note: it is strongly recommended to supply both a server and client address or network.\n">&2;
+	printf "\n">&2;
+	printf " Optional: --transaction-id x (where x is 0-4,294,967,296)\n">&2;
+	printf "  Note: the identifier of the DHCP transaction.\n">&2;
 	printf "\n">&2;
 	printf " Optional: --dhcp-service-uid X (where X is 1-65535)\n">&2;
-	printf " Note: it is strongly recommended to supply the user ID that is assigned to the DHCP server 'service' listed in the /etc/passwd file.\n">&2;
-	printf " Note: without this restriction, DHCP Decline packets are permitted to any program."
+	printf "  Note: it is stronly recommended to supply the user ID that is assigned to the DHCP server 'service' listed in the /etc/passwd file.\n">&2;
+	printf "  Note: without this restriction, DHCP DECLINE packets are permitted to any program.\n";
 	printf "\n">&2;
 	printf " Optional: --skip-validation\n">&2;
-	printf " Note: enabling this flag causes the program to skip validation (if you know the inputs are valid)\n">&2;
+	printf "  Note: causes the program to skip validating inputs (if you know they are valid).\n">&2;
 	printf "\n">&2;
-	printf " Optional: --only-validation\n">&2;
-	printf " Note: enabling this flag causes the program to exit after validating inputs.\n">&2;
+	printf " Optional: --only-validate\n">&2;
+	printf "  Note: causes the program to exit after validating inputs.\n">&2;
 	printf "\n">&2;
 }
 
@@ -99,10 +81,9 @@ if [ "$1" = "-h" ]; then print_usage_then_exit; fi
 if [ "$1" = "-ehd" ]; then print_description; printf "\n">&2; print_dependencies; printf "\n">&2; print_usage; exit 2; fi
 
 #ARGUMENTS:
-SERVER_ADDRESS_IPV4="";
-REQUESTED_ADDRESS_IPV4="";
 CLIENT_MAC_ADDRESS="";
 DHCP_SERVICE_UID="";
+TRANSACTION_ID="";
 
 #FLAGS:
 SKIP_VALIDATION=0;
@@ -116,28 +97,6 @@ while true; do
 		#Else, if the argument was provided, and its 'value' is empty; the program cannot continue.
 		#Else, assign the argument, and shift 2 (both the argument indicator and its value / move next)
 
-		--server-address-ipv4)
-			if [ $# -lt 2 ]; then
-				print_usage_then_exit;
-			elif [ -z "$2" ]; then
-				print_usage_then_exit;
-			else
-				SERVER_ADDRESS_IPV4=$2;
-				shift 2;
-			fi
-		;;
-
-		--requested-address-ipv4)
-			if [ $# -lt 2 ]; then
-				print_usage_then_exit;
-			elif [ -z "$2" ]; then
-				print_usage_then_exit;
-			else
-				REQUESTED_ADDRESS_IPV4=$2;
-				shift 2;
-			fi
-		;;
-
 		--client-mac-address)
 			if [ $# -lt 2 ]; then
 				print_usage_then_exit;
@@ -145,6 +104,17 @@ while true; do
 				print_usage_then_exit;
 			else
 				CLIENT_MAC_ADDRESS=$2;
+				shift 2;
+			fi
+		;;
+
+		--transaction-id)
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			elif [ -z "$2" ]; then
+				print_usage_then_exit;
+			else
+				TRANSACTION_ID=$2;
 				shift 2;
 			fi
 		;;
@@ -169,7 +139,7 @@ while true; do
 			shift 1;
 		;;
 
-		--only-validate)
+		--only-validation)
 			ONLY_VALIDATE=1;
 			shift 1;
 		;;
@@ -186,48 +156,52 @@ while true; do
 	esac
 done;
 
-#Why, user?
 if [ $SKIP_VALIDATION -eq 1 ] && [ $ONLY_VALIDATE -eq 1 ]; then exit 0; fi
 
 if [ $SKIP_VALIDATION -eq 0 ]; then
-	if [ -n "$SERVER_ADDRESS_IPV4" ]; then
-		$SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID --address $SERVER_ADDRESS_IPV4
-		case $? in
-			0) ;;
-			1) printf "\nInvalid --server-address-ipv4. "; print_usage_then_exit; ;;
-			*) printf "$0: dependency \"$SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID\" produced a failure exit code.\n"; exit 3 ;;
-		esac
-	fi
-
-	if [ -n "$REQUESTED_ADDRESS_IPV4" ]; then
-		$SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID --address $REQUESTED_ADDRESS_IPV4
-		case $? in
-			0) ;;
-			1) printf "\nInvalid --requested-address-ipv4. "; print_usage_then_exit; ;;
-			*) printf "$0: dependency \"$SCRIPT_DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID\" produced a failure exit code.\n"; exit 3 ;;
-		esac
-	fi
-
 	if [ -n "$CLIENT_MAC_ADDRESS" ]; then
-		$SCRIPT_DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID --address $CLIENT_MAC_ADDRESS
+		$DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID --address $CLIENT_MAC_ADDRESS
 		case $? in
 			0) ;;
-			1) printf "\nInvalid --client-mac-address. "; print_usage_then_exit; ;;
-			*) printf "$0: dependency \"$SCRIPT_DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID\" produced a failure exit code.\n"; exit 3 ;;
+			1) printf "\nInvalid --client-mac-address. \n">&2; print_usage_then_exit; ;;
+			*) printf "$0: dependency: \"$DEPENDENCY_PATH_CHECK_MAC_ADDRESS_IS_VALID\" produced a failure exit code ($?).\n">&2; exit 3; ;;
 		esac
+	fi
+
+	if [ -n "$TRANSACTION_ID" ]; then
+		if [ -z "$(echo $TRANSACTION_ID | grep '[0-9]\{1,10\}')" ]; then
+			printf "\nInvalid --transaction-id (must be a 1-10 digit number). ">&2;
+			print_usage_then_exit;
+		fi
+
+		if [ $TRANSACTION_ID -lt 0 ]; then
+			printf "\nInvalid --transaction-id (must be greater than or equal to 0). ">&2;
+			print_usage_then_exit;
+		fi
+
+		if [ $TRANSACTION_ID -gt 4294967295 ]; then
+			printf "\nInvalid --transaction-id (must be less than 4294967296). ">&2;
+			print_usage_then_exit;
+		fi
 	fi
 
 	if [ -n "$SERVICE_USER_ID" ]; then
-		$SCRIPT_DEPENDENCY_PATH_CHECK_SERVICE_USER_ID_IS_VALID --id $SERVICE_USER_ID
+		$DEPENDENCY_PATH_CHECK_SERVICE_USER_ID_IS_VALID --id $SERVICE_USER_ID
 		case $? in
 			0) ;;
-			1) printf "\nInvalid --dhcp-service-uid (confirm the /etc/passwd entry). "; print_usage_then_exit; ;;
-			*) printf "$0: dependency \"$SCRIPT_DEPENDENCY_PATH_CHECK_SERVICE_USER_ID_IS_VALID\" produced a failure exit code.\n"; exit 3 ;;
+			1) printf "\nInvalid --dhcp-service-uid. (confirm the /etc/passwd entry). \n">&2; print_usage_then_exit; ;;
+			*) printf "$0: dependency: \"$DEPENDENCY_PATH_CHECK_IPV4_ADDRESS_IS_VALID\" produced a failure exit code ($?).\n">&2; exit 3; ;;
 		esac
 	fi
 fi
 
 if [ $ONLY_VALIDATE -eq 1 ]; then exit 0; fi
+
+CLIENT_MAC_ADDRESS_CLEANED="";
+#nft expects hexadecimal format; format the MAC address
+if [ -n "$CLIENT_MAC_ADDRESS" ]; then
+	CLIENT_MAC_ADDRESS_CLEANED="0x$(echo $CLIENT_MAC_ADDRESS | sed 's/://g')";
+fi
 
 printf "\\t#DHCP message length is a minimum of 2000 bits, packet length should be greater than 250 bytes.\n";
 printf "\\t#Packet length should not be longer than 512 bytes to avoid fragmentation; DHCP messages should be delivered in a single transmission.\n";
@@ -241,57 +215,68 @@ else
 	printf "\\t\\t#meta skuid unknown - please consider the security implications\n";
 fi
 
+OFFSET_MARKER="ih";
+BIT_OFFSET_HEADER_BEGIN=0;
+BIT_OFFSET_OP_CODE=$BIT_OFFSET_HEADER_BEGIN;
+BIT_OFFSET_HARDWARE_ADDRESS_TYPE=$(($BIT_OFFSET_OP_CODE+8));
+BIT_OFFSET_HARDWARE_ADDRESS_LENGTH=$(($BIT_OFFSET_HARDWARE_ADDRESS_TYPE+8));
+BIT_OFFSET_HOPS=$(($BIT_OFFSET_HARDWARE_ADDRESS_LENGTH+8));
+BIT_OFFSET_XID=$(($BIT_OFFSET_HOPS+8));
+BIT_OFFSET_SECONDS=$(($BIT_OFFSET_XID+32));
+BIT_OFFSET_FLAGS=$(($BIT_OFFSET_SECONDS+16));
+BIT_OFFSET_CLIENT_IP_ADDRESS=$(($BIT_OFFSET_FLAGS+16));
+BIT_OFFSET_YOUR_IP_ADDRESS=$(($BIT_OFFSET_CLIENT_IP_ADDRESS+32));
+BIT_OFFSET_SERVER_IP_ADDRESS=$(($BIT_OFFSET_YOUR_IP_ADDRESS+32));
+BIT_OFFSET_GATEWAY_IP_ADDRESS=$(($BIT_OFFSET_SERVER_IP_ADDRESS+32));
+BIT_OFFSET_CLIENT_HARDWARE_ADDRESS=$(($BIT_OFFSET_GATEWAY_IP_ADDRESS+32));
+BIT_OFFSET_HARDWARE_ADDRESS_PADDING=$(($BIT_OFFSET_CLIENT_HARDWARE_ADDRESS+48));
+
 printf "\\t#DHCP OP Code of 1 (BOOTREQUEST)\n";
-printf "\\t\\t@ih,0,8 0x01 \\\\\n";
+printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_OP_CODE,8 0x01 \\\\\n";
 
 printf "\\t#HTYPE (Hardware Address Type) (1 Ethernet)\n";
-printf "\\t\\t@ih,8,8 1 \\\\\n";
+printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_HARDWARE_ADDRESS_TYPE,8 1 \\\\\n";
 
 printf "\\t#HLEN (Hardware Address Length) (6 Segment MAC)\n";
-printf "\\t\\t@ih,16,8 6 \\\\\n";
+printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_HARDWARE_ADDRESS_LENGTH,8 6 \\\\\n";
 
 printf "\\t#HOPS (Client sets to 0, optionally set by relay-agents)\n";
-printf "\\t\\t@ih,24,8 0 \\\\\n";
+printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_HOPS,8 0 \\\\\n";
 
-printf "\\t#XID (Transaction ID) client generated random number to associate communications\n";
-printf "\\t\\t@ih,32,32 != 0 \\\\\n";
+if [ -n "$TRANSACTION_ID" ]; then
+	printf "\\t#XID (Transaction ID) client generated random number to associate communications\n";
+	printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_XID,32 $TRANSACTION_ID \\\\\n";
+else
+	printf "\\t#TransactionID is not restricted - consider the implications.\n";
+fi
 
 printf "\\t#SECS (Seconds since the request was made)\n";
-printf "\\t#See RFC1541 - DHCP DECLINE should be 0 sec\n";
-printf "\\t\\t@ih,64,16 0 \\\\\n";
+printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_SECONDS,16 0 \\\\\n";
 
-printf "\\t#Flags: no flags for DHCP DECLINE, 16 zeroes, DHCPDECLINE is not broadcasted (See RFC1541)\n";
-printf "\\t\\t@ih,80,16 0 \\\\\n";
+printf "\\t#Flags: no flags for DHCP ACK, 16 zeroes, DHCPACK is not broadcasted (See RFC1541)\n";
+printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_FLAGS,16 0 \\\\\n";
 
-printf "\\t#CIADDR (Client IP Address)\n";
-printf "\\t#DHCPDECLINE does not require a client IP address\n";
-printf "\\t\\t@ih,96,32 0 \\\\\n";
+printf "\\t#Match CIADDR (Client IP Address)\n";
+printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_CLIENT_IP_ADDRESS,32 0 \\\\\n";
 
-printf "\\t#YIADDR (Your IP address) Your (client) IP address\n";
-printf "\\t\\t@ih,128,32 0 \\\\\n";
+printf "\\t#Match YIADDR (Your IP Address)\n";
+printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_YOUR_IP_ADDRESS,32 0 \\\\\n";
 
-printf "\\t#SIADDR (Server IP address) Returned in DHCPOFFER, DHCPACK, DHCPNAK\n";
-printf "\\t\\t@ih,160,32 0 \\\\\n";
+printf "\\t#Match SIADDR (Server IP Address)\n";
+printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_SERVER_IP_ADDRESS,32 0 \\\\\n";
 
-printf "\\t#GIADDR (Relay Agent IP address)\n";
-printf "\\t\\t@ih,192,32 0 \\\\\n";
+printf "\\t#Match GIADDR (Relay Agent IP address)\n";
+printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_GATEWAY_IP_ADDRESS,32 0 \\\\\n";
 
-printf "\\t#CHADDR (Client Hardware Address)\n";
-#printf "\\t\\t@ih,224,64 0 \\\\\n";
-printf "\\t#Confirm each segment of the MAC address matches\n";
 if [ -n "$CLIENT_MAC_ADDRESS" ]; then
-	printf "\\t\\t@ih,224,8 0x$(echo $CLIENT_MAC_ADDRESS | cut -d ':' -f 1) \\\\\n";
-	printf "\\t\\t@ih,232,8 0x$(echo $CLIENT_MAC_ADDRESS | cut -d ':' -f 2) \\\\\n";
-	printf "\\t\\t@ih,240,8 0x$(echo $CLIENT_MAC_ADDRESS | cut -d ':' -f 3) \\\\\n";
-	printf "\\t\\t@ih,248,8 0x$(echo $CLIENT_MAC_ADDRESS | cut -d ':' -f 4) \\\\\n";
-	printf "\\t\\t@ih,256,8 0x$(echo $CLIENT_MAC_ADDRESS | cut -d ':' -f 5) \\\\\n";
-	printf "\\t\\t@ih,264,8 0x$(echo $CLIENT_MAC_ADDRESS | cut -d ':' -f 6) \\\\\n";
+	printf "\\t#Match CHADDR (Client Hardware Address)\n";
+	printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_CLIENT_HARDWARE_ADDRESS,48 $CLIENT_MAC_ADDRESS_CLEANED \\\\\n";
 else
-	printf "\\t\\t#@ih,224,64 unrestricted - please consider the security implications\n";
+	printf "\\t\\t#Client Hardware Address unrestricted - please consider the security implications\n";
 fi
 
 printf "\\t#CHADDR Padding - pad to the full 128 bits - 48 consumed; 80 bits of padding\n";
-printf "\\t\\t@ih,272,80 0 \\\\\n";
+printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_HARDWARE_ADDRESS_PADDING,80 0 \\\\\n";
 
 printf "\\t#Cannot verify beyond the CHADDR as the server host name and boot file name fields may be used for options.\n";
 
