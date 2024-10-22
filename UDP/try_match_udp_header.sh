31a0974073e8b0c6021ceb9fbafe1425a0556302
Dylan <sunny73cr@protonmail.com>
2024-10-22 19:48:23 +1300

    modify: try_match_udp_header.sh - support ranges for most fields in the header, fix parameter dereferences, fix limits for validation, improve debug output

--- UDP/try_match_udp_header.sh
+++ UDP/try_match_udp_header.sh
@@ -37,70 +37,70 @@ if [ "$1" = "-d" ]; then print_dependencies_then_exit; fi
 
 print_usage() {
 	printf "Usage: $0 <parameters>\n">&2;
-	printf " -e\n">&2;
-	printf " calling the program with the '-e' flag prints an explanation of the scripts' function or purpose.\n">&2;
-	printf " The program then exits with a code of 2 (user input error).\n">&2;
-	printf "\n">&2;
-	printf " -h\n">&2;
-	printf " calling the program with the '-h' flag prints an explanation of the scripts' parameters and their effect.\n">&2;
-	printf " The program then exits with a code of 2 (user input error).\n">&2;
-	printf "\n">&2;
-	printf " -d\n">&2;
-	printf " callling the program with the '-d' flags prints a (new-line separated, and terminated) list of the programs' dependencies (what it needs to run).\n">&2;
-	printf " The program then exits with a code of 2 (user input error).\n">&2;
-	printf "\n">&2;
-	printf " -ehd\n">&2;
-	printf " calling the program with the '-ehd' flag (or, ehd-ucate me) prints the description, the dependencies list, and the usage text.\n">&2;
-	printf " The program then exits with a code of 2 (user input error).\n">&2;
-	printf "\n">&2;
-	printf " Note that calling all scripts in a project with the flag '-ehd', then concatenating their output using file redirection (string > file),\n">&2;
-	printf " Is a nice and easy way to maintain documentation for your project.\n">&2;
+	printf "Flags used by themselves: \n">&2;
+	printf " -e (prints an explanation of the functions' purpose) (exit code 2)\n">&2;
+	printf " -h (prints an explanation of the functions' available parameters, and their effect) (exit code 2)\n">&2;
+	printf " -d (prints the functions' dependencies: newline delimited list) (exit code 2)\n">&2
+	printf " -ehd (prints the above three) (exit code 2)\n">&2;
 	printf "\n">&2;
 	printf "Parameters:\n">&2;
 	printf "\n">&2;
 	printf " Optional: --offset-marker th|ih\n">&2;
-	printf "  Note: here, th means transport header (or the usual place of the UDP header).\n">&2;
-	printf "  Note: this is commonly what you are looking to match, unless the UDP header\n">&2;
-	printf "  Note: is part of the content in an encapsulating protocol (See ICMP Destination Unreachable).\n">&2;
-	printf "  Note: for these encapsulating protocols, the offset marker should be ih; with a predefined \"offset-header-begin\".\n">&2;
+	printf "  here, th means transport header (or the usual place of the UDP header).\n">&2;
+	printf "  this is commonly what you are looking to match, unless the UDP header\n">&2;
+	printf "  is part of the content in an encapsulating protocol (See ICMP Destination Unreachable).\n">&2;
+	printf "  for these encapsulating protocols, the offset marker should be ih; with a predefined \"offset-header-begin\".\n">&2;
 	printf "\n">&2;
 	printf " Optional: --offset-header-begin X (where x is a number from 0-65536)\n">&2;
-	printf "  Note: this indicates to the program where the beginning of the UDP header is.\n">&2;
-	printf "  Note: this is important for wncapsulating protocols such as an ICMP Destination Unreachable packet;\n">&2;
-	printf "  Note: otherwise, the header will simply not match.\n">&2;
+	printf "  this indicates to the program where the beginning of the UDP header is.\n">&2;
+	printf "  this is important for wncapsulating protocols such as an ICMP Destination Unreachable packet;\n">&2;
+	printf "  otherwise, the header will simply not match.\n">&2;
 	printf "\n">&2;
-	printf " Note: where --offset-marker is not provided, the default is \"th\" (the standard place to match a UDP header).\n">&2;
-	printf " Note: if --offset-header-begin is not provided, it will default to 0. If --offset marker is \"ih\", it will likely fail to match.\n">&2;
+	printf " where --offset-marker is not provided, the default is \"th\" (the standard place to match a UDP header).\n">&2;
+	printf " if --offset-header-begin is not provided, it will default to 0. If --offset marker is \"ih\", it will likely fail to match.\n">&2;
 	printf "\n">&2;
 	printf " Optional: --source-port x|x-x (where x is 1-65535)\n">&2;
-	printf "  Note: the source port. Depending on the IPV4/6 header, this could be either the server or client port.\n">&2;
-	printf "  Note: the source port argument may be a range of ports delmited by a hyphen (eg. x-x). The ports can be in ascending or descending order.\n">&2;
+	printf "  the source port. Depending on the IPV4/6 header, this could be either the server or client port.\n">&2;
+	printf "  the source port argument may be a range of ports delmited by a hyphen (eg. x-x). The ports can be in ascending or descending order.\n">&2;
 	printf "\n">&2;
 	printf " Optional: --destination-port x|x-x (where x is 1-65535)\n">&2;
-	printf "  Note: the destination port. Depending on the IPV4/6 header, this could be either the server or client port.\n">&2;
-	printf "  Note: the destination port argument may be a range of ports delmited by a hyphen (eg. x-x). The ports can be in ascending or descending order.\n">&2;
+	printf "  the destination port. Depending on the IPV4/6 header, this could be either the server or client port.\n">&2;
+	printf "  the destination port argument may be a range of ports delmited by a hyphen (eg. x-x). The ports can be in ascending or descending order.\n">&2;
 	printf "\n">&2;
 	printf " Optional: --length x (where x is 0-65536).\n">&2;
-	printf "  Note: match an exact packet length.\n">&2;
+	printf "  Restrict the packet to an exact length.\n">&2;
 	printf "\n">&2;
 	printf " Optional: --length-min x (where x is 0-65536).\n">&2;
-	printf "  Note: match a minimum packet length (and those that are shorter are not matched).\n">&2;
+	printf "  Restrict the packet to a minimum length.\n">&2;
 	printf "\n">&2;
 	printf " Optional: --length-max x (where x is 0-65536).\n">&2;
-	printf "  Note: match a maximum packet length (and those that are longer are not matched)\n">&2;
+	printf "  Restrict the packet to a maximum length.\n">&2;
 	printf "\n">&2;
-	printf " Note: you are not required to supply both a minimum and maximum packet length.\n">&2;
-	printf " Note: you must not supply an exact packet length alongside a minimum or maximum length restriction.\n">&2;
+	printf " You must not supply an exact packet length alongside a minimum or maximum length restriction.\n">&2;
+	printf " You are not required to supply both a minimum and maximum packet length.\n">&2;
+	printf " When supplied together, --length-min must be less than --length-max\n">&2;
 	printf "\n">&2;
 	printf " Optional: --checksum x (where x is 0-65536)\n">&2;
-	printf "  Note: this matches a specific packet checksum.\n">&2;
-	printf "  Note: this could be beneficial if you know the checksum of a previously sent/received packet.\n">&2;
+	printf "  Restrict the packet to an exact checksum.\n">&2;
+	printf "  this could be beneficial if you know the checksum of a previously sent/received packet.\n">&2;
+	printf "\n">&2;
+	printf " Optional: --checksum-min x (where x is 0-65536)\n">&2;
+	printf "  Restrict the packet checksum to a minimum.\n">&2;
+	printf "  this could be beneficial if you know the checksum of a previously sent/received packet.\n">&2;
+	printf "\n">&2;
+	printf " Optional: --checksum-max x (where x is 0-65536)\n">&2;
+	printf "  Restrict the packet checksum to a maximum.\n">&2;
+	printf "  this could be beneficial if you know the checksum of a previously sent/received packet.\n">&2;
+	printf "\n">&2;
+	printf " You must not supply an exact packet checksum alongside a minimum or maximum checksum restriction.\n">&2;
+	printf " You are not required to supply both a minimum and maximum packet checksum.\n">&2;
+	printf " When supplied together, --checksum-min must be less than --checksum-max\n">&2;
 	printf "\n">&2;
 	printf " Optional: --skip-validation\n">&2;
-	printf "  Note: this causes the program to skip validating inputs (if you know they are valid).\n">&2;
+	printf "  this causes the program to skip validating inputs (if you know they are valid).\n">&2;
 	printf "\n">&2;
 	printf " Optional: --only-validation\n">&2;
-	printf "  Note: this causes the program to exit after validating inputs.\n">&2;
+	printf "  this causes the program to exit after validating inputs.\n">&2;
 	printf "\n">&2;
 }
 
@@ -114,6 +114,8 @@ if [ "$1" = "-h" ]; then print_usage_then_exit; fi
 if [ "$1" = "-ehd" ]; then print_description; printf "\n">&2; print_dependencies; printf "\n">&2; print_usage; exit 2; fi
 
 #ARGUMENTS:
+OFFSET_MARKER="th";
+OFFSET_HEADER_BEGIN=0;
 SOURCE_PORT="";
 SOURCE_PORT_BEGIN=0;
 SOURCE_PORT_END=0;
@@ -126,8 +128,8 @@ LENGTH="";
 LENGTH_MIN="";
 LENGTH_MAX="";
 CHECKSUM="";
-OFFSET_MARKER="th";
-OFFSET_HEADER_BEGIN=0;
+CHECKSUM_MIN="";
+CHECKSUM_MAX="";
 
 #FLAGS:
 SKIP_VALIDATION=0;
@@ -140,6 +142,28 @@ while true; do
 		#since this is an argument parameter and requires a value; the program cannot continue.
 		#Else, if the argument was provided, and its 'value' is empty; the program cannot continue.
 		#Else, assign the argument, and shift 2 (both the argument indicator and its value / move next)
+
+		--offset-marker)
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			elif [ -z "$2" ]; then
+				print_usage_then_exit;
+			else
+				OFFSET_MARKER=$2;
+				shift 2;
+			fi
+		;;
+
+		--offset-header-begin)
+			if [ $# -lt 2 ]; then
+				print_usage_then_exit;
+			elif [ -z "$2" ]; then
+				print_usage_then_exit;
+			else
+				OFFSET_HEADER_BEGIN=$2;
+				shift 2;
+			fi
+		;;
 
 		--source-port)
 			if [ $# -lt 2 ]; then
@@ -207,24 +231,24 @@ while true; do
 			fi
 		;;
 
-		--offset-marker)
+		--checksum-min)
 			if [ $# -lt 2 ]; then
 				print_usage_then_exit;
 			elif [ -z "$2" ]; then
 				print_usage_then_exit;
 			else
-				OFFSET_MARKER=$2;
+				CHECKSUM_MIN=$2;
 				shift 2;
 			fi
 		;;
 
-		--offset-header-begin)
+		--checksum-max)
 			if [ $# -lt 2 ]; then
 				print_usage_then_exit;
 			elif [ -z "$2" ]; then
 				print_usage_then_exit;
 			else
-				OFFSET_HEADER_BEGIN=$2;
+				CHECKSUM_MAX=$2;
 				shift 2;
 			fi
 		;;
@@ -285,7 +309,7 @@ if [ $SKIP_VALIDATION -eq 0 ]; then
 	fi
 
 	if [ -n "$SOURCE_PORT" ]; then
-		if [ -n "$(echo $SOURCE_PORT | grep '-')" ]; then
+		if [ -n "$(echo $SOURCE_PORT | grep '[0-9]\{1,5\}-[0-9]\{1,5\}')" ]; then
 			SOURCE_PORT_IS_RANGE=1;
 			SOURCE_PORT_BEGIN=$(echo $SOURCE_PORT | cut -d '-' -f 1);
 			SOURCE_PORT_END=$(echo $SOURCE_PORT | cut -d '-' -f 2);
@@ -326,12 +350,7 @@ if [ $SKIP_VALIDATION -eq 0 ]; then
 				SOURCE_PORT_BEGIN=0;
 				SOURCE_PORT_END=0;
 			fi
-		else
-			if [ -z "$(echo $SOURCE_PORT | grep '[0-9]\{1,5\}')" ]; then
-				printf "\nInvalid --source-port (must be a 1-5 digit number). ">&2;
-				print_usage_then_exit;
-			fi
-
+		elif [ -n "$(echo $SOURCE_PORT | grep '[0-9]\{1,5\}')" ]
 			if [ $SOURCE_PORT -lt 1 ]; then
 				printf "\nInvalid --source-port (must be greater than 0). ">&2;
 				print_usage_then_exit;
@@ -341,11 +360,14 @@ if [ $SKIP_VALIDATION -eq 0 ]; then
 				printf "\nInvalid --source-port (must be less than 65536). ">&2;
 				print_usage_then_exit;
 			fi
+		else
+			printf "\nInvalid --source-port. ">&2;
+			print_usage_then_exit;
 		fi
 	fi
 
 	if [ -n "$DESTINATION_PORT" ]; then
-		if [ -n "$(echo $DESTINATION_PORT | grep '-')" ]; then
+		if [ -n "$(echo $DESTINATION_PORT | grep '[0-9]\{1,5\}-[0-9]\{1,5\}')" ]; then
 			DESTINATION_PORT_IS_RANGE=1;
 			DESTINATION_PORT_BEGIN=$(echo $DESTINATION_PORT | cut -d '-' -f 1);
 			DESTINATION_PORT_END=$(echo $DESTINATION_PORT | cut -d '-' -f 2);
@@ -386,12 +408,7 @@ if [ $SKIP_VALIDATION -eq 0 ]; then
 				DESTINATION_PORT_BEGIN=0;
 				DESTINATION_PORT_END=0;
 			fi
-		else
-			if [ -z "$(echo $DESTINATION_PORT | grep '[0-9]\{1,5\}')" ]; then
-				printf "\nInvalid --destination-port (must be a 1-5 digit number). ">&2;
-				print_usage_then_exit;
-			fi
-
+		elif [ -n "$(echo $DESTINATION_PORT | grep '[0-9]\{1,5\}')" ]
 			if [ $DESTINATION_PORT -lt 1 ]; then
 				printf "\nInvalid --destination-port (must be greater than 0). ">&2;
 				print_usage_then_exit;
@@ -401,6 +418,9 @@ if [ $SKIP_VALIDATION -eq 0 ]; then
 				printf "\nInvalid --destination-port (must be less than 65536). ">&2;
 				print_usage_then_exit;
 			fi
+		else
+			printf "\nInvalid --destination-port. ">&2;
+			print_usage_then_exit;
 		fi
 	fi
 
@@ -431,6 +451,7 @@ if [ $SKIP_VALIDATION -eq 0 ]; then
 		fi
 	fi
 
+	LENGTH_MIN_IS_VALID=0;
 	if [ -n "$LENGTH_MIN" ]; then
 		if [ -z "$(echo $LENGTH_MIN | grep '[0-9]\{1,5\}')" ]; then
 			printf "\nInvalid --length-min (must be a 1-5 digit number). ">&2;
@@ -441,11 +462,24 @@ if [ $SKIP_VALIDATION -eq 0 ]; then
 			printf "\nInvalid --length-min (must be 0 or greater). ">&2;
 			print_usage_then_exit;
 		fi
+
+		if [ $LENGTH_MIN -gt 65535 ]; then
+			printf "\nInvalid --length-min (must be less than 65536). ">&2;
+			print_usage_then_exit;
+		fi
+
+		LENGTH_MIN_IS_VALID=0;
 	fi
 
+	LENGTH_MAX_IS_VALID=0;
 	if [ -n "$LENGTH_MAX" ]; then
 		if [ -z "$(echo $LENGTH_MAX | grep '[0-9]\{1,5\}')" ]; then
 			printf "\nInvalid --length-max (must be a 1-5 digit number). ">&2;
+			print_usage_then_exit;
+		fi
+
+		if [ $LENGTH_MAX -lt 0 ]; then
+			printf "\nInvalid --length-max (must be 0 or greater). ">&2;
 			print_usage_then_exit;
 		fi
 
@@ -453,10 +487,22 @@ if [ $SKIP_VALIDATION -eq 0 ]; then
 			printf "\nInvalid --length-max (must be less than 65536). ">&2;
 			print_usage_then_exit;
 		fi
+
+		LENGTH_MAX_IS_VALID=0;
 	fi
 
-	if [ -n "$LENGTH_MIN" ] && [ -n "$LENGTH_MAX" ] && [ $LENGTH_MIN -ge $LENGTH_MAX ]; then
-		printf "">&2;
+	if [ $LENGTH_MIN_IS_VALID -eq 1 ] && [ $LENGTH_MAX_IS_VALID -eq 1 ] && [ $LENGTH_MIN -ge $LENGTH_MAX ]; then
+		printf "\nInvalid --length-min or --length-max (minimum must be less than maximum.)">&2;
+		print_usage_then_exit;
+	fi
+
+	if [ -n "$CHECKSUM" ] && [ -n "$CHECKSUM_MIN" ]; then
+		printf "\nInvalid combination of --checksum and --checksum-min. ">&2;
+		print_usage_then_exit;
+	fi
+
+	if [ -n "$CHECKSUM" ] && [ -n "$CHECKSUM_MAX" ]; then
+		printf "\nInvalid combinations of --checksum and --checksum-max. ">&2;
 		print_usage_then_exit;
 	fi
 
@@ -477,6 +523,50 @@ if [ $SKIP_VALIDATION -eq 0 ]; then
 		fi
 	fi
 
+	CHECKSUM_MIN_IS_VALID=0;
+	if [ -n "$CHECKSUM_MIN" ]; then
+		if [ -z "$(echo $CHECKSUM_MIN | grep '[0-9]\{1,5\}')" ]; then
+			printf "\nInvalid --checksum-min (must be a 1-5 digit number). ">&2;
+			print_usage_then_exit;
+		fi
+
+		if [ $CHECKSUM_MIN -lt 0 ]; then
+			printf "\nInvalid --checksum-min (must be 0 or greater). ">&2;
+			print_usage_then_exit;
+		fi
+
+		if [ $CHECKSUM_MIN -gt 65535 ]; then
+			printf "\nInvalid --checksum-min (must be less than 65536). ">&2;
+			print_usage_then_exit;
+		fi
+
+		CHECKSUM_MIN_IS_VALID=0;
+	fi
+
+	CHECKSUM_MAX_IS_VALID=0;
+	if [ -n "$CHECKSUM_MAX" ]; then
+		if [ -z "$(echo $CHECKSUM_MAX | grep '[0-9]\{1,5\}')" ]; then
+			printf "\nInvalid --checksum-max (must be a 1-5 digit number). ">&2;
+			print_usage_then_exit;
+		fi
+
+		if [ $CHECKSUM_MAX -lt 0 ]; then
+			printf "\nInvalid --checksum-max (must be 0 or greater). ">&2;
+			print_usage_then_exit;
+		fi
+
+		if [ $CHECKSUM_MAX -gt 65535 ]; then
+			printf "\nInvalid --checksum-max (must be less than 65536). ">&2;
+			print_usage_then_exit;
+		fi
+
+		CHECKSUM_MAX_IS_VALID=0;
+	fi
+
+	if [ $CHECKSUM_MIN_IS_VALID -eq 1 ] && [ $CHECKSUM_MAX_IS_VALID -eq 1 ] && [ $CHECKSUM_MIN -ge $CHECKSUM_MAX ]; then
+		printf "\nInvalid --checksum-min or --checksum-max (minimum must be less than maximum.)">&2;
+		print_usage_then_exit;
+	fi
 fi
 
 if [ $ONLY_VALIDATION -eq 1 ]; then exit 0; fi
@@ -486,6 +576,10 @@ BIT_OFFSET_DESTINATION_PORT=$(($BIT_OFFSET_SOURCE_PORT+16));
 BIT_OFFSET_LENGTH=$(($BIT_OFFSET_DESTINATION_PORT+16));
 BIT_OFFSET_CHECKSUM=$(($BIT_OFFSET_LENGTH+16));
 BIT_OFFSET_DATA=$(($BIT_OFFSET_CHECKSUM+16));
+
+if [ $SKIP_VALIDATION -eq 1 ] && [ -n "$(echo $SOURCE_PORT | grep '[0-9]\{1,5\}-[0-9]\{1,5\}')" ]; then
+	DESTINATION_PORT_IS_RANGE=1;
+fi
 
 if [ -n "$SOURCE_PORT" ]; then
 	printf "\\t\\t#Match source port\n";
@@ -497,6 +591,10 @@ if [ -n "$SOURCE_PORT" ]; then
 	fi
 else
 	printf "\\t\\t#Source port is unrestricted - confirm the security implications.\n"
+fi
+
+if [ $SKIP_VALIDATION -eq 1 ] && [ -n "$(echo $DESTINATION_PORT | grep '[0-9]\{1,5\}-[0-9]\{1,5\}')" ]; then
+	DESTINATION_PORT_IS_RANGE=1;
 fi
 
 if [ -n "$DESTINATION_PORT" ]; then
@@ -511,27 +609,52 @@ else
 	printf "\\t\\t#Destination port is unrestricted - confirm the security implications.\n"
 fi
 
-if [ -n "$LENGTH" ] || [ -n "$LENGTH_MIN" ] || [ -n "$LENGTH_MAX" ]; then
-	printf "\\t\\t#Match length\n";
-fi
 if [ -n "$LENGTH" ]; then
-	printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_LENGTH,16 = $LENGTH \\\\\n";
+	printf "\\t\\t#Match length (only $LENGTH)\n";
+	printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_LENGTH,16 $LENGTH \\\\\n";
 fi
-if [ -n "$LENGTH_MIN" ]; then
+
+if [ -n "$LENGTH_MIN" ] && [ -z "$LENGTH_MAX" ]; then
+	printf "\\t\\t#Match length (minimum $LENGTH_MIN)\n";
 	printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_LENGTH,16 >= $LENGTH_MIN \\\\\n";
 fi
-if [ -n "$LENGTH_MAX" ]; then
+
+if [ -z "$LENGTH_MIN" ] && [ -n "$LENGTH_MAX" ]; then
+	printf "\\t\\t#Match length (maximum $LENGTH_MAX)\n";
 	printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_LENGTH,16 <= $LENGTH_MAX \\\\\n";
 fi
+
+if [ -n "$LENGTH_MIN" ] && [ -n "$LENGTH_MAX" ]; then
+	printf "\\t\\t#Match length ($LENGTH_MIN-$LENGTH_MAX)\n";
+	printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_LENGTH,16 <= $LENGTH_MAX \\\\\n";
+fi
+
 if [ -z "$LENGTH" ] && [ -z "$LENGTH_MIN" ] && [ -z "$LENGTH_MAX" ]; then
 	printf "\\t\\t#Length is unrestricted\n";
 fi
 
 if [ -n "$CHECKSUM" ]; then
-	printf "\\t\\t#Match checksum\n";
+	printf "\\t\\t#Match checksum (only $CHECKSUM)\n";
 	printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_CHECKSUM,16 $CHECKSUM \\\\\n";
-else
-	printf "\\t\\t#Checksum is unrestricted\n";
+fi
+
+if [ -n "$CHECKSUM_MIN" ] && [ -z "$CHECKSUM_MAX" ]; then
+	printf "\\t\\t#Match checksum (minimum $CHECKSUM_MIN)\n";
+	printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_CHECKSUM,16 >= $CHECKSUM_MIN \\\\\n";
+fi
+
+if [ -z "$CHECKSUM_MIN" ] && [ -n "$CHECKSUM_MAX" ]; then
+	printf "\\t\\t#Match checksum (maximum $CHECKSUM_MAX)\n";
+	printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_CHECKSUM,16 <= $CHECKSUM_MAX \\\\\n";
+fi
+
+if [ -n "$CHECKSUM_MIN" ] && [ -n "$CHECKSUM_MAX" ]; then
+	printf "\\t\\t#Match checksum ($CHECKSUM_MIN-$CHECKSUM_MAX)\n";
+	printf "\\t\\t@$OFFSET_MARKER,$BIT_OFFSET_CHECKSUM,16 <= $CHECKSUM_MAX \\\\\n";
+fi
+
+if [ -z "$CHECKSUM" ] && [ -z "$CHECKSUM_MIN" ] && [ -z "$CHECKSUM_MAX" ]; then
+	printf "\\t\\t#Length is unrestricted\n";
 fi
 
 exit 0;
