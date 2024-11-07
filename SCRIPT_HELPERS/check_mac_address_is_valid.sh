4f39e09a29afaa8f6f2aaf20020b63a1a59b0f77
Dylan <sunny73cr@protonmail.com>
2024-11-08 10:20:18 +1300

    modify: check_mac_address_is_valid.sh - migrate script to new format. improve debug output. title script. document dependencies.

--- SCRIPT_HELPERS/check_mac_address_is_valid.sh
+++ SCRIPT_HELPERS/check_mac_address_is_valid.sh
@@ -1,42 +1,121 @@
 #!/bin/sh
 
-print_usage_then_exit () {
-	printf "Usage: $0 <arguments>\n">&2
-	printf " --address XX:XX:XX:XX:XX:XX (where X is a-f, A-F, 0-9)\n">&2;
-	printf "\n">&2;
+if [ -z "$ENV_SETUP_NFT" ]; then printf "setup-netfilter: set ENV_SETUP_NFT to the root path of the setup-netfilter directory before continuing.\n">&2; exit 4; fi
+
+print_description() {
+	printf "A program that confirms or denies if a MAC address is valid.\n">&2;
+}
+
+print_description_then_exit() {
+	print_description;
 	exit 2;
 }
 
-if [ "$1" = "" ]; then print_usage_then_exit; fi
+if [ "$1" = "-e" ]; then print_description_then_exit; fi
 
+print_dependencies() {
+	printf "printf\n">&2;
+	printf "echo\n">&2;
+	printf "grep\n">&2;
+	printf "\n">&2;
+}
+
+print_dependencies_then_exit() {
+	print_dependencies;
+	exit 2;
+}
+
+if [ "$1" = "-d" ]; then print_dependencies_then_exit; fi
+
+print_usage() {
+	printf "Flags used by themselves: \n">&2;
+	printf " -e (prints an explanation of the functions' purpose) (exit code 2)\n">&2
+	printf " -h (prints an explanation of the functions' available parameters, and their effect) (exit code 2)\n">&2;
+	printf " -d (prints the functions' dependencies: newline delimited list) (exit code 2)\n">&2;
+	printf " -ehd (prints the above three) (exit code 2)\n">&2;
+	printf "\n">&2;
+	printf "Parameters:\n">&2;
+	printf "\n">&2;
+	printf " Required: --address XX:XX:XX:XX:XX:XX (where X is 0-9, a-f, A-F; hexadecimal)\n">&2;
+	printf "  The MAC address string to check.\n">&2;
+	printf "  If the MAC address does not follow the form; an exit code of 1 is returned.\n">&2;
+	printf "\n">&2;
+}
+
+print_usage_then_exit() {
+	print_usage;
+	exit 2;
+}
+
+if [ "$1" = "-h" ]; then print_usage_then_exit; fi
+
+if [ "$1" = "-ehd" ]; then print_description; printf "\n">&2; print_dependencies; printf "\n">&2; print_usage; exit 2; fi
+
+#ARGUMENTS:
 ADDRESS="";
+
+#FLAGS:
+SKIP_VALIDATION=0;
+ONLY_VALIDATE=0;
 
 while true; do
 	case $1 in
-		--address )
+		#Approach to parsing arguments:
+		#If the length of 'all arguments' is less than 2 (shift reduces this number),
+		#since this is an argument parameter and requires a value; the program cannot continue.
+		#Else, if the argument was provided, and its 'value' is empty; the program cannot continue.
+		#Else, assign the argument, and shift 2 (both the argument indicator and its value / move next)
+
+		--address)
 			if [ $# -lt 2 ]; then
+				printf "\nNot enough arguments (value for $1 is missing.) ">&2;
 				print_usage_then_exit;
-			elif [ "$2" = "" ]; then
+			elif [ -z "$2" ]; then
+				printf "\nNot enough arguments (value for $1 is empty.) ">&2;
 				print_usage_then_exit;
 			else
 				ADDRESS=$2;
 				shift 2;
 			fi
 		;;
-		"") break; ;;
-		*) printf "Unrecognised argument $1. ">&2; print_usage_then_exit; ;;
-	esac
-done
 
-if [ -z "$ADDRESS" ]; then
-	printf "\nMissing --address. ">&2;
-	print_usage_then_exit;
+		#Approach to parsing flags:
+		#If the flag was provided, toggle on its value; then move next
+		#Or shift 1 / remove the flag from the list
+
+		--skip-validation)
+			SKIP_VALIDATION=1;
+			shift 1;
+		;;
+
+		--only-validate)
+			ONLY_VALIDATE=1;
+			shift 1;
+		;;
+
+		#Handle the case of 'end' of arg parsing; where all flags are shifted from the list,
+		#or the program was called without any parameters. exit the arg parsing loop.
+		"") break; ;;
+
+		#Handle the case where an argument or flag was called that the program does not recognise.
+		#This should prefix the 'usage' text with the reason the program failed.
+		#The 'Standard Error' file descriptor is used to separate failure output or log messages from actual program output.
+		*) printf "\nUnrecognised argument $1. ">&2; print_usage_then_exit; ;;
+
+	esac
+done;
+
+if [ $SKIP_VALIDATION -eq 0 ]; then
+	if [ -z "$ADDRESS" ]; then
+		printf "\nMissing --address. ">&2;
+		print_usage_then_exit;
+	fi
 fi
 
-REGEX='^[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}$';
+if [ $ONLY_VALIDATE -eq 1 ]; then exit 0; fi
 
-if [ -n "$(echo "$ADDRESS" | grep -E $REGEX)" ]; then
-	exit 0;
-else
+if [ -z "$(echo $ADDRESS | grep '[0-9a-fA-F]\{2\}:[0-9a-fA-F]\{2\}:[0-9a-fA-F]\{2\}:[0-9a-fA-F]\{2\}:[0-9a-fA-F]\{2\}:[0-9a-fA-F]\{2\}')" ]; then
 	exit 1;
-fi;
+fi
+
+exit 0;
