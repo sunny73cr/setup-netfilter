409b1c7b7e27b61500f24863b88a79a564cd64a4
Dylan <sunny73cr@protonmail.com>
2024-11-08 11:50:39 +1300

    modify: convert_base10_to_binary.sh - migrate the script the new format. improve debug output. title the script. document dependencies.

--- SCRIPT_HELPERS/convert_base10_to_binary.sh
+++ SCRIPT_HELPERS/convert_base10_to_binary.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -z "$ENV_SETUP_NFT" ]; then printf "setup-netfilter: set ENV_SETUP_NFT to the absolute path of the setup-netfilter directory first.\n">&2; exit 4; fi
+if [ -z "$ENV_SETUP_NFT" ]; then printf "setup-netfilter: set ENV_SETUP_NFT to the root path of the setup-netfilter directory before continuing.\n">&2; exit 4; fi
 
 DEPENDENCY_SCRIPT_PATH_SUBSTRING="$ENV_SETUP_NFT/SCRIPT_HELPERS/substring.sh";
 
@@ -16,94 +16,155 @@ if [ ! -x $DEPENDENCY_SCRIPT_PATH_EXPONENT ]; then
 	exit 2;
 fi
 
-print_usage_then_exit () {
-	printf "Usage: $0 <arguments>\n">&2;
-	printf " --number (0 - 4,294,967,296; without delimiters)\n">&2;
-	printf "\n">&2;
-	printf " Optional: --output-bit-order ('big-endian' or 'little-endian') (no hyphens)\n">&2;
-	printf " Note: if omitted, output-bit-order defaults to 'little endian'\n">&2;
-	printf "\n">&2;
-	printf " Optional: --output-bit-length (1 to 32)\n">&2;
-	printf " Note: if omitted, output-bit-length defaults to the smallest length required.\n">&2;
-	printf " Note: if the output-bit-length is greater than neccessary, the binary string is padded with zeroes.\n">&2;
-	printf "\n">&2;
-	printf " Optional: --newline-suffix-output\n">&2;
-	printf " Note: if enabled, append a newline character to the end of the output.\n">&2;
-	printf "\n">&2;
-	printf " Optional: --only-validate\n">&2;
-	printf " Note: this causes the program to exit after performing validation.\n">&2;
-	printf "\n">&2;
-	printf " Optional: --skip-validate\n">&2;
-	printf " Note: this causes the program to skip validation (if you know the inputs are correct.)\n">&2;
-	printf "\n">&2;
+print_description() {
+	printf "A program that converts base 10 (decimal) numbers into a binary string.\n">&2;
+}
+
+print_description_then_exit() {
+	print_description;
 	exit 2;
 }
 
-NUMBER="";
-BIT_ORDER="";
-BIT_LENGTH="";
+if [ "$1" = "-e" ]; then print_description_then_exit; fi
 
+print_dependencies() {
+	printf "printf\n">&2;
+	printf "echo\n">&2;
+	printf "$DEPENDENCY_SCRIPT_PATH_SUBSTRING\n">&2;
+	printf "$DEPENDENCY_SCRIPT_PATH_EXPONENT\n">&2;
+	printf "\n">&2;
+}
+
+print_dependencies_then_exit() {
+	print_dependencies;
+	exit 2;
+}
+
+if [ "$1" = "-d" ]; then print_dependencies_then_exit; fi
+
+print_usage() {
+	printf "Flags used by themselves: \n">&2;
+	printf " -e (prints an explanation of the functions' purpose) (exit code 2)\n">&2
+	printf " -h (prints an explanation of the functions' available parameters, and their effect) (exit code 2)\n">&2;
+	printf " -d (prints the functions' dependencies: newline delimited list) (exit code 2)\n">&2;
+	printf " -ehd (prints the above three) (exit code 2)\n">&2;
+	printf "\n">&2;
+	printf "Parameters:\n">&2;
+	printf "\n">&2;
+	printf " Required: --number (0-4,294,967,296)\n">&2;
+	printf "\n">&2;
+	printf " Optional: --output-bit-order ('big-endian' or 'little-endian') (no hyphens)\n">&2;
+	printf "  Alter the 'endianness' of the binary result.">&2;
+	printf "  if omitted, output-bit-order defaults to 'little endian'\n">&2;
+	printf "\n">&2;
+	printf " Optional: --output-bit-length (1 to 32)\n">&2;
+	printf "  if omitted, output-bit-length defaults to the smallest length required.\n">&2;
+	printf "  if the output-bit-length is greater than neccessary, the binary string is padded with zeroes.\n">&2;
+	printf "\n">&2;
+	printf " Optional: --skip-validation\n">&2;
+	printf "  Presence of this flag causes the program to skip validating inputs (if you know they are correct).\n">&2;
+	printf "\n">&2;
+	printf " Optional: --only-validate\n">&2;
+	printf "  Presence of this flag causes the program to exit after validating inputs.\n">&2;
+	printf "\n">&2;
+}
+
+print_usage_then_exit() {
+	print_usage;
+	exit 2;
+}
+
+if [ "$1" = "-h" ]; then print_usage_then_exit; fi
+
+if [ "$1" = "-ehd" ]; then print_description; printf "\n">&2; print_dependencies; printf "\n">&2; print_usage; exit 2; fi
+
+#ARGUMENTS:
+NUMBER="";
+OUTPUT_BIT_ORDER="";
+OUTPUT_BIT_LENGTH="";
+
+#FLAGS:
+SKIP_VALIDATION=0;
 ONLY_VALIDATE=0;
-SKIP_VALIDATE=0;
-NEWLINE_SUFFIX_OUTPUT=0;
 
 while true; do
-	case "$1" in
+	case $1 in
+		#Approach to parsing arguments:
+		#If the length of 'all arguments' is less than 2 (shift reduces this number),
+		#since this is an argument parameter and requires a value; the program cannot continue.
+		#Else, if the argument was provided, and its 'value' is empty; the program cannot continue.
+		#Else, assign the argument, and shift 2 (both the argument indicator and its value / move next)
+
 		--number)
 			if [ $# -lt 2 ]; then
+				printf "\nNot enough arguments (value for $1 is missing.) ">&2;
 				print_usage_then_exit;
-			elif [ "$2" = "" ]; then
+			elif [ -z "$2" ]; then
+				printf "\nNot enough arguments (value for $1 is empty.) ">&2;
 				print_usage_then_exit;
 			else
 				NUMBER=$2;
 				shift 2;
 			fi
 		;;
+
 		--output-bit-order)
 			if [ $# -lt 2 ]; then
+				printf "\nNot enough arguments (value for $1 is missing.) ">&2;
 				print_usage_then_exit;
-			elif [ "$2" = "" ]; then
+			elif [ -z "$2" ]; then
+				printf "\nNot enough arguments (value for $1 is empty.) ">&2;
 				print_usage_then_exit;
 			else
-				BIT_ORDER=$2;
+				OUTPUT_BIT_ORDER=$2;
 				shift 2;
 			fi
 		;;
+
 		--output-bit-length)
 			if [ $# -lt 2 ]; then
+				printf "\nNot enough arguments (value for $1 is missing.) ">&2;
 				print_usage_then_exit;
-			elif [ "$2" = "" ]; then
+			elif [ -z "$2" ]; then
+				printf "\nNot enough arguments (value for $1 is empty.) ">&2;
 				print_usage_then_exit;
 			else
-				BIT_LENGTH=$2;
+				OUTPUT_BIT_LENGTH=$2;
 				shift 2;
 			fi
 		;;
-		--newline-suffix-output)
-			NEWLINE_SUFFIX_OUTPUT=1;
+
+		#Approach to parsing flags:
+		#If the flag was provided, toggle on its value; then move next
+		#Or shift 1 / remove the flag from the list
+
+		--skip-validation)
+			SKIP_VALIDATION=1;
 			shift 1;
 		;;
+
 		--only-validate)
 			ONLY_VALIDATE=1;
 			shift 1;
 		;;
-		--skip-validate)
-			SKIP_VALIDATE=1;
-			shift 1;
-		;;
+
+		#Handle the case of 'end' of arg parsing; where all flags are shifted from the list,
+		#or the program was called without any parameters. exit the arg parsing loop.
 		"") break; ;;
-		*) printf "Unrecognised argument .">&2; print_usage_then_exit; ;;
+
+		#Handle the case where an argument or flag was called that the program does not recognise.
+		#This should prefix the 'usage' text with the reason the program failed.
+		#The 'Standard Error' file descriptor is used to separate failure output or log messages from actual program output.
+		*) printf "\nUnrecognised argument $1. ">&2; print_usage_then_exit; ;;
+
 	esac
-done
+done;
 
-if [ $ONLY_VALIDATE -eq 1 ] && [ $SKIP_VALIDATE -eq 1 ]; then
-	#why?
-	exit 0;
-fi
-
+#Read number from stdin (support piping input into the program)
 if [ -z "$NUMBER" ]; then
 	NUMBER=$(dd if=/dev/stdin of=/dev/stdout bs=1 count=10 status=none);
 
+	#else, no number; it is required; print usage and exit
 	if [ -z "$NUMBER" ]; then print_usage_then_exit; fi
 fi
 
@@ -113,7 +174,7 @@ if [ $SKIP_VALIDATE -eq 0 ]; then
 		print_usage_then_exit;
 	fi
 
-	if [ "$(printf $NUMBER | grep -E '^[0-9]{1,10}$')" = "" ]; then
+	if [ "$(printf $NUMBER | grep '^[0-9]\{1,10\}$')" = "" ]; then
 		printf "\nInvalid --number. ">&2;
 		print_usage_then_exit;
 	fi
@@ -124,7 +185,7 @@ if [ $SKIP_VALIDATE -eq 0 ]; then
 	fi
 
 	if [ -n "$BIT_LENGTH" ]; then
-		if [ "$(echo $BIT_LENGTH | grep -E '^[1-9][0-9]{0,1}$')" = "" ]; then
+		if [ "$(echo $BIT_LENGTH | grep '^[1-9][0-9]\{0,1\}$')" = "" ]; then
 			printf "\nInvalid --bit-length. ">&2;
 			print_usage_then_exit;
 		fi
@@ -142,7 +203,7 @@ if [ $SKIP_VALIDATE -eq 0 ]; then
 		BIT_LENGTH_CAPACITY=$($DEPENDENCY_SCRIPT_PATH_EXPONENT --base 2 --exponent $BIT_LENGTH);
 		case $? in
 			0) ;;
-			*) printf "$0: dependency: \"$DEPENDENCY_SCRIPT_PATH_EXPONENT\" produced a failure exit code.\n">&2; exit 3; ;;
+			*) printf "$0: dependency: \"$DEPENDENCY_SCRIPT_PATH_EXPONENT\" produced a failure exit code ($?).\n">&2; exit 3; ;;
 		esac
 		BIT_LENGTH_CAPACITY_MINUS_ONE=$(($BIT_LENGTH_CAPACITY - 1));
 
@@ -204,23 +265,19 @@ done;
 if [ -n "$BIT_LENGTH" ]; then
 	#Zero pad binary output to desired bit length.
 
-	#BIT_LENGTH_MINUS_ONE=$(($BIT_LENGTH-1));
-
 	ZERO_PAD_COUNT=$(( $BIT_LENGTH - ${#RESULT} ));
 
 	ZERO_PAD="";
 
-	ZERO="0";
-
 	while true; do
 		if [ $ZERO_PAD_COUNT -eq 0 ]; then break; fi
 
-		ZERO_PAD="$ZERO_PAD$ZERO";
+		ZERO_PAD="${ZERO_PAD}0";
 
 		ZERO_PAD_COUNT=$(( $ZERO_PAD_COUNT - 1 ));
 	done;
 
-	#Output in desired bit order.
+	#Zero pad for desired bit order.
 
 	if [ $BIT_ORDER -eq 0 ]; then
 		#big endian
@@ -232,10 +289,6 @@ if [ -n "$BIT_LENGTH" ]; then
 else
 	#no zero padding, already in desired order.
 	printf "$RESULT";
-fi
-
-if [ $NEWLINE_SUFFIX_OUTPUT -eq 1 ]; then
-	printf "\n";
 fi
 
 exit 0;
